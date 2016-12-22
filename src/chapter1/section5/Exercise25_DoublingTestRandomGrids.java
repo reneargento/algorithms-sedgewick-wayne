78e4b0176d0fd8e075c98630c00ca72034e8a321
Rene Argento <rene.argento@gmail.com>
2016-12-22 01:34:45 -0200

    Finished chapter 1.5

---
+++ src/chapter1/section5/Exercise25_DoublingTestRandomGrids.java
@@ -0,0 +1,132 @@
+package chapter1.section5;
+
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.Stopwatch;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by rene on 21/12/16.
+ */
+public class Exercise25_DoublingTestRandomGrids {
+
+    private class Experiment {
+
+        int numberOfSites;
+        int numberOfConnectionsProcessed;
+        double ratioOfRunningTimeToPrevious;
+
+        public Experiment(int numberOfSites, int numberOfConnectionsProcessed, double ratioOfRunningTimeToPrevious) {
+            this.numberOfSites = numberOfSites;
+            this.numberOfConnectionsProcessed = numberOfConnectionsProcessed;
+            this.ratioOfRunningTimeToPrevious = ratioOfRunningTimeToPrevious;
+        }
+    }
+
+    public static void main(String[] args) {
+        int numberOfExperiments = Integer.parseInt(args[0]);
+
+        Exercise25_DoublingTestRandomGrids doublingTestRandomGrids = new Exercise25_DoublingTestRandomGrids();
+
+        for(int i = 1; i <= 3; i++) {
+
+            switch (i) {
+                case 1: StdOut.println("Quick Find"); break;
+                case 2: StdOut.println("Quick Union"); break;
+                case 3: StdOut.println("Weighted Quick Union");  break;
+            }
+
+            doublingTestRandomGrids.doExperiments(numberOfExperiments, i);
+            StdOut.println();
+        }
+    }
+
+    private void doExperiments(int numberOfExperiments, int unionFindType) {
+
+        List<Experiment> experiments = new ArrayList<>();
+
+        int numberOfSites = 512;
+
+        //Previous time
+        UF initialUnionFind = generateUnionFind(numberOfSites / 2, unionFindType);
+
+        Stopwatch initialTimer = new Stopwatch();
+
+        randomGridsConnection(numberOfSites / 2, initialUnionFind);
+
+        double previousRunningTime = initialTimer.elapsedTime();
+
+        for(int i=0; i < numberOfExperiments; i++) {
+
+            UF unionFind = generateUnionFind(numberOfSites, unionFindType);
+
+            Stopwatch timer = new Stopwatch();
+
+            int pairsGenerated = randomGridsConnection(numberOfSites, unionFind);
+
+            double runningTime = timer.elapsedTime();
+
+            Experiment experiment = new Experiment(numberOfSites, pairsGenerated, runningTime / previousRunningTime);
+            experiments.add(experiment);
+
+            previousRunningTime = runningTime;
+            numberOfSites *= 2;
+        }
+
+        printResults(experiments);
+    }
+
+    private UF generateUnionFind(int numberOfSites, int unionFindType) {
+
+        UF unionFind;
+
+        switch (unionFindType) {
+            case 1: unionFind = new QuickFind(numberOfSites); break;
+            case 2: unionFind = new QuickUnion(numberOfSites); break;
+            case 3: unionFind = new WeightedQuickUnion(numberOfSites); break;
+            default: unionFind = null;
+        }
+
+        return unionFind;
+    }
+
+    private static int randomGridsConnection(int numberOfSites, UF unionFind) {
+        Exercise18_RandomGridGenerator randomGridGenerator = new Exercise18_RandomGridGenerator();
+        Exercise18_RandomGridGenerator.Connection[] connections = randomGridGenerator.generate(numberOfSites);
+
+        int connectionsGenerated = 0;
+        int connectionIndex = 0;
+
+        while(unionFind.count() != 1) {
+            int randomSite1 = connections[connectionIndex].p;
+            int randomSite2 = connections[connectionIndex].q;
+
+            connectionsGenerated++;
+
+            if(!unionFind.connected(randomSite1, randomSite2)) {
+                unionFind.union(randomSite1, randomSite2);
+            }
+
+            connectionIndex++;
+        }
+
+        return connectionsGenerated;
+    }
+
+    private void printResults(List<Experiment> experiments) {
+        StdOut.printf("%12s %17s %23s %23s\n", "Experiment |", "Number of Sites |",
+                "AVG Pairs Generated |", "Ratio of Running Time |");
+
+        int experimentId = 1;
+
+        for(Experiment experiment : experiments) {
+
+            StdOut.printf("%7d %13d %21d %23.1f\n", experimentId, experiment.numberOfSites,
+                    experiment.numberOfConnectionsProcessed, experiment.ratioOfRunningTimeToPrevious);
+
+            experimentId++;
+        }
+    }
+
+}
---
+++ src/chapter1/section5/Exercise26_AmortizedPlotErdosRenyi.java
@@ -0,0 +1,141 @@
+package chapter1.section5;
+
+import edu.princeton.cs.algs4.StdRandom;
+import util.GraphPanel;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by rene on 22/12/16.
+ */
+public class Exercise26_AmortizedPlotErdosRenyi {
+
+    private class UnionFind implements UF {
+
+        private int[] leaders;
+        private int[] ranks;
+
+        private int components;
+
+        //Used for plotting amortized costs
+        int operation;
+        int currentCost;
+        int totalCost;
+        List<Integer> total;
+
+        public UnionFind(int n) {
+            leaders = new int[n];
+            ranks = new int[n];
+            components = n;
+
+            operation = 1;
+            currentCost = 0;
+            totalCost = 0;
+            total = new ArrayList<>();
+
+            for(int i=0; i < n; i++) {
+                leaders[i]  = i;
+                ranks[i] = 0;
+            }
+        }
+
+        public int count() {
+            return components;
+        }
+
+        public boolean connected(int site1, int site2) {
+
+            boolean isConnected = find(site1) == find(site2);
+
+            if(isConnected) {
+                updateCostAnalysis();
+            }
+
+            return isConnected;
+        }
+
+        //O(inverse Ackermann function)
+        public int find(int site) {
+            currentCost++;
+
+            if(site == leaders[site]) {
+                return site;
+            }
+
+            currentCost++;
+
+            return leaders[site] = find(leaders[site]);
+        }
+
+        //O(inverse Ackermann function)
+        public void union(int site1, int site2) {
+
+            int leader1 = find(site1);
+            int leader2 = find(site2);
+
+            if(leader1 == leader2) {
+                return;
+            }
+
+            if(ranks[leader1] < ranks[leader2]) {
+                leaders[leader1] = leader2;
+            } else if (ranks[leader2] < ranks[leader1]) {
+                leaders[leader2] = leader1;
+            } else {
+                leaders[leader1] = leader2;
+                ranks[leader2]++;
+            }
+
+            components--;
+
+            currentCost++;
+            updateCostAnalysis();
+        }
+
+        private void updateCostAnalysis() {
+
+            totalCost += currentCost;
+
+            total.add(totalCost / operation);
+
+            currentCost = 0;
+            operation++;
+        }
+    }
+
+    public static void main(String[] args) {
+        int numberOfSites = Integer.parseInt(args[0]);
+
+        Exercise26_AmortizedPlotErdosRenyi amortizedPlotErdosRenyi = new Exercise26_AmortizedPlotErdosRenyi();
+        Exercise26_AmortizedPlotErdosRenyi.UnionFind unionFind = amortizedPlotErdosRenyi.new UnionFind(numberOfSites);
+
+        amortizedPlotErdosRenyi.erdosRenyi(numberOfSites, unionFind);
+        amortizedPlotErdosRenyi.draw(unionFind);
+    }
+
+    private void erdosRenyi(int numberOfSites, UF unionFind) {
+
+        while(unionFind.count() != 1) {
+
+            int randomSite1 = StdRandom.uniform(numberOfSites);
+            int randomSite2 = StdRandom.uniform(numberOfSites);
+
+            if(!unionFind.connected(randomSite1, randomSite2)) {
+                unionFind.union(randomSite1, randomSite2);
+            }
+        }
+    }
+
+    private void draw(UnionFind unionFind) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                GraphPanel graphPanel = new GraphPanel("Union Find", "Amortized Cost Plot for Erdos-Renyi",
+                        "Number of Connections", "Number of Array Accesses", unionFind.total);
+                graphPanel.createAndShowGui();
+            }
+        });
+    }
+
+}
