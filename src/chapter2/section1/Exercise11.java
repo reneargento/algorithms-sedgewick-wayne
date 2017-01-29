4a66f61184445f1517f18f92b7ef5692318ee6b2
Rene Argento <rene.argento@gmail.com>
2017-01-29 02:04:17 -0200

    Started chapter 2.1

---
+++ src/chapter2/section1/Exercise11.java
@@ -0,0 +1,66 @@
+package chapter2.section1;
+
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Created by rene on 28/01/17.
+ */
+public class Exercise11 {
+
+    public static void main(String[] args) {
+        int[] array = {2, 20, -1, -30, 30, 5, 6, 8, -99, -3, 0, 4, 4, 4};
+        shellsort(array);
+
+        for(int value : array) {
+            StdOut.print(value + " ");
+        }
+    }
+
+    private static void shellsort(int[] array) {
+
+        if(array == null || array.length == 0) {
+            return;
+        }
+
+        int maxIncrement = 1;
+        int numberOfIncrements = 1;
+
+        while((maxIncrement * 3) + 1 < array.length) {
+            maxIncrement = maxIncrement * 3;
+            maxIncrement++;
+
+            numberOfIncrements++;
+        }
+
+        int[] incrementSequence = new int[numberOfIncrements];
+
+        int index = 0;
+        while(maxIncrement > 0) {
+            incrementSequence[index] = maxIncrement;
+
+            maxIncrement--;
+            maxIncrement = maxIncrement / 3;
+            index++;
+        }
+
+        //Shellsort
+        for(int i=0; i < incrementSequence.length; i++) {
+
+            int increment = incrementSequence[i];
+
+            //h-sort the array
+            for(int j=increment; j < array.length; j++) {
+                int currentIndex = j;
+
+                while(currentIndex >= increment && array[currentIndex] < array[currentIndex - increment]) {
+                    int temp = array[currentIndex];
+                    array[currentIndex] = array[currentIndex - increment];
+                    array[currentIndex - increment] = temp;
+
+                    currentIndex = currentIndex - increment;
+                }
+            }
+        }
+    }
+
+}
---
+++ src/chapter2/section1/Exercise12.java
@@ -0,0 +1,90 @@
+package chapter2.section1;
+
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by rene on 29/01/17.
+ */
+public class Exercise12 {
+
+    private static class ComparesByIncrement {
+
+        int increment;
+        double comparesByArraySize;
+
+        public ComparesByIncrement(int increment, double comparesByArraySize) {
+            this.increment = increment;
+            this.comparesByArraySize = comparesByArraySize;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int maxArraySize = 10000000;
+
+        for(int i=100; i <= maxArraySize; i *= 10) {
+            double[] array = new double[i];
+
+            for(int j=0; j < array.length; j++) {
+                double value = StdRandom.uniform();
+                array[j] = value;
+            }
+
+            List<ComparesByIncrement> comparesByIncrements = shellsort(array);
+            printStatistics(comparesByIncrements);
+        }
+    }
+
+    private static List<ComparesByIncrement> shellsort(double[] array) {
+
+        List<ComparesByIncrement> comparesByIncrements = new ArrayList<>();
+        double numberOfCompares;
+
+        int incrementSequence = 1;
+
+        while(incrementSequence * 3 + 1 < array.length) {
+            incrementSequence *= 3;
+            incrementSequence++;
+        }
+
+        while (incrementSequence > 0) {
+
+            numberOfCompares = 0;
+
+            for(int i=incrementSequence; i < array.length; i++) {
+
+                numberOfCompares++;
+
+                for(int j=i; j >= incrementSequence && array[j] < array[j - incrementSequence]; j -= incrementSequence) {
+                    double temp = array[j];
+                    array[j] = array[j - incrementSequence];
+                    array[j - incrementSequence] = temp;
+
+                    numberOfCompares++;
+                }
+            }
+
+            double comparesByArraySize = numberOfCompares / (double) array.length;
+            ComparesByIncrement comparesByIncrement = new ComparesByIncrement(incrementSequence, comparesByArraySize);
+            comparesByIncrements.add(comparesByIncrement);
+
+            incrementSequence /= 3;
+        }
+
+        return comparesByIncrements;
+    }
+
+    private static void printStatistics(List<ComparesByIncrement> comparesByIncrements) {
+        StdOut.printf("%13s %22s\n", "Increment |", "Compares by Array Size");
+
+        for(ComparesByIncrement comparesByIncrement : comparesByIncrements) {
+            StdOut.printf("%11d %16.2f", comparesByIncrement.increment, comparesByIncrement.comparesByArraySize);
+            StdOut.println();
+        }
+    }
+
+}
