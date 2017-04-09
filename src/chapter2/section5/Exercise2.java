56c27a544025b36aa2a768ff1aab9be94cab66fc
Rene Argento <rene.argento@gmail.com>
2017-04-09 15:17:07 -0300

    Started section 2.5 exercises

---
+++ src/chapter2/section5/Exercise2.java
@@ -0,0 +1,74 @@
+package chapter2.section5;
+
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.*;
+
+/**
+ * Created by rene on 09/04/17.
+ */
+public class Exercise2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<String> wordList = new ArrayList<>();
+
+        /**
+         * Testcase
+         *
+         begin
+         end
+         test
+         afterthought
+         Brazil
+         thought
+         after
+         beginend
+         abc
+         */
+        while(scanner.hasNext()) {
+            wordList.add(scanner.next());
+        }
+
+        Exercise2 exercise2 = new Exercise2();
+        List<String> compoundWords = exercise2.getCompoundWords(wordList);
+        if(compoundWords.size() > 0) {
+            StdOut.println("Compound words:");
+        }
+        for(String compoundWord : compoundWords) {
+            StdOut.println(compoundWord);
+        }
+    }
+
+    private List<String> getCompoundWords(List<String> wordList) {
+        Set<String> wordsSet = new HashSet<>();
+
+        String[] words = new String[wordList.size()];
+        for(int i=0; i < wordList.size(); i++) {
+            words[i] = wordList.get(i);
+            wordsSet.add(words[i]);
+        }
+
+        Arrays.sort(words);
+
+        List<String> compoundWords = new ArrayList<>();
+
+        for(int i=0; i < words.length; i++) {
+            for(int j=i + 1; j < words.length; j++) {
+                //Does words[j] contain words[i] in its beginning?
+                if(words[j].length() >= words[i].length()
+                        && words[j].substring(0, words[i].length()).equals(words[i])) {
+
+                    String restOfTheWord = words[j].substring(words[i].length(), words[j].length());
+                    if(wordsSet.contains(restOfTheWord)) {
+                        compoundWords.add(words[j]);
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+
+        return compoundWords;
+    }
+}
---
+++ src/chapter2/section5/Exercise4.java
@@ -0,0 +1,64 @@
+package chapter2.section5;
+
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by rene on 09/04/17.
+ */
+public class Exercise4 {
+
+    public static void main(String[] args) {
+        String[] input = {"Algorithms", "Sedgewick", "Wayne", "Argento", "Djikstra", "Wayne", "Argento", "Prim"};
+
+        Exercise4 exercise4 = new Exercise4();
+        String[] dedupStrings = exercise4.dedup(input);
+
+        StdOut.println("Strings:");
+        for(String string : dedupStrings) {
+            StdOut.println(string);
+        }
+
+        StdOut.println();
+        StdOut.println("Expected: \n" +
+                "Algorithms\n" +
+                "Argento\n" +
+                "Djikstra\n" +
+                "Prim\n" +
+                "Sedgewick\n" +
+                "Wayne");
+    }
+
+    private String[] dedup(String[] strings) {
+        if(strings == null || strings.length == 0) {
+            return new String[0];
+        }
+
+        Arrays.sort(strings);
+
+        List<String> dedupStringList = new ArrayList<>();
+
+        String currentString = strings[0];
+        dedupStringList.add(strings[0]);
+
+        for(int i=1; i < strings.length; i++) {
+            if(strings[i].equals(currentString)) {
+                continue;
+            } else {
+                currentString = strings[i];
+            }
+
+            dedupStringList.add(strings[i]);
+        }
+
+        String[] dedupStringArray = new String[dedupStringList.size()];
+        for(int i=0; i < dedupStringArray.length; i++) {
+            dedupStringArray[i] = dedupStringList.get(i);
+        }
+
+        return dedupStringArray;
+    }
+}
