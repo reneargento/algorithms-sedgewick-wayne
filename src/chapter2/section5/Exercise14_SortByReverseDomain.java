23eb6127b36d21e4c154784607a279aad1af2b07
Rene Argento <rene.argento@gmail.com>
2017-04-12 19:14:34 -0300

    Added exercises 2.5.14 and 2.5.16

---
+++ src/chapter2/section5/Exercise14_SortByReverseDomain.java
@@ -0,0 +1,74 @@
+package chapter2.section5;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Arrays;
+
+/**
+ * Created by rene on 10/04/17.
+ */
+public class Exercise14_SortByReverseDomain {
+
+    private class Domain implements Comparable<Domain>{
+
+        String domainName;
+        String reverseDomainName;
+
+        Domain(String domainName) {
+            this.domainName = domainName;
+
+            String[] reverseDomain = domainName.split("\\.");
+            StringBuilder reverseDomainConcatenation = new StringBuilder();
+            for(int i = reverseDomain.length - 1; i >= 0; i--) {
+                reverseDomainConcatenation.append(reverseDomain[i]);
+
+                if(i != 0) {
+                    reverseDomainConcatenation.append(".");
+                }
+            }
+            this.reverseDomainName = reverseDomainConcatenation.toString();
+        }
+
+        @Override
+        public int compareTo(Domain that) {
+            return this.reverseDomainName.compareTo(that.reverseDomainName);
+        }
+    }
+
+    public static void main(String[] args) {
+        Exercise14_SortByReverseDomain sortByReverseDomain = new Exercise14_SortByReverseDomain();
+
+        /**
+         * Test case
+         *
+         * cs.princeton.edu
+         * www.google.com
+         * www.rene.argento
+         * www.somewebsite.gov
+         * cs.harvard.edu
+         */
+        String[] domainNames = StdIn.readAllLines();
+        Domain[] domains = new Domain[domainNames.length];
+
+        for(int i=0; i < domainNames.length; i++) {
+            Domain domain = sortByReverseDomain.new Domain(domainNames[i]);
+            domains[i] = domain;
+        }
+
+        Arrays.sort(domains);
+
+        for(Domain domain : domains) {
+            StdOut.println(domain.reverseDomainName);
+        }
+
+        StdOut.println();
+        StdOut.println("Expected:");
+        StdOut.println("argento.rene.www\n" +
+                "com.google.www\n" +
+                "edu.harvard.cs\n" +
+                "edu.princeton.cs\n" +
+                "gov.somewebsite.www");
+    }
+
+}
---
+++ src/chapter2/section5/Exercise16_UnbiasedElection.java
@@ -0,0 +1,70 @@
+package chapter2.section5;
+
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * Created by rene on 11/04/17.
+ */
+//Based on http://algs4.cs.princeton.edu/25applications/California.java.html
+public class Exercise16_UnbiasedElection {
+
+    private class CandidateOrder implements Comparator<String> {
+
+        private String order = "RWQOJMVAHBSGZXNTCIEKUPDYFL";
+
+        @Override
+        public int compare(String name1, String name2) {
+            int minimumLength = Math.min(name1.length(), name2.length());
+
+            for(int i=0; i < minimumLength; i++) {
+                char currentChar1 = name1.charAt(i);
+                char currentChar2 = name2.charAt(i);
+
+                if(order.indexOf(currentChar1) < order.indexOf(currentChar2)) {
+                    return -1;
+                } else if(order.indexOf(currentChar1) > order.indexOf(currentChar2)) {
+                    return 1;
+                }
+            }
+
+            return name1.length() - name2.length();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Exercise16_UnbiasedElection().california();
+    }
+
+    private void california() {
+
+        String candidate1 = "RENE";
+        String candidate2 = "LAST CANDIDATE";
+        String candidate3 = "VIP";
+        String candidate4 = "VIP 2";
+        String candidate5 = "ZORD";
+        String candidate6 = "OBAMA";
+        String candidate7 = "TRUMP";
+        String candidate8 = "TRYMP";
+
+        String[] candidates = {candidate1, candidate2, candidate3, candidate4, candidate5, candidate6, candidate7, candidate8};
+        Arrays.sort(candidates, new CandidateOrder());
+
+        for(String candidate : candidates) {
+            StdOut.println(candidate);
+        }
+
+        StdOut.println();
+        StdOut.println("Expected:");
+        StdOut.println("RENE\n" +
+                "OBAMA\n" +
+                "VIP\n" +
+                "VIP 2\n" +
+                "ZORD\n" +
+                "TRUMP\n" +
+                "TRYMP\n" +
+                "LAST CANDIDATE");
+    }
+}
