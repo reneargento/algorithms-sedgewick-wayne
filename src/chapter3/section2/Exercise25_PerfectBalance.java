2137909af7f0806a7bb63562e6b7de48ede6ae51
Rene Argento <rene.argento@gmail.com>
2017-06-04 18:28:18 -0300

    Added exercise 3.2.25 and 3.2.28

---
+++ src/chapter3/section2/Exercise25_PerfectBalance.java
@@ -0,0 +1,41 @@
+package chapter3.section2;
+
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Arrays;
+
+/**
+ * Created by rene on 03/06/17.
+ */
+public class Exercise25_PerfectBalance {
+
+    public static void main(String[] args) {
+        int[] keys = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+
+        //In the test we are using an already sorted array, but any array could be used for this program
+        Arrays.sort(keys);
+
+        BinarySearchTree<Integer, Integer> binarySearchTree = new BinarySearchTree<>();
+        Exercise25_PerfectBalance exercise25_perfectBalance = new Exercise25_PerfectBalance();
+        exercise25_perfectBalance.insertBalancedValues(keys, binarySearchTree);
+    }
+
+    private void insertBalancedValues(int[] values, BinarySearchTree<Integer, Integer> binarySearchTree) {
+        insertBalancedValues(values, binarySearchTree, 0, values.length - 1);
+    }
+
+    private void insertBalancedValues(int[] values, BinarySearchTree<Integer, Integer> binarySearchTree, int low, int high) {
+        if(low > high) {
+            return;
+        }
+
+        int middle = low + (high - low) / 2;
+
+        binarySearchTree.put(values[middle], values[middle]);
+        StdOut.print(values[middle] + " ");
+
+        insertBalancedValues(values, binarySearchTree, low, middle - 1);
+        insertBalancedValues(values, binarySearchTree, middle + 1, high);
+    }
+
+}
---
+++ src/chapter3/section2/Exercise28_SoftwareCaching.java
@@ -0,0 +1,436 @@
+package chapter3.section2;
+
+import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.NoSuchElementException;
+
+/**
+ * Created by rene on 03/06/17.
+ */
+public class Exercise28_SoftwareCaching {
+
+    private class BinarySearchTree<Key extends Comparable<Key>, Value>{
+
+        private class Node {
+            private Key key;
+            private Value value;
+
+            private Node left;
+            private Node right;
+
+            private int size; //# of nodes in subtree rooted here
+
+            public Node(Key key, Value value, int size) {
+                this.key = key;
+                this.value = value;
+                this.size = size;
+            }
+        }
+
+        private Node root;
+
+        private Node cacheItem;
+        private boolean isCacheValid;
+
+        public int size() {
+            return size(root);
+        }
+
+        private int size(Node node) {
+            if(node == null) {
+                return 0;
+            }
+
+            return node.size;
+        }
+
+        public Value get(Key key) {
+
+            if(isCacheValid && cacheItem != null && cacheItem.key == key) {
+                StdOut.println("Cache hit");
+                return cacheItem.value;
+            }
+
+            StdOut.println("Cache miss");
+
+            return get(root, key);
+        }
+
+        private Value get(Node node, Key key) {
+            if(node == null) {
+                return null;
+            }
+
+            int compare = key.compareTo(node.key);
+            if(compare < 0) {
+                return get(node.left, key);
+            } else if(compare > 0) {
+                return get(node.right, key);
+            } else {
+                isCacheValid = true;
+                cacheItem = node;
+
+                return node.value;
+            }
+        }
+
+        public void put(Key key, Value value) {
+
+            if(isCacheValid && cacheItem != null && cacheItem.key == key) {
+                cacheItem.value = value;
+
+                StdOut.println("Cache hit");
+                return;
+            }
+
+            StdOut.println("Cache miss");
+
+            root = put(root, key, value);
+        }
+
+        private Node put(Node node, Key key, Value value) {
+            if(node == null) {
+                return new Node(key, value, 1);
+            }
+
+            int compare = key.compareTo(node.key);
+
+            if(compare < 0) {
+                node.left = put(node.left, key, value);
+            } else if(compare > 0) {
+                node.right = put(node.right, key, value);
+            } else {
+                node.value = value;
+
+                isCacheValid = true;
+                cacheItem = node;
+            }
+
+            node.size = size(node.left) + 1 + size(node.right);
+            return node;
+        }
+
+        public Key min() {
+            if(root == null) {
+                throw new NoSuchElementException("Empty binary search tree");
+            }
+
+            return min(root).key;
+        }
+
+        private Node min(Node node) {
+            if(node.left == null) {
+
+                isCacheValid = true;
+                cacheItem = node;
+
+                return node;
+            }
+
+            return min(node.left);
+        }
+
+        public Key max() {
+            if(root == null) {
+                throw new NoSuchElementException("Empty binary search tree");
+            }
+
+            return max(root).key;
+        }
+
+        private Node max(Node node) {
+            if(node.right == null) {
+
+                isCacheValid = true;
+                cacheItem = node;
+
+                return node;
+            }
+
+            return max(node.right);
+        }
+
+        public Key floor(Key key) {
+            Node node = floor(root, key);
+            if(node == null) {
+                return null;
+            }
+
+            isCacheValid = true;
+            cacheItem = node;
+
+            return node.key;
+        }
+
+        private Node floor(Node node, Key key) {
+            if(node == null) {
+                return null;
+            }
+
+            int compare = key.compareTo(node.key);
+
+            if(compare == 0) {
+                return node;
+            } else if(compare < 0) {
+                return floor(node.left, key);
+            } else {
+                Node rightNode = floor(node.right, key);
+                if(rightNode != null) {
+                    return rightNode;
+                } else {
+                    return node;
+                }
+            }
+        }
+
+        public Key ceiling(Key key) {
+            Node node = ceiling(root, key);
+            if(node == null) {
+                return null;
+            }
+
+            isCacheValid = true;
+            cacheItem = node;
+
+            return node.key;
+        }
+
+        private Node ceiling(Node node, Key key) {
+            if(node == null) {
+                return null;
+            }
+
+            int compare = key.compareTo(node.key);
+
+            if(compare == 0) {
+                return node;
+            } else if(compare > 0) {
+                return ceiling(node.right, key);
+            } else {
+                Node leftNode = ceiling(node.left, key);
+                if(leftNode != null) {
+                    return leftNode;
+                } else {
+                    return node;
+                }
+            }
+        }
+
+        public Key select(int index) {
+            if(index >= size()) {
+                throw new IllegalArgumentException("Index is higher than tree size");
+            }
+
+            return select(root, index).key;
+        }
+
+        private Node select(Node node, int index) {
+            int leftSubtreeSize = size(node.left);
+
+            if(leftSubtreeSize == index) {
+                isCacheValid = true;
+                cacheItem = node;
+
+                return node;
+            } else if (leftSubtreeSize > index) {
+                return select(node.left, index);
+            } else {
+                return select(node.right, index - leftSubtreeSize - 1);
+            }
+        }
+
+        public int rank(Key key) {
+            return rank(root, key);
+        }
+
+        private int rank(Node node, Key key) {
+            if(node == null) {
+                return 0;
+            }
+
+            //Returns the number of keys less than node.key in the subtree rooted at node
+            int compare = key.compareTo(node.key);
+            if(compare < 0) {
+                return rank(node.left, key);
+            } else if(compare > 0) {
+                return size(node.left) + 1 + rank(node.right, key);
+            } else {
+                isCacheValid = true;
+                cacheItem = node;
+
+                return size(node.left);
+            }
+        }
+
+        public void deleteMin() {
+            root = deleteMin(root);
+        }
+
+        private Node deleteMin(Node node) {
+            if(node == null) {
+                return null;
+            }
+
+            if(node.left == null) {
+                if(isCacheValid && cacheItem == node) {
+                    invalidateCache();
+                }
+
+                return node.right;
+            }
+
+            node.left = deleteMin(node.left);
+            node.size = size(node.left) + 1 + size(node.right);
+            return node;
+        }
+
+        public void deleteMax() {
+            root = deleteMax(root);
+        }
+
+        private Node deleteMax(Node node) {
+            if(node == null) {
+                return null;
+            }
+
+            if(node.right == null) {
+                if(isCacheValid && cacheItem == node) {
+                    invalidateCache();
+                }
+
+                return node.left;
+            }
+
+            node.right = deleteMax(node.right);
+            node.size = size(node.left) + 1 + size(node.right);
+            return node;
+        }
+
+        public void delete(Key key) {
+            if(isCacheValid && cacheItem != null && cacheItem.key == key) {
+                invalidateCache();
+            }
+
+            root = delete(root, key);
+        }
+
+        private Node delete(Node node, Key key) {
+            if(node == null) {
+                return null;
+            }
+
+            int compare = key.compareTo(node.key);
+            if(compare < 0) {
+                node.left = delete(node.left, key);
+            } else if(compare > 0) {
+                node.right = delete(node.right, key);
+            } else {
+                if(node.left == null) {
+                    return node.right;
+                } else if (node.right == null) {
+                    return node.left;
+                } else {
+                    Node aux = node;
+                    node = min(aux.right);
+                    node.right = deleteMin(aux.right);
+                    node.left = aux.left;
+                }
+            }
+
+            node.size = size(node.left) + 1 + size(node.right);
+            return node;
+        }
+
+        public Iterable<Key> keys() {
+            return keys(min(), max());
+        }
+
+        public Iterable<Key> keys(Key low, Key high) {
+            Queue<Key> queue = new Queue<>();
+            keys(root, queue, low, high);
+            return queue;
+        }
+
+        private void keys(Node node, Queue<Key> queue, Key low, Key high) {
+            if(node == null) {
+                return;
+            }
+
+            int compareLow = low.compareTo(node.key);
+            int compareHigh = high.compareTo(node.key);
+
+            if(compareLow < 0) {
+                keys(node.left, queue, low, high);
+            }
+
+            if(compareLow <= 0 && compareHigh >= 0) {
+                queue.enqueue(node.key);
+            }
+
+            if(compareHigh > 0) {
+                keys(node.right, queue, low, high);
+            }
+        }
+
+        private void invalidateCache() {
+            isCacheValid = false;
+            cacheItem = null;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Exercise28_SoftwareCaching softwareCaching = new Exercise28_SoftwareCaching();
+
+        BinarySearchTree<Integer, String> binarySearchTree = softwareCaching.new BinarySearchTree<>();
+
+        for(int i=0; i < 10; i++) {
+            binarySearchTree.put(i, "Value " + i);
+        }
+
+        //Cache on get operation
+        StdOut.println("\nGet key 2:");
+        binarySearchTree.get(2);
+        StdOut.println("Expected: cache miss");
+
+        StdOut.println("\nGet key 2:");
+        binarySearchTree.get(2);
+        StdOut.println("Expected: cache hit");
+
+        StdOut.println("\nDelete key 4:");
+        binarySearchTree.delete(4);
+
+        StdOut.println("\nGet key 7:");
+        binarySearchTree.get(7);
+        StdOut.println("Expected: cache miss");
+
+        StdOut.println("\nGet key 7:");
+        binarySearchTree.get(7);
+        StdOut.println("Expected: cache hit");
+
+        StdOut.println("\nDelete key 7:");
+        binarySearchTree.delete(7);
+
+        binarySearchTree.min();
+        StdOut.println("\nGet key 0:");
+        binarySearchTree.get(0);
+        StdOut.println("Expected: cache hit");
+
+        StdOut.println("\nGet key 9:");
+        binarySearchTree.get(9);
+        StdOut.println("Expected: cache miss");
+
+        binarySearchTree.select(3);
+
+        StdOut.println("\nGet key 3:");
+        binarySearchTree.get(3);
+        StdOut.println("Expected: cache hit");
+
+        binarySearchTree.max();
+        StdOut.println("\nGet key 9:");
+        binarySearchTree.get(9);
+        StdOut.println("Expected: cache hit");
+    }
+
+}
