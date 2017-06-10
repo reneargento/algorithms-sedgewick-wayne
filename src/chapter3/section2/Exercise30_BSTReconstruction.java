3109843a6305058bc9fe1798c60f118f68fcd815
Rene Argento <rene.argento@gmail.com>
2017-06-09 23:05:39 -0300

    Added exercise 3.2.30 and 3.2.31

---
+++ src/chapter3/section2/Exercise30_BSTReconstruction.java
@@ -0,0 +1,127 @@
+package chapter3.section2;
+
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Created by rene on 04/06/17.
+ */
+@SuppressWarnings("unchecked")
+//Based on http://www.geeksforgeeks.org/construct-bst-from-given-preorder-traversa/
+public class Exercise30_BSTReconstruction {
+
+    public static void main(String[] args) {
+
+        /**
+         *     10
+         *  5      40
+         * 1 7   30  50
+         *    8
+         */
+
+        Exercise30_BSTReconstruction bstReconstruction = new Exercise30_BSTReconstruction();
+        int pre[] = new int[]{10, 5, 1, 7, 8, 40, 30, 50};
+
+        BinarySearchTree.Node root = bstReconstruction.constructBSTWithPreorder(pre);
+        bstReconstruction.printBSTInOrder(root);
+
+        StdOut.println();
+
+        int post[] = new int[] {1, 8, 7, 5, 30, 50, 40, 10};
+
+        BinarySearchTree.Node root2 = bstReconstruction.constructBSTWithPostorder(post);
+        bstReconstruction.printBSTInOrder(root2);
+    }
+
+    private int preIndex;
+    private int postIndex;
+
+    private BinarySearchTree.Node constructBSTWithPreorder(int[] pre) {
+        if(pre == null || pre.length == 0) {
+            return null;
+        }
+
+        preIndex = 0;
+
+        return constructBSTWithPreorder(pre, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    private BinarySearchTree.Node constructBSTWithPreorder(int[] pre, int min, int max) {
+        if(preIndex >= pre.length) {
+            return null;
+        }
+
+        BinarySearchTree.Node root = null;
+
+        //Check if the key is in range
+        if(min < pre[preIndex] && pre[preIndex] < max) {
+            root = new BinarySearchTree().new Node(pre[preIndex], pre[preIndex], 1);
+            preIndex++;
+
+            root.left = constructBSTWithPreorder(pre, min, pre[preIndex - 1]);
+            root.right = constructBSTWithPreorder(pre, pre[preIndex - 1], max);
+
+            //Update node's size
+            if(root.left != null) {
+                root.size += root.left.size;
+            }
+            if(root.right != null) {
+                root.size += root.right.size;
+            }
+        }
+
+        return root;
+    }
+
+    private BinarySearchTree.Node constructBSTWithPostorder(int[] post) {
+        if(post == null || post.length == 0) {
+            return null;
+        }
+
+        int[] reverseArray = new int[post.length];
+        for(int i=0; i < reverseArray.length; i++) {
+            reverseArray[i] = post[post.length - 1 - i];
+        }
+
+        postIndex = 0;
+
+        return constructBSTWithPostorder(reverseArray, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    private BinarySearchTree.Node constructBSTWithPostorder(int[] post, int min, int max) {
+        if(postIndex >= post.length) {
+            return null;
+        }
+
+        BinarySearchTree.Node root = null;
+
+        //Check if the key is in range
+        if(min < post[postIndex] && post[postIndex] < max) {
+            root = new BinarySearchTree().new Node(post[postIndex], post[postIndex], 1);
+            postIndex++;
+
+            root.right = constructBSTWithPostorder(post, post[postIndex - 1], max);
+            root.left = constructBSTWithPostorder(post, min, post[postIndex - 1]);
+
+            //Update node's size
+            if(root.left != null) {
+                root.size += root.left.size;
+            }
+            if(root.right != null) {
+                root.size += root.right.size;
+            }
+        }
+
+        return root;
+    }
+
+    private void printBSTInOrder(BinarySearchTree.Node node) {
+        if(node == null) {
+            return;
+        }
+
+        printBSTInOrder(node.left);
+        StdOut.print(node.key + " ");
+        printBSTInOrder(node.right);
+    }
+
+}
---
+++ src/chapter3/section2/Exercise31_Certification.java
@@ -0,0 +1,57 @@
+package chapter3.section2;
+
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Created by rene on 04/06/17.
+ */
+@SuppressWarnings("unchecked")
+public class Exercise31_Certification {
+
+    public static void main(String[] args) {
+        Exercise31_Certification certification = new Exercise31_Certification();
+
+        BinarySearchTree.Node root = new BinarySearchTree().new Node(10, "Value 10", 7);
+        root.left = new BinarySearchTree().new Node(5, "Value 5", 4);
+        root.left.left = new BinarySearchTree().new Node(2, "Value 2", 1);
+        root.left.right = new BinarySearchTree().new Node(7, "Value 7", 2);
+        root.left.right.right = new BinarySearchTree().new Node(9, "Value 9", 1);
+
+        root.right = new BinarySearchTree().new Node(14, "Value 14", 2);
+        root.right.left = new BinarySearchTree().new Node(11, "Value 11", 1);
+
+        StdOut.println(certification.isBST(root) + " Expected: true");
+
+        BinarySearchTree.Node root2 = new BinarySearchTree().new Node(20, "Value 1", 7);
+        root2.left = new BinarySearchTree().new Node(5, "Value 5", 4);
+        root2.left.left = new BinarySearchTree().new Node(2, "Value 2", 1);
+        root2.left.right = new BinarySearchTree().new Node(1, "Value 1", 2); //Not a BST
+        root2.left.right.right = new BinarySearchTree().new Node(9, "Value 9", 1);
+
+        root2.right = new BinarySearchTree().new Node(24, "Value 24", 2);
+        root2.right.left = new BinarySearchTree().new Node(21, "Value 21", 1);
+
+        StdOut.println(certification.isBST(root2) + " Expected: false");
+    }
+
+    private boolean isBST(BinarySearchTree.Node root) {
+        return isBST(root, null, null);
+    }
+
+    private boolean isBST(BinarySearchTree.Node node, Comparable low, Comparable high) {
+
+        if(node == null) {
+            return true;
+        }
+
+        if(low != null && low.compareTo(node.key) >= 0) {
+            return false;
+        }
+        if(high != null && high.compareTo(node.key) <= 0) {
+            return false;
+        }
+
+        return isBST(node.left, low, node.key) && isBST(node.right, node.key, high);
+    }
+
+}
