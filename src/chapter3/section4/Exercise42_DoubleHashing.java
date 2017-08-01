c08f5db2ced0479a6823a5d2b97559a08f8a2c03
Rene Argento <rene.argento@gmail.com>
2017-08-01 01:16:29 -0300

    Finished section 3.4

---
+++ src/chapter3/section4/Exercise42_DoubleHashing.java
@@ -0,0 +1,86 @@
+package chapter3.section4;
+
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.Stopwatch;
+
+/**
+ * Created by rene on 30/07/17.
+ */
+public class Exercise42_DoubleHashing {
+
+    private void doExperiment() {
+        LinearProbingHashTable<Integer, Integer> linearProbingHashTable = new LinearProbingHashTable<>(10);
+        DoubleHashingHashTable<Integer, Integer> doubleHashingHashTable = new DoubleHashingHashTable<>(10);
+
+        StdOut.printf("%12s %20s %20s\n", "Operation | ","Linear-probing HT time | ", "Double hashing HT time");
+
+        //Put tests
+        int[] randomKeysPut = new int[1000000];
+
+        for(int i = 0; i < randomKeysPut.length; i++) {
+            int randomKey = StdRandom.uniform(Integer.MAX_VALUE);
+            randomKeysPut[i] = randomKey;
+        }
+
+        Stopwatch stopwatch = new Stopwatch();
+        for(int i = 0; i < randomKeysPut.length; i++) {
+            linearProbingHashTable.put(randomKeysPut[i], randomKeysPut[i]);
+        }
+        double timeSpentOnPutLinearProbing = stopwatch.elapsedTime();
+
+        stopwatch = new Stopwatch();
+        for(int i = 0; i < randomKeysPut.length; i++) {
+            doubleHashingHashTable.put(randomKeysPut[i], randomKeysPut[i]);
+        }
+        double timeSpentOnPutDoubleHashing = stopwatch.elapsedTime();
+
+        printResults("Put", timeSpentOnPutLinearProbing, timeSpentOnPutDoubleHashing);
+
+        //Get tests
+        int[] randomKeysGet = new int[500000];
+
+        for(int i = 0; i < randomKeysGet.length; i++) {
+            int randomKey = StdRandom.uniform(Integer.MAX_VALUE);
+            randomKeysGet[i] = randomKey;
+        }
+
+        stopwatch = new Stopwatch();
+        for(int i = 0; i < randomKeysGet.length; i++) {
+            linearProbingHashTable.get(randomKeysGet[i]);
+        }
+        double timeSpentOnGetLinearProbing = stopwatch.elapsedTime();
+
+        stopwatch = new Stopwatch();
+        for(int i = 0; i < randomKeysGet.length; i++) {
+            doubleHashingHashTable.get(randomKeysGet[i]);
+        }
+        double timeSpentOnGetDoubleHashing = stopwatch.elapsedTime();
+
+        printResults("Get", timeSpentOnGetLinearProbing, timeSpentOnGetDoubleHashing);
+
+        //Delete tests
+        stopwatch = new Stopwatch();
+        for(int i = 0; i < randomKeysPut.length / 2; i++) {
+            linearProbingHashTable.delete(randomKeysPut[i]);
+        }
+        double timeSpentOnDeleteLinearProbing = stopwatch.elapsedTime();
+
+        stopwatch = new Stopwatch();
+        for(int i = 0; i < randomKeysPut.length / 2; i++) {
+            doubleHashingHashTable.delete(randomKeysPut[i]);
+        }
+        double timeSpentOnDeleteDoubleHashing = stopwatch.elapsedTime();
+
+        printResults("Delete", timeSpentOnDeleteLinearProbing, timeSpentOnDeleteDoubleHashing);
+    }
+
+    private void printResults(String operation, double linearProbingTime, double doubleHashingTime) {
+        StdOut.printf("%9s %25.2f %25.2f\n", operation, linearProbingTime, doubleHashingTime);
+    }
+
+    public static void main(String[] args) {
+        new Exercise42_DoubleHashing().doExperiment();
+    }
+
+}
---
+++ src/chapter3/section4/Exercise43_ParkingProblem.java
@@ -0,0 +1,80 @@
+package chapter3.section4;
+
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+/**
+ * Created by rene on 30/07/17.
+ */
+public class Exercise43_ParkingProblem {
+
+    private class LinearProbingHashTableFixedSize<Key, Value> extends LinearProbingHashTable<Key, Value> {
+
+        LinearProbingHashTableFixedSize(int size) {
+            super(size);
+        }
+
+        private int totalNumberOfCompares;
+
+        public void put(Key key, Value value) {
+            if (key == null) {
+                throw new IllegalArgumentException("Key cannot be null");
+            }
+
+            if(value == null) {
+                delete(key);
+            }
+
+            totalNumberOfCompares++;
+
+            int tableIndex;
+            for(tableIndex = hash(key); keys[tableIndex] != null; tableIndex = (tableIndex + 1) % size) {
+                totalNumberOfCompares++;
+
+                if(keys[tableIndex].equals(key)) {
+                    values[tableIndex] = value;
+                    return;
+                }
+            }
+
+            keys[tableIndex] = key;
+            values[tableIndex] = value;
+            keysSize++;
+        }
+    }
+
+    private void doExperiment() {
+        StdOut.printf("%12s %20s %20s\n", "Hash table size | ","Number of compares | ", "Expected number of compares");
+
+        int[] hashTableSizes = {1000, 10000, 100000, 1000000};
+        for(int hashTableSize : hashTableSizes) {
+            putKeysAndPrintResults(hashTableSize);
+        }
+    }
+
+    private void putKeysAndPrintResults(int hashTableSize) {
+
+        LinearProbingHashTableFixedSize<Integer, Integer> linearProbingHashTable = new LinearProbingHashTableFixedSize<>(hashTableSize);
+
+        for(int i = 0; i < hashTableSize; i++) {
+            int randomKey = StdRandom.uniform(Integer.MAX_VALUE);
+            linearProbingHashTable.put(randomKey, randomKey);
+        }
+
+        long totalNumberOfCompares = linearProbingHashTable.totalNumberOfCompares;
+
+        double constant = Math.sqrt(Math.PI / 2);
+        long expectedTotalNumberOfCompares = (long) (constant * Math.pow(hashTableSize, 1.5));
+
+        printResults(hashTableSize, totalNumberOfCompares, expectedTotalNumberOfCompares);
+    }
+
+    private void printResults(int hashTableSize, long totalNumberOfCompares, long expectedTotalNumberOfCompares) {
+        StdOut.printf("%15d %21d %30d\n", hashTableSize, totalNumberOfCompares, expectedTotalNumberOfCompares);
+    }
+
+    public static void main(String[] args) {
+        new Exercise43_ParkingProblem().doExperiment();
+    }
+
+}
