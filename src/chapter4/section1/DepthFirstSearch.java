6228858362779de26d4450420eccce79f00e4dd5
Rene Argento <rene.argento@gmail.com>
2017-09-12 01:34:20 -0300

    Started section 4.1

---
+++ src/chapter4/section1/DepthFirstSearch.java
@@ -0,0 +1,35 @@
+package chapter4.section1;
+
+/**
+ * Created by rene on 12/09/17.
+ */
+public class DepthFirstSearch {
+    
+    private boolean[] visited;
+    private int count;
+
+    public DepthFirstSearch(Graph graph, int sourceVertex) {
+        visited = new boolean[graph.vertices()];
+        dfs(graph, sourceVertex);
+    }
+
+    private void dfs(Graph graph, int vertex) {
+        visited[vertex] = true;
+        count++;
+
+        for(int neighbor : graph.adjacent(vertex)) {
+            if(!visited[neighbor]) {
+                dfs(graph, neighbor);
+            }
+        }
+    }
+
+    public boolean marked(int vertex) {
+        return visited[vertex];
+    }
+
+    public int count() {
+        return count;
+    }
+
+}
---
+++ src/chapter4/section1/Graph.java
@@ -0,0 +1,55 @@
+package chapter4.section1;
+
+import chapter1.section3.Bag;
+import edu.princeton.cs.algs4.In;
+
+/**
+ * Created by rene on 12/09/17.
+ */
+@SuppressWarnings("unchecked")
+public class Graph {
+
+    private final int vertices;
+    private int edges;
+    private Bag<Integer>[] adjacent;
+
+    public Graph(int vertices) {
+        this.vertices = vertices;
+        this.edges = 0;
+        adjacent = (Bag<Integer>[]) new Bag[vertices];
+
+        for(int i = 0; i < vertices; i++) {
+            adjacent[i] = new Bag<>();
+        }
+    }
+
+    public Graph(In in) {
+        this(in.readInt());
+        int edges = in.readInt();
+
+        for(int i = 0; i < edges; i++) {
+            int vertex1 = in.readInt();
+            int vertex2 = in.readInt();
+            addEdge(vertex1, vertex2);
+        }
+    }
+
+    public int vertices() {
+        return vertices;
+    }
+
+    public int edges() {
+        return edges;
+    }
+
+    public void addEdge(int vertex1, int vertex2) {
+        adjacent[vertex1].add(vertex2);
+        adjacent[vertex2].add(vertex1);
+        edges++;
+    }
+
+    public Iterable<Integer> adjacent(int vertex) {
+        return adjacent(vertex);
+    }
+
+}
