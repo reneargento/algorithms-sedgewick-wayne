f06bdf388e1df8c9b9ab2a5c0d276eb49f6f6126
Rene Argento <rene.argento@gmail.com>
2017-09-17 18:21:16 -0300

    Added exercises 4.1.4 and 4.1.5

---
+++ src/chapter4/section1/Exercise4.java
@@ -0,0 +1,42 @@
+package chapter4.section1;
+
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Created by rene on 16/09/17.
+ */
+public class Exercise4 {
+
+    public class GraphWithHasEdge extends Graph {
+
+        public GraphWithHasEdge(int vertices) {
+            super(vertices);
+        }
+
+        public boolean hasEdge(int vertex1, int vertex2) {
+            for(int neighbor: adjacent(vertex1)) {
+                if(neighbor == vertex2) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Exercise4 exercise4 = new Exercise4();
+        GraphWithHasEdge graphWithHasEdge = exercise4.new GraphWithHasEdge(5);
+
+        graphWithHasEdge.addEdge(0, 1);
+        graphWithHasEdge.addEdge(1, 4);
+        graphWithHasEdge.addEdge(2, 3);
+
+        StdOut.println("Has edge 0 - 1: " + graphWithHasEdge.hasEdge(0 ,1) + " Expected: true");
+        StdOut.println("Has edge 0 - 0: " + graphWithHasEdge.hasEdge(0 ,0) + " Expected: false");
+        StdOut.println("Has edge 1 - 0: " + graphWithHasEdge.hasEdge(1 ,0) + " Expected: true");
+        StdOut.println("Has edge 2 - 3: " + graphWithHasEdge.hasEdge(2 ,3) + " Expected: true");
+        StdOut.println("Has edge 2 - 4: " + graphWithHasEdge.hasEdge(2 ,4) + " Expected: false");
+    }
+
+}
---
+++ src/chapter4/section1/Exercise5.java
@@ -0,0 +1,57 @@
+package chapter4.section1;
+
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Created by rene on 16/09/17.
+ */
+public class Exercise5 {
+
+    public class GraphNoParallelEdgesOrSelfLoops extends Graph {
+
+        public GraphNoParallelEdgesOrSelfLoops(int vertices) {
+            super(vertices);
+        }
+
+        @Override
+        public void addEdge(int vertex1, int vertex2) {
+            //Self-loops and parallel edges not allowed
+            if(vertex1 == vertex2
+                    || hasEdge(vertex1, vertex2)) {
+                return;
+            }
+
+            super.addEdge(vertex1, vertex2);
+        }
+
+        private boolean hasEdge(int vertex1, int vertex2) {
+            for(int neighbor: adjacent(vertex1)) {
+                if(neighbor == vertex2) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Exercise5 exercise5 = new Exercise5();
+        GraphNoParallelEdgesOrSelfLoops graphNoParallelEdgesOrSelfLoops =
+                exercise5.new GraphNoParallelEdgesOrSelfLoops(5);
+
+        graphNoParallelEdgesOrSelfLoops.addEdge(0, 1);
+        graphNoParallelEdgesOrSelfLoops.addEdge(1, 4);
+        graphNoParallelEdgesOrSelfLoops.addEdge(2, 3);
+        StdOut.println("Number of edges: " + graphNoParallelEdgesOrSelfLoops.edges() + " Expected: 3");
+
+        graphNoParallelEdgesOrSelfLoops.addEdge(1, 0);
+        StdOut.println("Number of edges: " + graphNoParallelEdgesOrSelfLoops.edges() + " Expected: 3");
+
+        graphNoParallelEdgesOrSelfLoops.addEdge(2, 4);
+        StdOut.println("Number of edges: " + graphNoParallelEdgesOrSelfLoops.edges() + " Expected: 4");
+        graphNoParallelEdgesOrSelfLoops.addEdge(4, 4);
+        StdOut.println("Number of edges: " + graphNoParallelEdgesOrSelfLoops.edges() + " Expected: 4");
+    }
+
+}
