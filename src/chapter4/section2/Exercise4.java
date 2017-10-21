cd638c4e4c12eedec49eaf7b89ecb169358d2129
Rene Argento <rene.argento@gmail.com>
2017-10-21 00:26:45 -0200

    Added exercise 4.2.4

---
+++ src/chapter4/section2/Exercise4.java
@@ -0,0 +1,42 @@
+package chapter4.section2;
+
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Created by rene on 19/10/17.
+ */
+public class Exercise4 {
+
+    public class DigraphWithHasEdge extends Digraph {
+
+        public DigraphWithHasEdge(int vertices) {
+            super(vertices);
+        }
+
+        public boolean hasEdge(int vertex1, int vertex2) {
+            for(int neighbor: adjacent(vertex1)) {
+                if(neighbor == vertex2) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Exercise4 exercise4 = new Exercise4();
+        DigraphWithHasEdge digraphWithHasEdge = exercise4.new DigraphWithHasEdge(5);
+
+        digraphWithHasEdge.addEdge(0, 1);
+        digraphWithHasEdge.addEdge(1, 4);
+        digraphWithHasEdge.addEdge(2, 3);
+
+        StdOut.println("Has edge 0 -> 1: " + digraphWithHasEdge.hasEdge(0 ,1) + " Expected: true");
+        StdOut.println("Has edge 0 -> 0: " + digraphWithHasEdge.hasEdge(0 ,0) + " Expected: false");
+        StdOut.println("Has edge 1 -> 0: " + digraphWithHasEdge.hasEdge(1 ,0) + " Expected: false");
+        StdOut.println("Has edge 2 -> 3: " + digraphWithHasEdge.hasEdge(2 ,3) + " Expected: true");
+        StdOut.println("Has edge 2 -> 4: " + digraphWithHasEdge.hasEdge(2 ,4) + " Expected: false");
+    }
+
+}
