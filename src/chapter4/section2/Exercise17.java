9b4b64a61d4e6d4c9ac831a9fbe7072d42d33c88
Rene Argento <rene.argento@gmail.com>
2017-10-26 04:17:07 -0200

    Added exercise 4.2.17

---
+++ src/chapter4/section2/Exercise17.java
@@ -0,0 +1,22 @@
+package chapter4.section2;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+import util.Constants;
+
+/**
+ * Created by rene on 22/10/17.
+ */
+//The digraph on page 591 is located at https://algs4.cs.princeton.edu/42digraph/mediumDG.txt
+public class Exercise17 {
+
+    public static void main(String[] args) {
+        String filePath = Constants.FILES_PATH + Constants.MEDIUM_DIGRAPH_FILE;
+
+        Digraph digraph = new Digraph(new In(filePath));
+        KosarajuSharirSCC kosarajuSharirSCC = new KosarajuSharirSCC(digraph);
+
+        StdOut.println("Number of strong components: " + kosarajuSharirSCC.count());
+    }
+
+}
