85b53dae21772a3e433f2127a24f00087091515d
Rene Argento <rene.argento@gmail.com>
2017-11-04 00:34:08 -0200

    Added exercises 4.2.39 and 4.2.40

---
+++ src/chapter4/section2/Exercise39_QueueBasedTopologicalSort.java
@@ -0,0 +1,116 @@
+package chapter4.section2;
+
+import chapter1.section3.Queue;
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Created by rene on 27/10/17.
+ */
+public class Exercise39_QueueBasedTopologicalSort {
+
+    private int[] ranks;
+
+    // O(V + E)
+    public int[] topologicalSort(Digraph digraph) {
+
+        DirectedCycle directedCycle = new DirectedCycle(digraph);
+        if(directedCycle.hasCycle()) {
+            // Digraph is not a DAG so no topological order exists
+            return null;
+        }
+
+        int[] indegree = new int[digraph.vertices()];
+        Queue<Integer> sources = new Queue<>();
+
+        ranks = new int[digraph.vertices()];
+
+        int[] topologicalSort = new int[digraph.vertices()];
+        int topologicalSortIndex = 0;
+
+        for(int vertex = 0; vertex < digraph.vertices(); vertex++) {
+            for(int neighbor : digraph.adjacent(vertex)) {
+                indegree[neighbor]++;
+            }
+        }
+
+        for(int vertex = 0; vertex < digraph.vertices(); vertex++) {
+            if(indegree[vertex] == 0) {
+                sources.enqueue(vertex);
+            }
+        }
+
+        while (!sources.isEmpty()) {
+            int currentSource = sources.dequeue();
+
+            ranks[currentSource] = topologicalSortIndex;
+            topologicalSort[topologicalSortIndex++] = currentSource;
+
+            for(int neighbor : digraph.adjacent(currentSource)) {
+                indegree[neighbor]--;
+
+                if(indegree[neighbor] == 0) {
+                    sources.enqueue(neighbor);
+                }
+            }
+        }
+
+        return topologicalSort;
+    }
+
+    public static void main(String[] args) {
+        Exercise39_QueueBasedTopologicalSort queueBasedTopologicalSort = new Exercise39_QueueBasedTopologicalSort();
+
+        Digraph digraph1 = new Digraph(5);
+        digraph1.addEdge(0, 1);
+        digraph1.addEdge(0, 2);
+        digraph1.addEdge(1, 2);
+        digraph1.addEdge(2, 3);
+        digraph1.addEdge(3, 4);
+
+        int[] topologicalOrder1 = queueBasedTopologicalSort.topologicalSort(digraph1);
+
+        StdOut.println("Topological order 1: ");
+
+        for(int vertex : topologicalOrder1) {
+            StdOut.print(vertex + " ");
+        }
+        StdOut.println("\nExpected: 0 1 2 3 4");
+
+        Digraph digraph2 = new Digraph(6);
+        digraph2.addEdge(0, 1);
+        digraph2.addEdge(1, 2);
+        digraph2.addEdge(3, 4);
+        digraph2.addEdge(4, 5);
+
+        int[] topologicalOrder2 = queueBasedTopologicalSort.topologicalSort(digraph2);
+
+        StdOut.println("\nTopological order 2: ");
+
+        for(int vertex : topologicalOrder2) {
+            StdOut.print(vertex + " ");
+        }
+        StdOut.println("\nExpected: 0 3 1 4 2 5 ");
+
+        Digraph digraph3 = new Digraph(9);
+        digraph3.addEdge(0, 1);
+        digraph3.addEdge(1, 2);
+        digraph3.addEdge(1, 3);
+
+        digraph3.addEdge(4, 5);
+        digraph3.addEdge(5, 6);
+        digraph3.addEdge(6, 8);
+        digraph3.addEdge(6, 7);
+        digraph3.addEdge(7, 2);
+        digraph3.addEdge(8, 3);
+
+        int[] topologicalOrder3 = queueBasedTopologicalSort.topologicalSort(digraph3);
+
+        StdOut.println("\nTopological order 3: ");
+
+        for(int vertex : topologicalOrder3) {
+            StdOut.print(vertex + " ");
+        }
+        StdOut.println("\nExpected: 0 4 1 5 6 7 8 2 3");
+    }
+
+}
---
+++ src/chapter4/section2/Exercise40_ShortestDirectedCycle.java
@@ -0,0 +1,137 @@
+package chapter4.section2;
+
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by rene on 28/10/17.
+ */
+public class Exercise40_ShortestDirectedCycle {
+
+    // O(V * (V + E)) -> O(V * E)
+    public List<Integer> getShortestDirectedCycle(Digraph digraph) {
+
+        int shortestCycleLength = Integer.MAX_VALUE;
+        List<Integer> cycle = null;
+
+        Digraph reverseDigraph = digraph.reverse();
+
+        // For each edge u->v check if v has a path to u
+        // If there is a path, it is part of a cycle. Then, check if it has the shortest length
+        for(int vertex = 0; vertex < digraph.vertices(); vertex++) {
+            BreadthFirstDirectedPaths breadthFirstDirectedPaths = new BreadthFirstDirectedPaths(digraph, vertex);
+
+            for(int neighbor : reverseDigraph.adjacent(vertex)) {
+                if(breadthFirstDirectedPaths.hasPathTo(neighbor)) {
+                    int cycleLength = breadthFirstDirectedPaths.distTo(neighbor) + 1;
+
+                    if(cycleLength < shortestCycleLength) {
+                        shortestCycleLength = cycleLength;
+                        cycle = new ArrayList<>();
+
+                        for(int vertexInCycle : breadthFirstDirectedPaths.pathTo(neighbor)) {
+                            cycle.add(vertexInCycle);
+                        }
+                        cycle.add(vertex);
+                    }
+                }
+            }
+        }
+
+        if(cycle == null) {
+            //There is no cycle in the graph
+            return null;
+        }
+
+        return cycle;
+    }
+
+    public static void main(String[] args) {
+        Exercise40_ShortestDirectedCycle shortestDirectedCycle = new Exercise40_ShortestDirectedCycle();
+
+        Digraph digraph1 = new Digraph(5);
+        digraph1.addEdge(0, 1);
+        digraph1.addEdge(1, 2);
+        digraph1.addEdge(2, 3);
+        digraph1.addEdge(3, 4);
+        digraph1.addEdge(4, 0);
+
+        List<Integer> directedCycle1 = shortestDirectedCycle.getShortestDirectedCycle(digraph1);
+
+        if(directedCycle1 != null) {
+            StdOut.println("Shortest cycle 1 length: " + (directedCycle1.size() - 1));
+            for (int vertex : directedCycle1) {
+                StdOut.print(vertex + " ");
+            }
+        } else {
+            StdOut.println("The digraph is a DAG");
+        }
+        StdOut.println("\nExpected: 0 1 2 3 4 0\n");
+
+        Digraph digraph2 = new Digraph(8);
+        digraph2.addEdge(0, 1);
+        digraph2.addEdge(1, 2);
+        digraph2.addEdge(2, 3);
+        digraph2.addEdge(3, 4);
+        digraph2.addEdge(4, 0);
+        digraph2.addEdge(5, 6);
+        digraph2.addEdge(6, 7);
+        digraph2.addEdge(7, 5);
+
+        List<Integer> directedCycle2 = shortestDirectedCycle.getShortestDirectedCycle(digraph2);
+
+        if(directedCycle2 != null) {
+            StdOut.println("Shortest cycle 2 length: " + (directedCycle2.size() - 1));
+            for (int vertex : directedCycle2) {
+                StdOut.print(vertex + " ");
+            }
+        } else {
+            StdOut.println("The digraph is a DAG");
+        }
+        StdOut.println("\nExpected: 5 6 7 5\n");
+
+        Digraph digraph3 = new Digraph(8);
+        digraph3.addEdge(0, 1);
+        digraph3.addEdge(1, 2);
+        digraph3.addEdge(2, 3);
+        digraph3.addEdge(3, 4);
+        digraph3.addEdge(4, 3);
+        digraph3.addEdge(4, 0);
+        digraph3.addEdge(5, 6);
+        digraph3.addEdge(6, 7);
+        digraph3.addEdge(7, 5);
+
+        List<Integer> directedCycle3 = shortestDirectedCycle.getShortestDirectedCycle(digraph3);
+
+        if(directedCycle3 != null) {
+            StdOut.println("Shortest cycle 3 length: " + (directedCycle3.size() - 1));
+            for (int vertex : directedCycle3) {
+                StdOut.print(vertex + " ");
+            }
+        } else {
+            StdOut.println("The digraph is a DAG");
+        }
+        StdOut.println("\nExpected: 3 4 3\n");
+
+        Digraph digraph4 = new Digraph(5);
+        digraph4.addEdge(0, 1);
+        digraph4.addEdge(1, 2);
+        digraph4.addEdge(3, 4);
+        digraph4.addEdge(4, 0);
+
+        List<Integer> directedCycle4 = shortestDirectedCycle.getShortestDirectedCycle(digraph4);
+
+        if(directedCycle4 != null) {
+            StdOut.println("Shortest cycle 4 length: " + (directedCycle4.size() - 1));
+            for(int vertex : directedCycle4) {
+                StdOut.print(vertex + " ");
+            }
+        } else {
+            StdOut.println("The digraph is a DAG");
+        }
+        StdOut.println("Expected: The digraph is a DAG");
+    }
+
+}
