a1c12debf2a49e46b409f6090afbb57490327eb4
Rene Argento <rene.argento@gmail.com>
2017-11-05 02:25:26 -0200

    Added exercises 4.2.41 and 4.2.42

---
+++ src/chapter4/section2/Exercise41_OddLengthDirectedCycle.java
@@ -0,0 +1,115 @@
+package chapter4.section2;
+
+import chapter4.section1.OptimizedGraph;
+import chapter4.section1.TwoColor;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by rene on 28/10/17.
+ */
+@SuppressWarnings("unchecked")
+public class Exercise41_OddLengthDirectedCycle {
+
+    // A digraph G has an odd-length directed cycle if and only if one (or more) of its strong components
+    // (when treated as an undirected graph) is nonbipartite
+
+    // Proof
+    // If the digraph G has an odd-length directed cycle, then this cycle will be entirely contained in one of the
+    // strong components. When the strong component is treated as an undirected graph, the odd-length directed cycle
+    // becomes an odd-length cycle.
+    // An undirected graph is bipartite if and only if it has no odd-length cycle.
+
+    // Suppose a strong component of G is nonbipartite (when treated as an undirected graph). This means that there is
+    // an odd-length cycle C in the strong component, ignoring direction.
+    // If C is a directed cycle, then we are done.
+    // Otherwise, if and edge v->w is pointing in the "wrong" direction, we can replace it with an odd-length path that
+    // is pointing in the opposite direction (which preserves the parity of the number of edges in the cycle).
+    // To see how, note that there exists a directed path P from w to v because v and w are in the same strong component.
+    // If P has odd length, then we replace edge v->w by P; if P has even length, then this path P combined with
+    // v->w is an odd-length cycle.
+    // O(V + E)
+    public boolean hasOddLengthDirectedCycle(Digraph digraph) {
+        KosarajuSharirSCC kosarajuSharirSCC = new KosarajuSharirSCC(digraph);
+
+        Set<Integer>[] strongComponents = (Set<Integer>[]) new HashSet[kosarajuSharirSCC.count()];
+
+        for(int scc = 0; scc < strongComponents.length; scc++) {
+            strongComponents[scc] = new HashSet<>();
+        }
+
+        for(int vertex = 0; vertex < digraph.vertices(); vertex++) {
+            int strongComponentId = kosarajuSharirSCC.id(vertex);
+            strongComponents[strongComponentId].add(vertex);
+        }
+
+        for(int scc = 0; scc < strongComponents.length; scc++) {
+            // Uses optimized graph to make the algorithm O(V + E) instead of O(V^2)
+            // (No need to initialize all adjacency lists for the subGraphs)
+            OptimizedGraph subGraph = new OptimizedGraph(digraph.vertices());
+
+            for(int vertex : strongComponents[scc]) {
+                for(int neighbor : digraph.adjacent(vertex)) {
+                    if(strongComponents[scc].contains(neighbor)) {
+                        subGraph.addEdge(vertex, neighbor);
+                    }
+                }
+            }
+
+            TwoColor twoColor = new TwoColor(subGraph);
+            if(!twoColor.isBipartite()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Exercise41_OddLengthDirectedCycle oddLengthDirectedCycle = new Exercise41_OddLengthDirectedCycle();
+
+        Digraph digraph1 = new Digraph(4);
+        digraph1.addEdge(0, 1);
+        digraph1.addEdge(1, 2);
+        digraph1.addEdge(2, 3);
+        digraph1.addEdge(3, 0);
+
+        StdOut.println("Has odd length directed cycle: " + oddLengthDirectedCycle.hasOddLengthDirectedCycle(digraph1)
+                + " Expected: false");
+
+        Digraph digraph2 = new Digraph(5);
+        digraph2.addEdge(0, 1);
+        digraph2.addEdge(1, 2);
+        digraph2.addEdge(2, 0);
+        digraph2.addEdge(3, 4);
+
+        StdOut.println("Has odd length directed cycle: " + oddLengthDirectedCycle.hasOddLengthDirectedCycle(digraph2)
+                + " Expected: true");
+
+        Digraph digraph3 = new Digraph(10);
+        digraph3.addEdge(0, 1);
+        digraph3.addEdge(1, 2);
+        digraph3.addEdge(3, 4);
+        digraph3.addEdge(4, 6);
+        digraph3.addEdge(6, 8);
+        digraph3.addEdge(8, 5);
+        digraph3.addEdge(5, 9);
+        digraph3.addEdge(9, 4);
+        digraph3.addEdge(7, 0);
+
+        StdOut.println("Has odd length directed cycle: " + oddLengthDirectedCycle.hasOddLengthDirectedCycle(digraph3)
+                + " Expected: true");
+
+        Digraph digraph4 = new Digraph(5);
+        digraph4.addEdge(0, 1);
+        digraph4.addEdge(1, 0);
+        digraph4.addEdge(2, 3);
+        digraph4.addEdge(3, 4);
+
+        StdOut.println("Has odd length directed cycle: " + oddLengthDirectedCycle.hasOddLengthDirectedCycle(digraph4)
+                + " Expected: false");
+    }
+
+}
---
+++ src/chapter4/section2/Exercise42_ReachableVertexInDAG.java
@@ -0,0 +1,121 @@
+package chapter4.section2;
+
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Created by rene on 29/10/17.
+ */
+public class Exercise42_ReachableVertexInDAG {
+
+    // If a vertex is reachable from every other vertex in a DAG, it must be the last vertex in the topological order
+    public boolean hasVertexReachableFromEveryOtherVertex(Digraph digraph) {
+        // 0- Check if it is a DAG
+        DirectedCycle directedCycle = new DirectedCycle(digraph);
+        if(directedCycle.hasCycle()) {
+            throw new IllegalArgumentException("Digraph is not a DAG");
+        }
+
+        // 1- Get topological order
+        Topological topological = new Topological(digraph);
+
+        int[] topologicalOrderArray = new int[digraph.vertices()];
+        int topologicalOrderArrayIndex = 0;
+
+        for(int vertex : topological.order()) {
+            topologicalOrderArray[topologicalOrderArrayIndex++] = vertex;
+        }
+
+        int possibleReachableVertex = topologicalOrderArray[topologicalOrderArray.length - 1];
+
+        // 2- Invert graph and do a dfs from the last vertex in the topological order
+        Digraph reverseDigraph = digraph.reverse();
+        boolean[] visited = new boolean[digraph.vertices()];
+
+        dfs(reverseDigraph, possibleReachableVertex, visited);
+
+        // 3- Check if it reaches all other vertices
+        for(int vertex = 0; vertex < visited.length; vertex++) {
+            if(!visited[vertex]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void dfs(Digraph digraph, int vertex, boolean[] visited) {
+        visited[vertex] = true;
+
+        for(int neighbor : digraph.adjacent(vertex)) {
+            dfs(digraph, neighbor, visited);
+        }
+    }
+
+    // Another method: if there is only one vertex with outdegree == 0 in the DAG,
+    // then this is the vertex reachable from every other vertex
+    public boolean hasVertexReachableFromEveryOtherVertex2(Digraph digraph) {
+        // 0- Check if it is a DAG
+        DirectedCycle directedCycle = new DirectedCycle(digraph);
+        if(directedCycle.hasCycle()) {
+            throw new IllegalArgumentException("Digraph is not a DAG");
+        }
+
+        // 1- Check if there is only one sink
+        int sinks = 0;
+
+        for(int vertex = 0; vertex < digraph.vertices(); vertex++) {
+            if(digraph.outdegree(vertex) == 0) {
+                sinks++;
+            }
+        }
+
+        return sinks == 1;
+    }
+
+    public static void main(String[] args) {
+        Exercise42_ReachableVertexInDAG reachableVertexInDAG = new Exercise42_ReachableVertexInDAG();
+
+        Digraph digraph1 = new Digraph(4);
+        digraph1.addEdge(0, 1);
+        digraph1.addEdge(1, 2);
+        digraph1.addEdge(2, 3);
+
+        StdOut.println("Reachable 1: " + reachableVertexInDAG.hasVertexReachableFromEveryOtherVertex(digraph1)
+                + " Expected: true");
+        StdOut.println("Reachable 1: " + reachableVertexInDAG.hasVertexReachableFromEveryOtherVertex2(digraph1)
+                + " Expected: true");
+
+        Digraph digraph2 = new Digraph(4);
+        digraph2.addEdge(0, 1);
+        digraph2.addEdge(2, 3);
+
+        StdOut.println("Reachable 2: " + reachableVertexInDAG.hasVertexReachableFromEveryOtherVertex(digraph2)
+                + " Expected: false");
+        StdOut.println("Reachable 2: " + reachableVertexInDAG.hasVertexReachableFromEveryOtherVertex2(digraph2)
+                + " Expected: false");
+
+        Digraph digraph3 = new Digraph(5);
+        digraph3.addEdge(0, 1);
+        digraph3.addEdge(0, 2);
+        digraph3.addEdge(4, 3);
+        digraph3.addEdge(3, 1);
+
+        StdOut.println("Reachable 3: " + reachableVertexInDAG.hasVertexReachableFromEveryOtherVertex(digraph3)
+                + " Expected: false");
+        StdOut.println("Reachable 3: " + reachableVertexInDAG.hasVertexReachableFromEveryOtherVertex2(digraph3)
+                + " Expected: false");
+
+        Digraph digraph4 = new Digraph(5);
+        digraph4.addEdge(0, 1);
+        digraph4.addEdge(0, 2);
+        digraph4.addEdge(2, 1);
+        digraph4.addEdge(4, 3);
+        digraph4.addEdge(3, 1);
+
+        StdOut.println("Reachable 4: " + reachableVertexInDAG.hasVertexReachableFromEveryOtherVertex(digraph4)
+                + " Expected: true");
+        StdOut.println("Reachable 4: " + reachableVertexInDAG.hasVertexReachableFromEveryOtherVertex2(digraph4)
+                + " Expected: true");
+    }
+
+}
