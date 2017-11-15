bc318bef9948da458c23b07e0abff3149cbe1480
Rene Argento <rene.argento@gmail.com>
2017-11-15 17:51:03 -0200

    Added exercise 4.3.25

---
+++ src/chapter1/section3/DoublyLinkedListCircular.java
@@ -0,0 +1,401 @@
+package chapter1.section3;
+
+import java.util.Iterator;
+
+/**
+ * Created by rene on 14/11/17.
+ */
+public class DoublyLinkedListCircular<Item> implements Iterable<Item> {
+
+    public class DoubleNode {
+        public Item item;
+        DoubleNode previous;
+        DoubleNode next;
+    }
+
+    private int size;
+    private DoubleNode first;
+    private DoubleNode last;
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public DoubleNode first() {
+        if(size == 0) {
+            return null;
+        }
+
+        return first;
+    }
+
+    public DoubleNode last() {
+        if(size == 0) {
+            return null;
+        }
+
+        return last;
+    }
+
+    public Item get(int index) {
+        if(index >= size) {
+            throw new IllegalArgumentException("Index must be lower than size");
+        }
+
+        DoubleNode current = first;
+        int count = 0;
+
+        while (count != index) {
+            current = current.next;
+            count++;
+        }
+
+        return current.item;
+    }
+
+    public void insertNodeAtTheBeginning(DoubleNode doubleNode) {
+        DoubleNode oldFirst = first;
+
+        first = doubleNode;
+        first.next = oldFirst;
+
+        if (oldFirst != null) {
+            first.previous = oldFirst.previous;
+            oldFirst.previous = first;
+        }
+
+        //If the list was empty before adding the new item:
+        if (last == null) {
+            last = first;
+        } else {
+            last.next = first;
+        }
+
+        size++;
+    }
+
+    public void insertNodeAtTheEnd(DoubleNode doubleNode) {
+        DoubleNode oldLast = last;
+
+        last = doubleNode;
+        last.previous = oldLast;
+
+        if (oldLast != null) {
+            last.next = oldLast.next;
+            oldLast.next = last;
+        }
+
+        //If the list was empty before adding the new item:
+        if (first == null) {
+            first = last;
+        } else {
+            first.previous = last;
+        }
+
+        size++;
+    }
+
+    public void insertAtTheBeginning(Item item) {
+        DoubleNode oldFirst = first;
+
+        first = new DoubleNode();
+        first.item = item;
+        first.next = oldFirst;
+
+        if (oldFirst != null) {
+            first.previous = oldFirst.previous;
+            oldFirst.previous = first;
+        }
+
+        //If the list was empty before adding the new item:
+        if (last == null) {
+            last = first;
+        } else {
+            last.next = first;
+        }
+
+        size++;
+    }
+
+    public void insertAtTheEnd(Item item) {
+        DoubleNode oldLast = last;
+
+        last = new DoubleNode();
+        last.item = item;
+        last.previous = oldLast;
+
+        if (oldLast != null) {
+            last.next = oldLast.next;
+            oldLast.next = last;
+        }
+
+        //If the list was empty before adding the new item:
+        if (first == null) {
+            first = last;
+        } else {
+            first.previous = last;
+        }
+
+        size++;
+    }
+
+    public void insertBeforeNode(Item beforeItem, Item item) {
+        if (isEmpty()) {
+            return;
+        }
+
+        DoubleNode currentNode;
+
+        for(currentNode = first; currentNode != null; currentNode = currentNode.next) {
+            if (currentNode.item.equals(beforeItem)) {
+                break;
+            }
+        }
+
+        if (currentNode != null) {
+            DoubleNode newNode = new DoubleNode();
+            newNode.item = item;
+
+            DoubleNode previousNode = currentNode.previous;
+            currentNode.previous = newNode;
+            newNode.next = currentNode;
+            newNode.previous = previousNode;
+
+            if (newNode.previous == null) {
+                //This is the first node
+                first = newNode;
+            } else {
+                newNode.previous.next = newNode;
+            }
+
+            size++;
+        }
+    }
+
+    public void insertAfterNode(Item afterNode, Item item) {
+        if(isEmpty()) {
+            return;
+        }
+
+        DoubleNode currentNode;
+
+        for(currentNode = first; currentNode != null; currentNode = currentNode.next) {
+            if (currentNode.item.equals(afterNode)) {
+                break;
+            }
+        }
+
+        if(currentNode != null) {
+            DoubleNode newNode = new DoubleNode();
+            newNode.item = item;
+
+            DoubleNode nextNode = currentNode.next;
+            currentNode.next = newNode;
+            newNode.previous = currentNode;
+            newNode.next = nextNode;
+
+            if(newNode.next == null) {
+                //This is the last node
+                last = newNode;
+            } else {
+                newNode.next.previous = newNode;
+            }
+
+            size++;
+        }
+    }
+
+    public void insertLinkedListAtTheEnd(DoublyLinkedListCircular<Item> doublyLinkedListCircular) {
+        DoubleNode oldLast = last;
+
+        last = doublyLinkedListCircular.last();
+        doublyLinkedListCircular.first().previous = oldLast;
+
+        if (oldLast != null) {
+            doublyLinkedListCircular.last().next = oldLast.next;
+            oldLast.next = doublyLinkedListCircular.first();
+        }
+
+        //If the list was empty before adding the new item:
+        if (first == null) {
+            first = doublyLinkedListCircular.first();
+        } else {
+            first.previous = doublyLinkedListCircular.last();
+        }
+
+        size += doublyLinkedListCircular.size;
+    }
+
+    public Item removeFromTheBeginning() {
+        if (isEmpty()){
+            return null;
+        }
+
+        Item item = first.item;
+
+        if(first.next != null) {
+            first.next.previous = first.previous;
+        } else { // There is only 1 element in the list
+            last = null;
+        }
+
+        if(first.previous != null) {
+            if(size > 2) {
+                first.previous.next = first.next;
+            } else {
+                first.previous.next = null;
+            }
+        }
+
+        first = first.next;
+
+        size--;
+
+        return item;
+    }
+
+    public Item removeFromTheEnd(){
+        if (isEmpty()) {
+            return null;
+        }
+
+        Item item = last.item;
+
+        if(last.previous != null) {
+            if(size > 2) {
+                last.previous.next = last.next;
+            } else {
+                last.previous.next = null;
+            }
+        } else { // There is only 1 element in the list
+            first = null;
+        }
+
+        if(last.next != null) {
+            if(size > 2) {
+                last.next.previous = last.previous;
+            } else {
+                last.next.previous = null;
+            }
+        }
+
+        last = last.previous;
+
+        size--;
+
+        return item;
+    }
+
+    public void removeItem(Item item) {
+        if(isEmpty()) {
+            return;
+        }
+
+        DoubleNode currentNode = first;
+
+        while (currentNode != null) {
+            if(currentNode.item.equals(item)) {
+                removeItemWithNode(currentNode);
+                break;
+            }
+
+            currentNode = currentNode.next;
+        }
+    }
+
+    public void removeItemWithNode(DoubleNode doubleNode) {
+        if(doubleNode == null) {
+            throw new IllegalArgumentException("Node cannot be null");
+        }
+
+        if(isEmpty()) {
+            return;
+        }
+
+        DoubleNode previousNode = doubleNode.previous;
+        DoubleNode nextNode = doubleNode.next;
+
+        if (previousNode != null) {
+            previousNode.next = nextNode;
+        }
+        if (nextNode != null) {
+            nextNode.previous = previousNode;
+        }
+
+        if(doubleNode == first) {
+            first = nextNode;
+        }
+        if (doubleNode == last) {
+            last = previousNode;
+        }
+
+        size--;
+    }
+
+    public Item removeItemWithIndex(int nodeIndex) {
+        if (nodeIndex <= 0 || nodeIndex >= size()){
+            throw new IllegalArgumentException("Index must be higher than 0 and lower than size");
+        }
+
+        if(isEmpty()) {
+            return null;
+        }
+
+        boolean startFromTheBeginning = nodeIndex <= size() / 2;
+
+        int index = startFromTheBeginning ? 0 : size() - 1;
+
+        DoubleNode currentNode = startFromTheBeginning? first : last;
+
+        while (currentNode != null) {
+            if(nodeIndex == index) {
+                break;
+            }
+
+            if(startFromTheBeginning) {
+                index++;
+            } else{
+                index--;
+            }
+
+            currentNode = startFromTheBeginning ? currentNode.next : currentNode.previous;
+        }
+
+        @SuppressWarnings("ConstantConditions") //if we got here, the node exists
+        Item item = currentNode.item;
+        removeItemWithNode(currentNode);
+
+        return item;
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        return new DoublyLinkedListCircularIterator();
+    }
+
+    private class DoublyLinkedListCircularIterator implements Iterator<Item> {
+
+        int index = 0;
+        DoubleNode currentNode = first;
+
+        @Override
+        public boolean hasNext() {
+            return index < size();
+        }
+
+        @Override
+        public Item next() {
+            Item item = currentNode.item;
+            currentNode = currentNode.next;
+
+            index++;
+
+            return item;
+        }
+    }
+
+}
---
+++ src/chapter4/section3/Exercise25_WorstCaseGenerator.java
@@ -0,0 +1,155 @@
+package chapter4.section3;
+
+import chapter1.section3.Bag;
+import chapter1.section3.Queue;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+/**
+ * Created by rene on 09/11/17.
+ */
+public class Exercise25_WorstCaseGenerator {
+
+    // This edge-weighted graph implementation keeps the adjacent neighbors in the order added.
+    // This is necessary to guarantee a worst-case for the eager version of Prim's algorithm.
+    @SuppressWarnings("unchecked")
+    private class EdgeWeightedGraphWithNeighborsInOrder implements EdgeWeightedGraphInterface {
+
+        private final int vertices;
+        private int edges;
+        private Queue<Edge>[] adjacent;
+
+        public EdgeWeightedGraphWithNeighborsInOrder(int vertices) {
+            this.vertices = vertices;
+            edges = 0;
+            adjacent = (Queue<Edge>[]) new Queue[vertices];
+
+            for(int vertex = 0; vertex < vertices; vertex++) {
+                adjacent[vertex] = new Queue<>();
+            }
+        }
+
+        public EdgeWeightedGraphWithNeighborsInOrder(In in) {
+            this(in.readInt());
+            int edges = in.readInt();
+
+            if (edges < 0) {
+                throw new IllegalArgumentException("Number of edges must be nonnegative");
+            }
+
+            for(int i = 0; i < edges; i++) {
+                int vertex1 = in.readInt();
+                int vertex2 = in.readInt();
+                double weight = in.readDouble();
+
+                Edge edge = new Edge(vertex1, vertex2, weight);
+                addEdge(edge);
+            }
+        }
+
+        public int vertices() {
+            return vertices;
+        }
+
+        public int edgesCount() {
+            return edges;
+        }
+
+        public void addEdge(Edge edge) {
+            int vertex1 = edge.either();
+            int vertex2 = edge.other(vertex1);
+
+            adjacent[vertex1].enqueue(edge);
+            adjacent[vertex2].enqueue(edge);
+            edges++;
+        }
+
+        public Iterable<Edge> adjacent(int vertex) {
+            return adjacent[vertex];
+        }
+
+        public Iterable<Edge> edges() {
+            Bag<Edge> edges = new Bag<>();
+
+            for(int vertex = 0; vertex < vertices; vertex++) {
+                for(Edge edge : adjacent[vertex]) {
+                    if(edge.other(vertex) > vertex) {
+                        edges.add(edge);
+                    }
+                }
+            }
+
+            return edges;
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for(int vertex = 0; vertex < vertices(); vertex++) {
+                stringBuilder.append(vertex).append(": ");
+
+                for(Edge neighbor : adjacent(vertex)) {
+                    stringBuilder.append(neighbor).append(" ");
+                }
+                stringBuilder.append("\n");
+            }
+
+            return stringBuilder.toString();
+        }
+    }
+
+    //Worst-case for the lazy version of Prim's algorithm -> a complete graph, leading to a priority queue of size E = V^2.
+    // The complexity is O(E lg E)
+    public EdgeWeightedGraph generateWorstCaseGraphForLazyPrim(int vertices) {
+        EdgeWeightedGraph edgeWeightedGraph = new EdgeWeightedGraph(vertices);
+
+        for(int vertex1 = 0; vertex1 < vertices; vertex1++) {
+            for(int vertex2 = vertex1 + 1; vertex2 < vertices; vertex2++) {
+                double weight = StdRandom.uniform();
+                Edge edge = new Edge(vertex1, vertex2, weight);
+                edgeWeightedGraph.addEdge(edge);
+            }
+        }
+
+        return edgeWeightedGraph;
+    }
+
+    //Worst-case for the eager version of Prim's algorithm -> a complete graph, where each new vertex analyzed that
+    // is not in the current MST is closer to the other vertices that are not in the MST than any other vertex in
+    // the current MST is (for each v and w that are not in the MST, v-w weight is less than distTo[w]).
+    // This causes edgeTo, distTo and the priority queue to be updated E times.
+    // The complexity is O(E lg V)
+    public EdgeWeightedGraphWithNeighborsInOrder generateWorstCaseGraphForEagerPrim(int vertices) {
+        EdgeWeightedGraphWithNeighborsInOrder edgeWeightedGraph = new EdgeWeightedGraphWithNeighborsInOrder(vertices);
+
+        double maxWeight = StdRandom.uniform();
+        double valueToDecrease = 0.00001;
+
+        for(int vertex1 = 0; vertex1 < vertices; vertex1++) {
+            valueToDecrease = valueToDecrease + 0.00001;
+
+            for(int vertex2 = vertex1 + 1; vertex2 < vertices; vertex2++) {
+                double weight = maxWeight - valueToDecrease;
+                Edge edge = new Edge(vertex1, vertex2, weight);
+                edgeWeightedGraph.addEdge(edge);
+            }
+        }
+
+        return edgeWeightedGraph;
+    }
+
+    public static void main(String[] args) {
+        Exercise25_WorstCaseGenerator worstCaseGenerator = new Exercise25_WorstCaseGenerator();
+
+        int numberOfVertices = 10;
+        EdgeWeightedGraph worstCaseForLazyPrim = worstCaseGenerator.generateWorstCaseGraphForLazyPrim(numberOfVertices);
+        StdOut.println(worstCaseForLazyPrim);
+
+        EdgeWeightedGraphWithNeighborsInOrder worstCaseForEagerPrim =
+                worstCaseGenerator.generateWorstCaseGraphForEagerPrim(numberOfVertices);
+        StdOut.println(worstCaseForEagerPrim);
+    }
+
+}
