09c3ff66e2e525b5ddf51f0acb9e15f99632b722
Rene Argento <rene.argento@gmail.com>
2017-12-04 00:33:42 -0200

    Added exercise 4.4.12

---
+++ src/chapter4/section4/Exercise12.java
@@ -0,0 +1,125 @@
+package chapter4.section4;
+
+import chapter1.section3.Stack;
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Created by rene on 02/12/17.
+ */
+public class Exercise12 {
+
+    public class EdgeWeightedDirectedCycle {
+
+        private boolean visited[];
+        private DirectedEdge[] edgeTo;
+        private Stack<DirectedEdge> cycle; // vertices on a cycle (if one exists)
+        private boolean[] onStack; // vertices on recursive call stack
+
+        public EdgeWeightedDirectedCycle(EdgeWeightedDigraph edgeWeightedDigraph) {
+            onStack = new boolean[edgeWeightedDigraph.vertices()];
+            edgeTo = new DirectedEdge[edgeWeightedDigraph.vertices()];
+            visited = new boolean[edgeWeightedDigraph.vertices()];
+
+            for(int vertex = 0; vertex < edgeWeightedDigraph.vertices(); vertex++) {
+                if(!visited[vertex]) {
+                    dfs(edgeWeightedDigraph, vertex);
+                }
+            }
+        }
+
+        private void dfs(EdgeWeightedDigraph edgeWeightedDigraph, int vertex) {
+            onStack[vertex] = true;
+            visited[vertex] = true;
+
+            for(DirectedEdge edge : edgeWeightedDigraph.adjacent(vertex)) {
+                int neighbor = edge.to();
+
+                if(hasCycle()) {
+                    return;
+                } else if(!visited[neighbor]) {
+                    edgeTo[neighbor] = edge;
+                    dfs(edgeWeightedDigraph, neighbor);
+                } else if(onStack[neighbor]) {
+                    cycle = new Stack<>();
+
+                    DirectedEdge edgeInCycle = edge;
+
+                    while (edgeInCycle.from() != neighbor) {
+                        cycle.push(edgeInCycle);
+                        edgeInCycle = edgeTo[edgeInCycle.from()];
+                    }
+
+                    cycle.push(edgeInCycle);
+                    return;
+                }
+            }
+
+            onStack[vertex] = false;
+        }
+
+        public boolean hasCycle() {
+            return cycle != null;
+        }
+
+        public Iterable<DirectedEdge> cycle() {
+            return cycle;
+        }
+    }
+
+    public class Topological {
+
+        private Iterable<Integer> topologicalOrder;
+
+        public Topological(EdgeWeightedDigraph edgeWeightedDigraph) {
+            EdgeWeightedDirectedCycle cycleFinder = new EdgeWeightedDirectedCycle(edgeWeightedDigraph);
+
+            if(!cycleFinder.hasCycle()) {
+                DepthFirstOrder depthFirstOrder = new DepthFirstOrder(edgeWeightedDigraph);
+                topologicalOrder = depthFirstOrder.reversePostOrder();
+            }
+        }
+
+        public Iterable<Integer> order() {
+            return topologicalOrder;
+        }
+
+        public boolean isDAG() {
+            return topologicalOrder != null;
+        }
+    }
+
+    public static void main(String[] args) {
+        Exercise12 exercise12 = new Exercise12();
+
+        EdgeWeightedDigraph edgeWeightedDigraphWithCycle = new EdgeWeightedDigraph(8);
+        edgeWeightedDigraphWithCycle.addEdge(new DirectedEdge(0, 1, 0.35));
+        edgeWeightedDigraphWithCycle.addEdge(new DirectedEdge(1, 2, 0.35));
+        edgeWeightedDigraphWithCycle.addEdge(new DirectedEdge(2, 3, 0.37));
+        edgeWeightedDigraphWithCycle.addEdge(new DirectedEdge(3, 4, 0.28));
+        edgeWeightedDigraphWithCycle.addEdge(new DirectedEdge(4, 1, 0.28));
+        edgeWeightedDigraphWithCycle.addEdge(new DirectedEdge(6, 7, 0.32));
+        edgeWeightedDigraphWithCycle.addEdge(new DirectedEdge(7, 5, 0.38));
+
+        EdgeWeightedDigraph edgeWeightedDAG = new EdgeWeightedDigraph(5);
+        edgeWeightedDAG.addEdge(new DirectedEdge(0, 1, 0.35));
+        edgeWeightedDAG.addEdge(new DirectedEdge(1, 2, 0.22));
+        edgeWeightedDAG.addEdge(new DirectedEdge(3, 4, 0.31));
+        edgeWeightedDAG.addEdge(new DirectedEdge(4, 0, 0.29));
+
+        StdOut.println("Cycle:");
+        EdgeWeightedDirectedCycle edgeWeightedDirectedCycle =
+                exercise12.new EdgeWeightedDirectedCycle(edgeWeightedDigraphWithCycle);
+        for(DirectedEdge edge : edgeWeightedDirectedCycle.cycle()) {
+            StdOut.print(edge.from() + "->" + edge.to() + " ");
+        }
+        StdOut.println("\nExpected: 1->2 2->3 3->4 4->1 ");
+
+        Topological topological = exercise12.new Topological(edgeWeightedDAG);
+        StdOut.println("\nTopological order:");
+        for(int vertex : topological.order()) {
+            StdOut.print(vertex + " ");
+        }
+        StdOut.println("\nExpected: 3 4 0 1 2");
+    }
+
+}
---
+++ src/chapter4/section4/KShortestPaths.java
@@ -0,0 +1,112 @@
+package chapter4.section4;
+
+import chapter2.section4.PriorityQueueResize;
+import chapter3.section4.SeparateChainingHashTable;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by rene on 01/12/17.
+ */
+// Based on https://en.wikipedia.org/wiki/K_shortest_path_routing#Algorithm
+@SuppressWarnings("unchecked")
+public class KShortestPaths {
+
+    public class Path implements Comparable<Path> {
+
+        private Path previousPath;
+        private DirectedEdge directedEdge;
+        private int lastVertexInPath;
+        private double weight;
+        private HashSet<Integer> verticesInPath;
+
+        Path(int vertex) {
+            lastVertexInPath = vertex;
+
+            verticesInPath = new HashSet<>();
+            verticesInPath.add(vertex);
+        }
+
+        Path(Path previousPath, DirectedEdge directedEdge) {
+            this(directedEdge.to());
+            this.previousPath = previousPath;
+
+            verticesInPath.addAll(previousPath.verticesInPath);
+
+            this.directedEdge = directedEdge;
+            weight += previousPath.weight() + directedEdge.weight();
+        }
+
+        public double weight() {
+            return weight;
+        }
+
+        public Iterable<DirectedEdge> getPath() {
+            LinkedList<DirectedEdge> path = new LinkedList<>();
+
+            while (previousPath != null && previousPath.directedEdge != null) {
+                path.addFirst(previousPath.directedEdge);
+
+                previousPath = previousPath.previousPath;
+            }
+            path.add(directedEdge);
+
+            return path;
+        }
+
+        @Override
+        public int compareTo(Path other) {
+            if(this.weight < other.weight) {
+                return -1;
+            } else if(this.weight > other.weight) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+    }
+
+    public List<Path> getKShortestPaths(EdgeWeightedDigraph edgeWeightedDigraph, int source, int target, int kPaths) {
+
+        List<Path> paths = new ArrayList<>();
+        SeparateChainingHashTable<Integer, Integer> countMap = new SeparateChainingHashTable<>();
+        countMap.put(target, 0);
+
+        PriorityQueueResize<Path> priorityQueue = new PriorityQueueResize(PriorityQueueResize.Orientation.MIN);
+        priorityQueue.insert(new Path(source));
+
+        while (!priorityQueue.isEmpty() && countMap.get(target) < kPaths) {
+            Path currentPath = priorityQueue.deleteTop();
+            int lastVertexInPath = currentPath.lastVertexInPath;
+
+            int pathsToCurrentVertex = 0;
+
+            if(countMap.get(lastVertexInPath) != null) {
+                pathsToCurrentVertex = countMap.get(lastVertexInPath);
+            }
+
+            pathsToCurrentVertex++;
+            countMap.put(lastVertexInPath, pathsToCurrentVertex);
+
+            if(lastVertexInPath == target) {
+                paths.add(currentPath);
+            }
+
+            if(pathsToCurrentVertex <= kPaths) {
+                for(DirectedEdge edge : edgeWeightedDigraph.adjacent(lastVertexInPath)) {
+                    // Do not repeat vertices - we are interested in paths and not walks
+                    if(!currentPath.verticesInPath.contains(edge.to())) {
+                        Path newPath = new Path(currentPath, edge);
+                        priorityQueue.insert(newPath);
+                    }
+                }
+            }
+        }
+
+        return paths;
+    }
+
+}
