179332cd586800e76e2331a352d30b5d97463df4
Rene Argento <rene.argento@gmail.com>
2018-04-03 20:18:53 -0300

    Fixed bug in exercises 1.4.8 and 1.4.10

--- src/chapter1/section4/Exercise10.java
+++ src/chapter1/section4/Exercise10.java
@@ -5,49 +5,50 @@ import edu.princeton.cs.algs4.StdOut;
 /**
  * Created by Rene Argento on 9/28/16.
  */
+// Thanks for Vivek Bhojawala (https://github.com/VBhojawala) for fixing a bug and suggesting improvements
+// in the binarySearch() method at https://github.com/reneargento/algorithms-sedgewick-wayne/issues/6
 public class Exercise10 {
 
     public static void main(String[] args) {
-        int[] testArray = {3, 4, 4, 5, 6, 10, 15, 20, 20, 20, 20, 21};
+        int[] testArray1 = {3, 4, 4, 5, 6, 10, 15, 20, 20, 20, 20, 21};
         int elementToSearch1 = 4;
         int elementToSearch2 = 20;
         int elementToSearch3 = -5;
 
-        StdOut.println("Binary search: " + binarySearch(testArray, elementToSearch1, 0, testArray.length) +
+        StdOut.println("Binary search: " + binarySearch(testArray1, elementToSearch1, 0, testArray1.length) +
                 " Expected: 1");
-        StdOut.println("Binary search: " + binarySearch(testArray, elementToSearch2, 0, testArray.length) +
+        StdOut.println("Binary search: " + binarySearch(testArray1, elementToSearch2, 0, testArray1.length) +
                 " Expected: 7");
-        StdOut.println("Binary search: " + binarySearch(testArray, elementToSearch3, 0, testArray.length) +
+        StdOut.println("Binary search: " + binarySearch(testArray1, elementToSearch3, 0, testArray1.length) +
                 " Expected: -1");
+
+        int[] testArray2 = {4, 4, 4, 4, 4, 4, 15, 20, 20, 20, 20, 21};
+        int elementToSearch4 = 4;
+
+        StdOut.println("Binary search: " + binarySearch(testArray2, elementToSearch4, 0, testArray2.length) +
+                " Expected: 0");
     }
 
     private static int binarySearch(int[] array, int element, int low, int high) {
 
-        if (low >= high) {
+        if (low > high) {
             return -1;
         }
 
         int middle = low + (high - low) / 2;
 
         if (array[middle] < element) {
-            return binarySearch(array, element, middle+1, high);
+            return binarySearch(array, element, middle + 1, high);
         } else if (array[middle] > element) {
-            return binarySearch(array, element, low, middle);
+            return binarySearch(array, element, low, middle - 1);
         } else {
 
             int possibleSmallestIndex = binarySearch(array, element, 0, middle - 1);
             if (possibleSmallestIndex == -1) {
                 return middle;
             } else {
-                return binarySearch(array, element, 0, middle - 1);
+                return possibleSmallestIndex;
             }
-
-            //O(n)
-            //int smallestIndex = middle;
-//            while(smallestIndex - 1 >= 0 && array[smallestIndex - 1] == array[smallestIndex]) {
-//                smallestIndex--;
-//            }
-            //return smallestIndex;
         }
     }
 }
--- src/chapter1/section4/Exercise8.java
+++ src/chapter1/section4/Exercise8.java
@@ -3,22 +3,65 @@ package chapter1.section4;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
  * Created by Rene Argento on 9/28/16.
  */
+// Thanks for Vivek Bhojawala (https://github.com/VBhojawala) for mentioning that the solution should use
+// Arrays.sort(), as requested in the exercise.g https://github.com/reneargento/algorithms-sedgewick-wayne/issues/6
+
+//Quantity of numbers : Quantity of equal pairs
+//        2                      1
+//        3                      3
+//        4                      6
+//        5                      10
+// The quantity of equal pairs is equal to:
+// (n - 1) * n / 2
 public class Exercise8 {
 
     public static void main(String[] args) {
         In in = new In(args[0]);
         int[] values = in.readAllInts();
+        StdOut.println(countNumberOfPairs(values));
 
-        StdOut.println(countEqualNumbers(values));
+        // Tests
+        int[] values1 = {1, 2, 4, 1, 2, 1, 2, 4, 5, 1, 2, 4, 5, 1, 2 ,5, 6, 7, 7, 8, 2, 1, 2, 4, 5};
+        StdOut.println("Equal pairs 1: " + countNumberOfPairs(values1) + " Expected: 49");
+
+        int[] values2 = {1, 1, 1};
+        StdOut.println("Equal pairs 2: " + countNumberOfPairs(values2) + " Expected: 3");
     }
 
-    private static int countEqualNumbers(int[] values) {
+    // O(N lg N) solution
+    private static int countNumberOfPairs(int[] values) {
+        Arrays.sort(values);
+
+        int count = 0;
+        int currentFrequency = 1;
+
+        for (int i = 1; i < values.length; i++) {
+            if (values[i] == values[i - 1]) {
+                currentFrequency++;
+            } else {
+                if (currentFrequency > 1) {
+                    count += (currentFrequency - 1) * currentFrequency / 2;
+                    currentFrequency = 1;
+                }
+            }
+        }
+
+        if (currentFrequency > 1) {
+            count += (currentFrequency - 1) * currentFrequency / 2;
+        }
+
+        return count;
+    }
+
+    // O(N) solution
+    private static int countNumberOfPairs2(int[] values) {
 
         Map<Integer, Integer> valuesMap = new HashMap<>();
         int equalNumbersCount = 0;
@@ -34,13 +77,6 @@ public class Exercise8 {
 
         for(int numberKey : valuesMap.keySet()) {
             if (valuesMap.get(numberKey) > 1) {
-                //Quantity of numbers : Quantity of equal pairs
-                //        2                      1
-                //        3                      3
-                //        4                      6
-                //        5                      10
-                // The quantity of equal pairs is equal to:
-                // (n - 1) * n / 2
                 int n = valuesMap.get(numberKey);
                 equalNumbersCount += (n - 1) * n / 2;
             }
