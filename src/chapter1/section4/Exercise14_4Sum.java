872dbd1fb13254535d422fcf8a6803db37f90da2
Rene Argento <rene.argento@gmail.com>
2018-04-07 00:06:34 -0300

    Updated exercise 1.4.14

--- src/chapter1/section4/Exercise14_4Sum.java
+++ src/chapter1/section4/Exercise14_4Sum.java
@@ -2,7 +2,7 @@ package chapter1.section4;
 
 import edu.princeton.cs.algs4.StdOut;
 
-import java.util.Arrays;
+import java.util.*;
 
 /**
  * Created by Rene Argento on 9/29/16.
@@ -11,14 +11,74 @@ public class Exercise14_4Sum {
 
     public static void main(String[] args) {
 
-        int[] array = {1, 2, 3, 4, -4, -5, -6, 2, 4, -1};
-        StdOut.println("4 sum: " + fourSum(array));
-        StdOut.println("Expected: 8");
+        // Method 1
+        StdOut.println("Method 1");
+        int[] array1 = {5, 2, -2, -5, -2};
+        StdOut.println("4 sum: " + fourSum(array1));
+        StdOut.println("Expected: 2");
+
+        int[] array2 = {1, 2, 3, 4, -4, -5, -6, 2, 4, -1};
+        StdOut.println("4 sum: " + fourSum(array2));
+        StdOut.println("Expected: 13");
+
+        // Method 2
+        StdOut.println("\nMethod 2");
+        StdOut.println("4 sum: " + fourSum2(array1));
+        StdOut.println("Expected: 2");
+
+        StdOut.println("4 sum: " + fourSum2(array2));
+        StdOut.println("Expected: 13");
     }
 
-    //O(n^3 lg n)
-    private static int fourSum(int[] array) {
+    private static class Pair {
+        int index1;
+        int index2;
 
+        Pair(int index1, int index2) {
+            this.index1 = index1;
+            this.index2 = index2;
+        }
+    }
+
+    // O(n^3)
+    private static int fourSum(int[] array) {
+        Map<Integer, List<Pair>> sumMap = new HashMap<>();
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                int sum = array[i] + array[j];
+
+                if (!sumMap.containsKey(sum)) {
+                    sumMap.put(sum, new ArrayList<>());
+                }
+
+                sumMap.get(sum).add(new Pair(i, j));
+            }
+        }
+
+        int count = 0;
+
+        for (int key : sumMap.keySet()) {
+            
+            if (sumMap.containsKey(-key)) {
+                List<Pair> pairs = sumMap.get(key);
+                List<Pair> pairsComplement = sumMap.get(-key);
+
+                for (Pair pair1 : pairs) {
+                    for (Pair pair2 : pairsComplement) {
+                        if (pair1.index2 < pair2.index1) {
+                            count++;
+                        }
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+
+    // O(n^3 lg n)
+    private static int fourSum2(int[] array) {
         Arrays.sort(array);
 
         int count = 0;
@@ -28,11 +88,28 @@ public class Exercise14_4Sum {
                 for(int k = j + 1; k < array.length; k++) {
 
                     int searchElement = -1 * (array[i] + array[j] + array[k]);
-                    int elementIndex = binarySearch(array, searchElement, 0, array.length - 1);
-                    if (elementIndex > k) {
-                        StdOut.println("" + array[i] +  " " + array[j] +  " " + array[k] +  " " + array[elementIndex]);
-                        count++;
+                    int elementIndexLeft = binarySearch(array, searchElement, 0, array.length - 1, true);
+
+                    if (elementIndexLeft == -1) {
+                        continue;
                     }
+
+                    int elementIndexRight = binarySearch(array, searchElement, 0, array.length - 1, false);
+
+                    if (elementIndexLeft < k + 1) {
+                        if (elementIndexRight >= k +1) {
+                            elementIndexLeft = k + 1;
+                        } else {
+                            continue;
+                        }
+                    }
+
+                    // Debug
+//                    for (int d = elementIndexLeft; d <= elementIndexRight; d++) {
+//                        StdOut.println(array[i] +  " " + array[j] +  " " + array[k] +  " " + array[d]);
+//                    }
+
+                    count += elementIndexRight - elementIndexLeft + 1;
                 }
             }
         }
@@ -40,7 +117,7 @@ public class Exercise14_4Sum {
         return count;
     }
 
-    private static int binarySearch(int[] array, int key, int low, int high) {
+    private static int binarySearch(int[] array, int target, int low, int high, boolean searchLow) {
 
         if (low > high) {
             return -1;
@@ -48,12 +125,24 @@ public class Exercise14_4Sum {
 
         int middle = low + (high - low) / 2;
 
-        if (array[middle] > key) {
-            return binarySearch(array, key, low, middle - 1);
-        } else if (array[middle] < key) {
-            return binarySearch(array, key, middle + 1, high);
+        if (array[middle] > target) {
+            return binarySearch(array, target, low, middle - 1, searchLow);
+        } else if (array[middle] < target) {
+            return binarySearch(array, target, middle + 1, high, searchLow);
         } else {
-            return middle;
+            int nextIndex;
+
+            if (searchLow) {
+                nextIndex = binarySearch(array, target, low, middle - 1, true);
+            } else {
+                nextIndex = binarySearch(array, target, middle + 1, high, false);
+            }
+
+            if (nextIndex != -1) {
+                return nextIndex;
+            } else {
+                return middle;
+            }
         }
     }
 }
