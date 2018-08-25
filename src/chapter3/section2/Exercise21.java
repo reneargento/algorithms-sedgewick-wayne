a942e27fb667b7ae446c902a183604a1e4e06074
Rene Argento <rene.argento@gmail.com>
2018-08-26 01:29:43 +0200

    Fixed exercise 3.2.21

--- src/chapter3/section2/Exercise21.java
+++ src/chapter3/section2/Exercise21.java
@@ -6,40 +6,20 @@ import edu.princeton.cs.algs4.StdRandom;
 /**
  * Created by Rene Argento on 02/06/17.
  */
+// Thanks to Daniel Bedrenko (https://github.com/dbedrenko) for correcting and suggesting a better implementation
+// for this exercise: https://github.com/reneargento/algorithms-sedgewick-wayne/issues/19
+// Based on https://github.com/ChangeMyUsername/algorithms-sedgewick-python/blob/master/chapter_3/module_3_2.py
 public class Exercise21 {
 
     private class BinarySearchTree<Key extends Comparable<Key>, Value> extends chapter3.section2.BinarySearchTree<Key, Value> {
 
         public Key randomKey() {
-            if (size() == 0) {
+            if (isEmpty()) {
                 return null;
             }
 
-            Node current = root;
-
-            while (current != null) {
-                int randomMove = StdRandom.uniform(3);
-                //0 - Move left
-                //1 - Move right
-                //2 - Choose the current key
-                if (randomMove == 0) {
-                    if (current.left != null) {
-                        current = current.left;
-                    } else {
-                        return current.key;
-                    }
-                } else if (randomMove == 1) {
-                    if (current.right != null) {
-                        current = current.right;
-                    } else {
-                        return current.key;
-                    }
-                } else {
-                    return current.key;
-                }
-            }
-
-            return null;
+            int randomIndex = StdRandom.uniform(size());
+            return select(randomIndex);
         }
     }
 
@@ -55,17 +35,15 @@ public class Exercise21 {
         binarySearchTree.put(0, "Value 0");
         binarySearchTree.put(99, "Value 99");
 
-        StdOut.println();
-
         //Test size()
-        StdOut.println("Size: " + binarySearchTree.size() + " Expected: 6");
+        StdOut.println("Size: " + binarySearchTree.size() + " Expected: 6\n");
 
         //Test get() and keys()
         for(Integer key : binarySearchTree.keys()) {
             StdOut.println("Key " + key + ": " + binarySearchTree.get(key));
         }
 
-        StdOut.println("Random keys:");
+        StdOut.println("\nRandom keys:");
         StdOut.println(binarySearchTree.randomKey());
         StdOut.println(binarySearchTree.randomKey());
         StdOut.println(binarySearchTree.randomKey());
