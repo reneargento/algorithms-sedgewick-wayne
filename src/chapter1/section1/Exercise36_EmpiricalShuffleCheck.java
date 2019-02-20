3ad44914c70a29823cb3c256fe87af7979528c0e
Rene Argento <rene.argento@gmail.com>
2019-02-21 00:34:13 +0100

    Fixed exercises 1.1.36 and 1.1.37

--- src/chapter1/section1/Exercise36_EmpiricalShuffleCheck.java
+++ src/chapter1/section1/Exercise36_EmpiricalShuffleCheck.java
@@ -6,6 +6,8 @@ import edu.princeton.cs.algs4.StdRandom;
 /**
  * Created by Rene Argento
  */
+// Thanks to ccumulatio (https://github.com/ccumulatio) for mentioning that the arrays should be reinitialized at each
+// loop iteration: https://github.com/reneargento/algorithms-sedgewick-wayne/issues/33
 public class Exercise36_EmpiricalShuffleCheck {
 
 	// Parameters example: 200000 5
@@ -13,46 +15,48 @@ public class Exercise36_EmpiricalShuffleCheck {
 
 		int n = Integer.parseInt(args[0]);
 		int m = Integer.parseInt(args[1]);
-		
+
 		int[][] positions = new int[m][m];
-		
-		double[] arr = new double[m];
-		arr[0] = 0;
-		arr[1] = 1;
-		arr[2] = 2;
-		arr[3] = 3;
-		arr[4] = 4;
-		
+
+		double[] array = new double[m];
+
 		for (int i = 0; i < n; i++) {
-			shuffle(arr);
-			
-			for (int j = 0; j < arr.length; j++) {
-				positions[j][(int)arr[j]]++;
+			initializeArray(array);
+			shuffle(array);
+
+			for (int j = 0; j < array.length; j++) {
+				positions[j][(int)array[j]]++;
 			}
 		}
-		
+
 		printTable(positions);
 		//Entries are close to N/M
 	}
-	
-	public static void shuffle(double[] a) {
-		int n = a.length;
-		
-		for (int i = 0; i < n; i++) {
-			int r = i + StdRandom.uniform(n - i);
-			double temp = a[i];
-			a[i] = a[r];
-			a[r] = temp;
+
+	private static void initializeArray(double[] array) {
+		for (int i = 0; i < array.length; i++) {
+			array[i] = i;
 		}
 	}
-	
+
+	public static void shuffle(double[] array) {
+		int n = array.length;
+
+		for (int i = 0; i < n; i++) {
+			int r = i + StdRandom.uniform(n - i);
+			double temp = array[i];
+			array[i] = array[r];
+			array[r] = temp;
+		}
+	}
+
 	private static void printTable(int[][] positions) {
-		
+
 		StdOut.println("TABLE");
-		
+
 		for (int i = 0; i < positions.length; i++) {
 			StdOut.printf("%3d  ", i);
-			
+
 			for (int j = 0; j < positions[0].length; j++) {
 				StdOut.printf("%4d ", positions[i][j]);
 			}
--- src/chapter1/section1/Exercise37_BadShuffling.java
+++ src/chapter1/section1/Exercise37_BadShuffling.java
@@ -6,60 +6,57 @@ import edu.princeton.cs.algs4.StdRandom;
 /**
  * Created by Rene Argento
  */
+// Thanks to ccumulatio (https://github.com/ccumulatio) for mentioning that the arrays should be reinitialized at each
+// loop iteration: https://github.com/reneargento/algorithms-sedgewick-wayne/issues/33
 public class Exercise37_BadShuffling {
-	
-public static void main(String[] args) {
-		
-		int n = 200000;
-		int m = 5;
-		
+
+	// Parameters example: 200000 5
+	public static void main(String[] args) {
+
+		int n = Integer.parseInt(args[0]);
+		int m = Integer.parseInt(args[1]);
+
 		int[][] positions = new int[m][m];
-		
-		double[] arr = new double[m];
-		arr[0] = 0;
-		arr[1] = 1;
-		arr[2] = 2;
-		arr[3] = 3;
-		arr[4] = 4;
-		
+
+		double[] array = new double[m];
+
 		for (int i = 0; i < n; i++) {
-			badShuffle(arr);
-			
-			for (int j = 0; j < arr.length; j++) {
-				positions[j][(int)arr[j]]++;
+			initializeArray(array);
+			badShuffle(array);
+
+			for (int j = 0; j < array.length; j++) {
+				positions[j][(int)array[j]]++;
 			}
 		}
-		
-		StdOut.println("Order after shuffles:");
-		
-		for (int i = 0; i < arr.length; i++) {
-			StdOut.print(arr[i] + " ");
-		}
-		
-		StdOut.println();
-		
+
 		printTable(positions);
-		//Entries are close to N/M
 	}
-	
-	public static void badShuffle(double[] a) {
-		int n = a.length;
-		
-		for (int i = 0; i < n; i++) {
-			int r = 0 + StdRandom.uniform(n - i);
-			double temp = a[i];
-			a[i] = a[r];
-			a[r] = temp;
+
+	private static void initializeArray(double[] array) {
+		for (int i = 0; i < array.length; i++) {
+			array[i] = i;
 		}
 	}
-	
+
+	public static void badShuffle(double[] array) {
+		int n = array.length;
+
+		for (int i = 0; i < n; i++) {
+			int randomIndex = StdRandom.uniform(n - i);
+
+			double temp = array[i];
+			array[i] = array[randomIndex];
+			array[randomIndex] = temp;
+		}
+	}
+
 	private static void printTable(int[][] positions) {
-		
+
 		StdOut.println("TABLE");
-		
+
 		for (int i = 0; i < positions.length; i++) {
 			StdOut.printf("%3d  ", i);
-			
+
 			for (int j = 0; j < positions[0].length; j++) {
 				StdOut.printf("%4d ", positions[i][j]);
 			}
