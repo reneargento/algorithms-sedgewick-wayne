07585ee341b2a503febf797239fefe906614c432
Rene Argento <rene.argento@gmail.com>
2019-05-02 22:37:53 +0200

    Updated exercise 1.4.26

--- src/chapter1/section4/Exercise26_3Collinearity.java
+++ src/chapter1/section4/Exercise26_3Collinearity.java
@@ -8,6 +8,8 @@ import java.util.*;
 /**
  * Created by Rene Argento on 20/11/16.
  */
+// Thanks to shftdlt (https://github.com/shftdlt) for fixing a bug in this exercise.
+// https://github.com/reneargento/algorithms-sedgewick-wayne/issues/48
 public class Exercise26_3Collinearity {
 
     public static void main(String[] args) {
@@ -21,9 +23,9 @@ public class Exercise26_3Collinearity {
         }
 
         int numberOfTriples1 = exercise26_3Collinearity.countTriples(points);
-        StdOut.println("Number of triples: " + numberOfTriples1  + " Expected: 10");
+        StdOut.println("Number of triples: " + numberOfTriples1  + " Expected: 20");
 
-        //Based on https://www.algebra.com/algebra/homework/Length-and-distance/Length-and-distance.faq.question.530663.html
+        // Based on https://www.algebra.com/algebra/homework/Length-and-distance/Length-and-distance.faq.question.530663.html
         //(-3,4) (3,2) (6,1) are on the same line
         Point2D pointA = new Point2D(-3, 4);
         Point2D pointB = new Point2D(3, 2);
@@ -39,9 +41,9 @@ public class Exercise26_3Collinearity {
         Point2D[] points3 = {pointA, pointB, pointC, pointD};
 
         int numberOfTriples3 = exercise26_3Collinearity.countTriples(points3);
-        StdOut.println("Number of triples: " + numberOfTriples3  + " Expected: 3");
+        StdOut.println("Number of triples: " + numberOfTriples3  + " Expected: 4");
 
-        //Case with cubic y coordinate
+        // Case with cubic y coordinate
         Point2D pointE = new Point2D(1, 1);
         Point2D pointF = new Point2D(2, 8);
         Point2D pointG = new Point2D(-3, -27);
@@ -51,7 +53,7 @@ public class Exercise26_3Collinearity {
         StdOut.println("Number of triples: " + numberOfTriples4  + " Expected: 1");
     }
 
-    //This algorithm can be reduced to 3-SUM since we are searching for
+    // This algorithm can be reduced to 3-SUM since we are searching for
     // a combination of 3 points that can be summed to specific value
     private int countTriples(Point2D[] points) {
 
@@ -72,9 +74,9 @@ public class Exercise26_3Collinearity {
         }
     }
 
-    //Based on http://stackoverflow.com/questions/3813681/checking-to-see-if-3-points-are-on-the-same-line
+    // Based on http://stackoverflow.com/questions/3813681/checking-to-see-if-3-points-are-on-the-same-line
     private int countTriplesUsingSlopes(Point2D[] points) {
-        //If (y1 - y2) * (x1 - x3) == (y1 - y3) * (x1 - x2) then the points are collinear
+        // If (y1 - y2) * (x1 - x3) == (y1 - y3) * (x1 - x2) then the points are collinear
 
         Map<Double, List<Integer>> slopes = new HashMap<>();
 
@@ -125,7 +127,6 @@ public class Exercise26_3Collinearity {
                     for (Integer index : pointIndexes) {
                         if (index > i && index > j) {
                             triples++;
-                            break;
                         }
                     }
                 }
@@ -136,22 +137,22 @@ public class Exercise26_3Collinearity {
     }
 
     private int countTriplesWithCubicYs(Point2D[] points) {
-        //Based on http://stackoverflow.com/questions/4179581/what-is-the-most-efficient-algorithm-to-find-a-straight-line-that-goes-through-m
-        //If the points have the same slope, then they are collinear (on the same line)
+        // Based on http://stackoverflow.com/questions/4179581/what-is-the-most-efficient-algorithm-to-find-a-straight-line-that-goes-through-m
+        // If the points have the same slope, then they are collinear (on the same line)
 
-        //If x + y + z = 0 then the slope of the line from x to y is
+        // If x + y + z = 0 then the slope of the line from x to y is
         // (y^3 - x^3) / (y - x) = y^2 + yx + x^2
         // And the slope of the line from x to z is
         //(z^3 - x^3) / (z - x) = z^2 + zx + x^2
 
-        //Conversely, if the slope from x to y equals the slope from x to z then
+        // Conversely, if the slope from x to y equals the slope from x to z then
         // y^2 + yx + x^2 = z^2 + zx + x^2
 
-        //Which implies that
+        // Which implies that
         // (y - z) (x + y + z) = 0
         // So either y = z or z = -x - y which suffices to prove that the reduction is valid
 
-        //If there are duplicates in X, first
+        // If there are duplicates in X, first
         // check whether x + 2y = 0 for any x and duplicate element y
         // (in linear time using hashing or O(n lg n) time using sorting),
         // and then remove the duplicates before reducing to the collinear point-finding problem.
@@ -184,7 +185,6 @@ public class Exercise26_3Collinearity {
                     for (Integer index : pointIndexes) {
                         if (index > i && index > j) {
                             triples++;
-                            break;
                         }
                     }
                 }
