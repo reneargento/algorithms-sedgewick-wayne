d74098db020089ca44f4cb7d4211480291a7eb70
Rene Argento <rene.argento@gmail.com>
2019-08-06 11:05:52 +0200

    Updated exercise 1.1.30

--- src/chapter1/section1/Exercise30_ArrayExercise.java
+++ src/chapter1/section1/Exercise30_ArrayExercise.java
@@ -5,22 +5,22 @@ import edu.princeton.cs.algs4.StdOut;
 /**
  * Created by Rene Argento
  */
-// Thanks to alexrad89 (https://github.com/alexrad89) for suggesting an improvement on this exercise.
+// Thanks to alexrad89 (https://github.com/alexrad89) and to shrutigupta23 (https://github.com/shrutigupta23) for
+// suggesting improvements to this exercise.
 // https://github.com/reneargento/algorithms-sedgewick-wayne/issues/34
 public class Exercise30_ArrayExercise {
 
 	public static void main(String[] args) {
-		
 		int n = 5;
-		
+
 		boolean[][] array = new boolean[n][n];
-		
+
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
 				array[i][j] = isCoprime(i, j);
 			}
 		}
-		
+
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
 				StdOut.print(array[i][j] + " ");
@@ -28,25 +28,20 @@ public class Exercise30_ArrayExercise {
 			StdOut.println();
 		}
 	}
-	
+
 	private static boolean isCoprime(int i, int j) {
 		if (i == j) {
 			return false;
 		}
+		return i == 0 || j == 0 || gcd(i, j) == 1;
+	}
 
-		boolean isPrime = true;
-		
-		int smallestValue = (i < j)? i : j;
-		
-		for (int k = 2; k <= smallestValue; k++) {
-			
-			if (i % k == 0 && j % k == 0) {
-				isPrime = false;
-				break;
-			}
-			
+	private static int gcd(int number1, int number2) {
+		if (number2 == 0) {
+			return number1;
+		} else {
+			return gcd(number2, number1 % number2);
 		}
-		return isPrime;
 	}
 
 }
