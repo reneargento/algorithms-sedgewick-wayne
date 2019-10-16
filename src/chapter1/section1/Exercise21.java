09963dc8234dfc3d7e821747a0b4395971e81499
thiendao1407 <53300329+thiendao1407@users.noreply.github.com>
2019-10-17 06:36:27 +0700

    Exercise21.java - Working with text files
    I contribute a way to read data from text files.

--- src/chapter1/section1/Exercise21.java
+++ src/chapter1/section1/Exercise21.java
@@ -1,44 +1,47 @@
 package chapter1.section1;
 
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 
-/**
- * Created by Rene Argento
- */
 public class Exercise21 {
 
-	// Parameters example: "Rene 2 1 \nBacon 4 3 \nAbcdef 6 2"
+	/**
+     	* Reads in a sequence of strings from the whitelist file, specified as
+     	* a command-line argument; reads in strings from standard input;
+     	* prints to standard output a table with a column of the names, 
+	* the integers, and the result of dividing the first by the second,
+	* accurate to three decimal places.
+     	*/
+	
+	/**
+	 * Exercise21.txt
+	 * Rene 2 1 
+	 * Bacon 4 3 
+	 * Abcdef 6 2
+	 */
 	public static void main(String[] args) {
 
-		StdOut.printf("%8s %7s %7s %7s","Names","Number1","Number2","Result\n");
+		StdOut.printf("%8s %7s %7s %7s", "Names", "Number1", "Number2", "Result\n");
 
-		String[] allLines = args[0].split("\\n");
+		In in = new In("Exercise21.txt");
+		String[] allLines = in.readAllStrings();
 
-		for(String line: allLines) {
-			String formattedString = line.replace("\\n", "");
-			formattedPrint(formattedString);
-		}
+		formattedPrint(allLines);
+
 	}
-	
-	private static void formattedPrint(String lines) {
-		
-		String[] splittedValues = lines.split(" ");
-		
-		for (int i = 0; i < splittedValues.length; i++) {
-			if (i % 3 != 0) {
-				StdOut.printf("%8s",splittedValues[i]);
-			} else {
-				StdOut.printf("%7s",splittedValues[i]);
-			}
-			
+
+	private static void formattedPrint(String[] allLines) {
+
+		for (int i = 0; i < allLines.length; i++) {
+			StdOut.printf("%8s", allLines[i]);
 			if ((i + 1) % 3 == 0) {
-				double value1 = Double.parseDouble(splittedValues[i-1]);
-				double value2 = Double.parseDouble(splittedValues[i]);
+				double value1 = Double.parseDouble(allLines[i - 1]);
+				double value2 = Double.parseDouble(allLines[i]);
 				double result = value1 / value2;
-				StdOut.printf("%7.3f \n",result);
+				StdOut.printf("%7.3f \n", result);
 			}
 		}
-		
+
 	}
-	
+
 }
