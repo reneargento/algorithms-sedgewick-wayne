9ad7e14b080090824671e29d641c1fdd9c3b86ff
Rene Argento <rene.argento@gmail.com>
2019-10-18 11:24:09 +0200

    Minor improvements in exercise 1.1.21

--- src/chapter1/section1/Exercise21.java
+++ src/chapter1/section1/Exercise21.java
@@ -3,45 +3,37 @@ package chapter1.section1;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 
+/**
+ * Created by Rene Argento
+ * Updated by thiendao1407
+ */
 public class Exercise21 {
 
-	/**
-     	* Reads in a sequence of strings from the whitelist file, specified as
-     	* a command-line argument; reads in strings from standard input;
-     	* prints to standard output a table with a column of the names, 
-	* the integers, and the result of dividing the first by the second,
-	* accurate to three decimal places.
-     	*/
-	
-	/**
-	 * Exercise21.txt
-	 * Rene 2 1 
-	 * Bacon 4 3 
-	 * Abcdef 6 2
-	 */
+	// Parameters example:
+	// Rene 2 1
+	// Bacon 4 3
+	// Abcdef 6 2
 	public static void main(String[] args) {
-
 		StdOut.printf("%8s %7s %7s %7s", "Names", "Number1", "Number2", "Result\n");
 
-		In in = new In("Exercise21.txt");
-		String[] allLines = in.readAllStrings();
+		In in = new In();
 
-		formattedPrint(allLines);
-
+		while (in.hasNextLine()) {
+			String line = in.readLine();
+			String[] values = line.split(" ");
+			formattedPrint(values);
+		}
 	}
 
-	private static void formattedPrint(String[] allLines) {
+	private static void formattedPrint(String[] values) {
+		StdOut.printf("%8s", values[0]);
+		StdOut.printf("%8s", values[1]);
+		StdOut.printf("%8s", values[2]);
 
-		for (int i = 0; i < allLines.length; i++) {
-			StdOut.printf("%8s", allLines[i]);
-			if ((i + 1) % 3 == 0) {
-				double value1 = Double.parseDouble(allLines[i - 1]);
-				double value2 = Double.parseDouble(allLines[i]);
-				double result = value1 / value2;
-				StdOut.printf("%7.3f \n", result);
-			}
-		}
-
+		double value1 = Double.parseDouble(values[1]);
+		double value2 = Double.parseDouble(values[2]);
+		double result = value1 / value2;
+		StdOut.printf("%7.3f \n", result);
 	}
 
 }
