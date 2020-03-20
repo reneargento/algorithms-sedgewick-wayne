743d3752361a917040460b7bdc6ae0292b7274db
Rene Argento <rene.argento@gmail.com>
2020-03-21 00:17:03 +0100

    Updated exercise 2.2.16

--- src/chapter2/section2/Exercise16_NaturalMergesort.java
+++ src/chapter2/section2/Exercise16_NaturalMergesort.java
@@ -5,7 +5,10 @@ import util.ArrayGenerator;
 /**
  * Created by Rene Argento on 14/02/17.
  */
-// Running time: O(n * IS)
+// Thanks to svezyridis (https://github.com/svezyridis) for finding an improvement to the natural mergesort algorithm.
+// https://github.com/reneargento/algorithms-sedgewick-wayne/issues/101
+
+// Running time: O(n lg IS)
 // Array size = n
 // Number of maximal increasing sequences in the array = IS
 @SuppressWarnings("unchecked")
@@ -20,31 +23,34 @@ public class Exercise16_NaturalMergesort {
         if (array == null || array.length == 1) {
             return;
         }
-
         Comparable[] aux = new Comparable[array.length];
 
         int low = 0;
-        int middle = 0;
-        int high = 0;
+        int middle;
+        int high;
 
-        boolean secondSubArray = false;
-
-        for(int i = 1; i < array.length; i++) {
-
-            if (array[i].compareTo(array[i - 1]) < 0) {
-                if (!secondSubArray) {
-                    middle = i - 1;
-                    secondSubArray = true;
-                } else {
-                    high = i - 1;
-                    BottomUpMergeSort.merge(array, aux, low, middle, high);
-                    middle = high;
+        while (true) {
+            middle = findSortedSubArray(array, low);
+            if (middle == array.length - 1) {
+                if (low == 0) // Array is sorted
+                    break;
+                else {
+                    low = 0;
+                    continue;
                 }
             }
+            high = findSortedSubArray(array, middle + 1);
+            BottomUpMergeSort.merge(array, aux, low, middle, high);
+            low = (high == array.length - 1) ? 0 : high + 1;
         }
+    }
 
-        if (high != array.length - 1) {
-            BottomUpMergeSort.merge(array, aux, low, middle, array.length - 1);
+    private static int findSortedSubArray(Comparable[] array, int start) {
+        for(int i = start + 1; i < array.length; i++) {
+            if (array[i].compareTo(array[i - 1]) < 0) {
+                return i - 1;
+            }
         }
+        return array.length - 1;
     }
 }
