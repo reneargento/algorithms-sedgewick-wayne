15e4a7418e89fa522080496166c890d8f4cdbce9
ektasingh151 <65308335+ektasingh151@users.noreply.github.com>
2020-05-14 00:31:08 +0530

    Update Exercise12.java
    Hi,
    Please verify my solution and kindly comment on it.
    P.S: I am new to algorithms having 3 years of experience in java.

--- src/chapter1/section4/Exercise12.java
+++ src/chapter1/section4/Exercise12.java
@@ -3,52 +3,49 @@ package chapter1.section4;
 import edu.princeton.cs.algs4.StdOut;
 
 /**
- * Created by Rene Argento on 9/29/16.
+ * Created by ektasingh151 on 5/14/20.
  */
-public class Exercise12 {
+public class Exer1_4_12 {
 
-    public static void main(String[] args) {
-        int[] array1 = {-2, 1, 2, 2, 5, 6, 6, 8, 25};
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] array1 = {-2, 1, 2, 2, 5, 6, 6, 8, 25};
         int[] array2 = {0, 1, 2, 2, 2, 3, 4, 5, 10, 20, 25};
 
         printElementsThatAppearInBothArrays(array1, array2);
-        StdOut.println("\nExpected: 1 2 5 25");
-    }
+	}
 
-    private static void printElementsThatAppearInBothArrays(int[] array1, int[] array2) {
-
-        int array1Index = 0;
-        int array2Index = 0;
-
-        while(array1Index < array1.length && array2Index < array2.length) {
-
-            while(array1Index < array1.length && array1[array1Index] < array2[array2Index]) {
-                array1Index++;
-            }
-
-            if (array1Index < array1.length && array2Index < array2.length &&
-                    array1[array1Index] == array2[array2Index]) {
-                StdOut.print(array1[array1Index] + " ");
-
-                int equalNumber = array1[array1Index];
-
-                array1Index++;
-                array2Index++;
-
-                //Avoid printing the same number more than once
-                while(array1Index < array1.length && array1[array1Index] == equalNumber) {
-                    array1Index++;
-                }
-                while(array2Index < array2.length && array2[array2Index] == equalNumber) {
-                    array2Index++;
-                }
-            }
-
-            while(array1Index < array1.length && array2Index < array2.length &&
-                    array2[array2Index] < array1[array1Index]) {
-                array2Index++;
-            }
-        }
-    }
+	static void printElementsThatAppearInBothArrays(int [] array1, int [] array2) {
+		int array1Index = 0;
+		int array2Index = 0;
+		int recentMatchedValue = 0;
+		int count=0;
+		while(array1Index < array1.length && array2Index < array2.length) {
+			if(array1[array1Index] < array2[array2Index])
+				array1Index++;
+			
+			else if(array2[array2Index] < array1[array1Index])
+				array2Index++;
+			
+			else {
+				if(count == 0) {
+					System.out.print(array1[array1Index] +" ");
+					recentMatchedValue = array1[array1Index];
+					count++;
+				}
+				else if(recentMatchedValue != array1[array1Index]) {
+					System.out.print(array1[array1Index] +" ");
+					recentMatchedValue = array1[array1Index];
+				}
+				
+				
+				array1Index++;
+				array2Index++;
+			}
+		}
+		
+	}
+}
 
 }
