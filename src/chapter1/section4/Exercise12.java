b253335d1149949c053f7db74d1a7dc655b90c64
Rene Argento <rene.argento@gmail.com>
2020-05-14 17:15:46 +0200

    Updated exercise 1.4.12

--- src/chapter1/section4/Exercise12.java
+++ src/chapter1/section4/Exercise12.java
@@ -5,50 +5,37 @@ import edu.princeton.cs.algs4.StdOut;
 /**
  * Created by Rene Argento on 9/29/16.
  */
+// Thanks to ektasingh151 (https://github.com/ektasingh151) for suggesting a simpler solution:
+// https://github.com/reneargento/algorithms-sedgewick-wayne/pull/149
 public class Exercise12 {
 
     public static void main(String[] args) {
-        int[] array1 = {-2, 1, 2, 2, 5, 6, 6, 8, 25};
-        int[] array2 = {0, 1, 2, 2, 2, 3, 4, 5, 10, 20, 25};
+        int[] array1 = { 0, 1, 2, 2, 5, 6, 6, 8, 25, 25 };
+        int[] array2 = { -2, 0, 1, 2, 2, 2, 3, 4, 5, 10, 20, 25, 25 };
 
+        StdOut.print("Elements: ");
         printElementsThatAppearInBothArrays(array1, array2);
-        StdOut.println("\nExpected: 1 2 5 25");
+        StdOut.println("\nExpected: 0 1 2 5 25");
     }
 
     private static void printElementsThatAppearInBothArrays(int[] array1, int[] array2) {
-
         int array1Index = 0;
         int array2Index = 0;
+        Integer recentMatchedValue = null;
 
-        while(array1Index < array1.length && array2Index < array2.length) {
-
-            while(array1Index < array1.length && array1[array1Index] < array2[array2Index]) {
+        while (array1Index < array1.length && array2Index < array2.length) {
+            if (array1[array1Index] < array2[array2Index]) {
                 array1Index++;
-            }
-
-            if (array1Index < array1.length && array2Index < array2.length &&
-                    array1[array1Index] == array2[array2Index]) {
-                StdOut.print(array1[array1Index] + " ");
-
-                int equalNumber = array1[array1Index];
-
-                array1Index++;
+            } else if (array2[array2Index] < array1[array1Index]) {
                 array2Index++;
-
-                //Avoid printing the same number more than once
-                while(array1Index < array1.length && array1[array1Index] == equalNumber) {
-                    array1Index++;
+            } else {
+                if(recentMatchedValue == null || recentMatchedValue != array1[array1Index]) {
+                    StdOut.print(array1[array1Index] + " ");
+                    recentMatchedValue = array1[array1Index];
                 }
-                while(array2Index < array2.length && array2[array2Index] == equalNumber) {
-                    array2Index++;
-                }
-            }
-
-            while(array1Index < array1.length && array2Index < array2.length &&
-                    array2[array2Index] < array1[array1Index]) {
+                array1Index++;
                 array2Index++;
             }
         }
     }
-
 }
