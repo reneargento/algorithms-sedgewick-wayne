655d8b3927ea461ce0cef9ea74f47f4d3451b14c
Rene Argento <rene.argento@gmail.com>
2020-05-19 19:44:03 +0200

    Updated the second part of exercise 1.4.15 2

--- src/chapter1/section4/Exercise15_2_ThreeSumFaster.java
+++ src/chapter1/section4/Exercise15_2_ThreeSumFaster.java
@@ -17,18 +17,21 @@ public class Exercise15_2_ThreeSumFaster {
         int[] arrayTest1 = {-3, -2, 2, 3, 5, 99};
         int[] arrayTest2 = {-10, -10, -10, 10};
         int[] arrayTest3 = {0, 0, 0, 0, 0, 0, 0};
+        int[] arrayTest4 = {-2, -1, 0, 0, 0, 0, 0, 0, 3};
 
         StdOut.println("Method 1");
         StdOut.println("ThreeSumFaster: " + threeSumFaster(array) + " Expected: 8");
         StdOut.println("ThreeSumFaster: " + threeSumFaster(arrayTest1) + " Expected: 1");
         StdOut.println("ThreeSumFaster: " + threeSumFaster(arrayTest2) + " Expected: 0");
         StdOut.println("ThreeSumFaster: " + threeSumFaster(arrayTest3) + " Expected: 35");
+        StdOut.println("ThreeSumFaster: " + threeSumFaster(arrayTest4) + " Expected: 21");
 
         StdOut.println("\nMethod 2");
         StdOut.println("ThreeSumFaster: " + threeSumFaster2(array) + " Expected: 8");
         StdOut.println("ThreeSumFaster: " + threeSumFaster2(arrayTest1) + " Expected: 1");
         StdOut.println("ThreeSumFaster: " + threeSumFaster2(arrayTest2) + " Expected: 0");
         StdOut.println("ThreeSumFaster: " + threeSumFaster2(arrayTest3) + " Expected: 35");
+        StdOut.println("ThreeSumFaster: " + threeSumFaster2(arrayTest4) + " Expected: 21");
     }
 
     // Runs in O(n^2) when there are no duplicate keys and in O(n^3) when there are duplicate keys.
@@ -65,14 +68,10 @@ public class Exercise15_2_ThreeSumFaster {
     // Considering that the array is already sorted.
     // Runs in O(n^2).
     private static int threeSumFaster2(int[] array) {
-        if (isAllZeros(array)) {
-            return handleAllZerosEdgeCase(array.length);
-        }
-
         int start = 0;
         int end = array.length - 1;
 
-        int count = 0;
+        int count = countZeroMatches(array);
 
         if ((array[start] > 0 && array[end] > 0) || (array[start] < 0 && array[end] < 0)) {
             return 0;
@@ -88,6 +87,12 @@ public class Exercise15_2_ThreeSumFaster {
                 } else if (array[i] + array[start] + array[end] < 0) {
                     start++;
                 } else {
+                    if (array[start] == 0 && array[end] == 0) {
+                        start++;
+                        end--;
+                        continue;
+                    }
+
                     int startElement = array[start];
                     int equalStartElements = 1;
 
@@ -116,18 +121,20 @@ public class Exercise15_2_ThreeSumFaster {
         return count;
     }
 
-    private static boolean isAllZeros(int[] array) {
+    private static int countZeroMatches(int[] array) {
+        int count = 0;
+
         for (int value : array) {
-            if (value != 0) {
-                return false;
+            if (value == 0) {
+                count++;
             }
         }
-        return true;
+        return handleZerosEdgeCase(count);
     }
 
     // Based on the tetrahedral numbers formula
-    private static int handleAllZerosEdgeCase(int arrayLength) {
-        return ((arrayLength - 2) * (arrayLength - 1) * arrayLength) / 6;
+    private static int handleZerosEdgeCase(int numberOfZeros) {
+        return ((numberOfZeros - 2) * (numberOfZeros - 1) * numberOfZeros) / 6;
     }
 
 }
