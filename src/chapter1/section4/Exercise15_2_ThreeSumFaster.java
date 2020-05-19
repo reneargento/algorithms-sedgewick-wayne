c722b5025d25a41e6ca7a61c61cb75fa0a98cb34
Rene Argento <rene.argento@gmail.com>
2020-05-19 19:27:56 +0200

    Updated the second part of exercise 1.4.15

--- src/chapter1/section4/Exercise15_2_ThreeSumFaster.java
+++ src/chapter1/section4/Exercise15_2_ThreeSumFaster.java
@@ -16,16 +16,19 @@ public class Exercise15_2_ThreeSumFaster {
         int[] array = {-10, -10, -5, 0, 5, 10, 10, 15, 20};
         int[] arrayTest1 = {-3, -2, 2, 3, 5, 99};
         int[] arrayTest2 = {-10, -10, -10, 10};
+        int[] arrayTest3 = {0, 0, 0, 0, 0, 0, 0};
 
         StdOut.println("Method 1");
         StdOut.println("ThreeSumFaster: " + threeSumFaster(array) + " Expected: 8");
         StdOut.println("ThreeSumFaster: " + threeSumFaster(arrayTest1) + " Expected: 1");
         StdOut.println("ThreeSumFaster: " + threeSumFaster(arrayTest2) + " Expected: 0");
+        StdOut.println("ThreeSumFaster: " + threeSumFaster(arrayTest3) + " Expected: 35");
 
         StdOut.println("\nMethod 2");
         StdOut.println("ThreeSumFaster: " + threeSumFaster2(array) + " Expected: 8");
         StdOut.println("ThreeSumFaster: " + threeSumFaster2(arrayTest1) + " Expected: 1");
         StdOut.println("ThreeSumFaster: " + threeSumFaster2(arrayTest2) + " Expected: 0");
+        StdOut.println("ThreeSumFaster: " + threeSumFaster2(arrayTest3) + " Expected: 35");
     }
 
     // Runs in O(n^2) when there are no duplicate keys and in O(n^3) when there are duplicate keys.
@@ -56,19 +59,18 @@ public class Exercise15_2_ThreeSumFaster {
                 }
             }
         }
-
         return count;
     }
 
     // Considering that the array is already sorted.
-    // Runs in O(n^2) when there are no duplicate keys and in O(n^3) when there are duplicate keys.
-    // The exercise asks to avoid using binary search. If we used binary search to get the ranges we could
-    // reduce the complexity to O(n^2 lg n) when there are duplicate keys.
+    // Runs in O(n^2).
     private static int threeSumFaster2(int[] array) {
+        if (isAllZeros(array)) {
+            return handleAllZerosEdgeCase(array.length);
+        }
+
         int start = 0;
         int end = array.length - 1;
-
-        int tempIndex;
 
         int count = 0;
 
@@ -76,46 +78,56 @@ public class Exercise15_2_ThreeSumFaster {
             return 0;
         }
 
-        for(int i = 0; i < array.length; i++) {
+        for (int i = 0; i < array.length; i++) {
             start = i + 1;
             end = array.length - 1;
 
-            while(start < end) {
+            while (start < end) {
                 if (array[i] + array[start] + array[end] > 0) {
                     end--;
                 } else if (array[i] + array[start] + array[end] < 0) {
                     start++;
                 } else {
-                    StdOut.println(array[i] + " " + array[start] + " " + array[end]);
-                    count++;
+                    int startElement = array[start];
+                    int equalStartElements = 1;
 
-                    // Compare all following elements with array[end]
-                    // Could be improved with binary search
-                    tempIndex = start + 1;
-                    while(tempIndex < end && array[i] + array[tempIndex] + array[end] == 0) {
-                        StdOut.println(array[i] + " " + array[tempIndex] + " " + array[end]);
-
-                        count++;
-                        tempIndex++;
+                    // Compare all following elements with startElement
+                    while (start + 1 < end && array[start + 1] == startElement) {
+                        equalStartElements++;
+                        start++;
                     }
 
-                    // Compare all previous elements with array[start]
-                    // Could be improved with binary search
-                    tempIndex = end - 1;
-                    while(tempIndex > start && array[i] + array[start] + array[tempIndex] == 0) {
-                        StdOut.println(array[i] + " " + array[start] + " " + array[tempIndex]);
+                    int endElement = array[end];
+                    int equalEndElements = 1;
 
-                        count++;
-                        tempIndex--;
+                    // Compare all previous elements with endElement
+                    while (end - 1 > start && array[end - 1] == endElement) {
+                        equalEndElements++;
+                        end--;
                     }
+
+                    count += equalStartElements * equalEndElements;
 
                     start++;
                     end--;
                 }
             }
         }
-
         return count;
+    }
+
+    private static boolean isAllZeros(int[] array) {
+        for (int value : array) {
+            if (value != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Based on the tetrahedral numbers formula
+    private static int handleAllZerosEdgeCase(int arrayLength) {
+        return ((arrayLength - 2) * (arrayLength - 1) * arrayLength) / 6;
     }
 
 }
