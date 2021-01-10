c4247a13cc66bdccaf7ba44e4dab3cdd1475fb2f
Amirali Tavakoli <amiralitavakoli80@gmail.com>
2021-01-10 22:40:32 +0330

    change zero to one to avoid additional useless check

--- src/chapter2/section1/Exercise25_InsertionSortWithoutExchanges.java
+++ src/chapter2/section1/Exercise25_InsertionSortWithoutExchanges.java
@@ -20,7 +20,7 @@ public class Exercise25_InsertionSortWithoutExchanges {
 
     private static void insertionSort(Comparable[] array) {
 
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 1; i < array.length; i++) {
             for (int j = i; j > 0 && array[j].compareTo(array[j - 1]) < 0; j--) {
                 Comparable temp = array[j];
                 array[j] = array[j - 1];
