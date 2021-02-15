bd4e0842bca005b0ccecfa5e514951c4fe754a4f
Rene Argento <rene.argento@gmail.com>
2021-02-15 19:22:22 +0100

    Updated exercises 1.2.16 and 1.2.17

--- src/chapter1/section2/Exercise16_RationalNumbers.java
+++ src/chapter1/section2/Exercise16_RationalNumbers.java
@@ -7,135 +7,134 @@ import edu.princeton.cs.algs4.StdOut;
  */
 public class Exercise16_RationalNumbers {
 
-	private long numerator;
-	private long denominator;
-	
-	public Exercise16_RationalNumbers(int numerator, int denominator) {
-		if (denominator == 0) {
-			throw new RuntimeException("Denominator cannot be 0");
-		}
-		
-		this.numerator = numerator;
-		this.denominator = denominator;
-		
-		int gcd = gcd(numerator, denominator);
-		this.numerator /= gcd;
-		this.denominator /= gcd;
-		
-		if (this.denominator < 0) {
-			this.denominator = -1 * this.denominator;
-			this.numerator = -1 * this.numerator;
-		}
-	}
-	
-	private static int gcd(int numerator, int denominator) {
-		if (denominator == 0) {
-			return numerator;
-		} else {
-			return gcd(denominator, numerator % denominator);
-		}
-	}
-	
-	public int numerator() {
-		return (int) numerator;
-	}
-	
-	public int denominator() {
-		return (int) denominator;
-	}
-	
-	public Exercise16_RationalNumbers plus(Exercise16_RationalNumbers b) {
-		int newNumeratorA = this.numerator() * b.denominator();
-		int newNumeratorB = b.numerator() * this.denominator();
-		
-		int resultNumerator = newNumeratorA + newNumeratorB;
-		
-		int resultDenominator = this.denominator() * b.denominator();
+	private class RationalNumbers {
+		private long numerator;
+		private long denominator;
 
-		Exercise16_RationalNumbers plusResult = new Exercise16_RationalNumbers(resultNumerator, resultDenominator);
-		return plusResult;
-	}
-	
-	public Exercise16_RationalNumbers minus(Exercise16_RationalNumbers b) {
-		int newNumeratorA = this.numerator() * b.denominator();
-		int newNumeratorB = b.numerator() * this.denominator();
-		
-		int resultNumerator = newNumeratorA - newNumeratorB;
-		
-		int resultDenominator = this.denominator() * b.denominator();
+		public RationalNumbers(int numerator, int denominator) {
+			if (denominator == 0) {
+				throw new RuntimeException("Denominator cannot be 0");
+			}
 
-		Exercise16_RationalNumbers minusResult = new Exercise16_RationalNumbers(resultNumerator, resultDenominator);
-		return minusResult;
-	}
-	
-	public Exercise16_RationalNumbers times(Exercise16_RationalNumbers b) {
-		int resultNumerator = this.numerator() * b.numerator();
-		int resultDenominator = this.denominator() * b.denominator();
+			this.numerator = numerator;
+			this.denominator = denominator;
 
-		Exercise16_RationalNumbers minusResult = new Exercise16_RationalNumbers(resultNumerator, resultDenominator);
-		return minusResult;
-	}
-	
-	public Exercise16_RationalNumbers dividedBy(Exercise16_RationalNumbers b) {
-		Exercise16_RationalNumbers bReciprocal = new Exercise16_RationalNumbers(b.denominator(), b.numerator());
-		
-		return times(bReciprocal);
-	}
-	
-	public boolean equals(Object x) {
-		if (this == x) {
+			int gcd = gcd(numerator, denominator);
+			this.numerator /= gcd;
+			this.denominator /= gcd;
+
+			if (this.denominator < 0) {
+				this.denominator = -1 * this.denominator;
+				this.numerator = -1 * this.numerator;
+			}
+		}
+
+		private int gcd(int numerator, int denominator) {
+			if (denominator == 0) {
+				return numerator;
+			} else {
+				return gcd(denominator, numerator % denominator);
+			}
+		}
+
+		public int numerator() {
+			return (int) numerator;
+		}
+
+		public int denominator() {
+			return (int) denominator;
+		}
+
+		public RationalNumbers plus(RationalNumbers b) {
+			int newNumeratorA = this.numerator() * b.denominator();
+			int newNumeratorB = b.numerator() * this.denominator();
+
+			int resultNumerator = newNumeratorA + newNumeratorB;
+			int resultDenominator = this.denominator() * b.denominator();
+
+			return new RationalNumbers(resultNumerator, resultDenominator);
+		}
+
+		public RationalNumbers minus(RationalNumbers b) {
+			int newNumeratorA = this.numerator() * b.denominator();
+			int newNumeratorB = b.numerator() * this.denominator();
+
+			int resultNumerator = newNumeratorA - newNumeratorB;
+			int resultDenominator = this.denominator() * b.denominator();
+
+			return new RationalNumbers(resultNumerator, resultDenominator);
+		}
+
+		public RationalNumbers times(RationalNumbers b) {
+			int resultNumerator = this.numerator() * b.numerator();
+			int resultDenominator = this.denominator() * b.denominator();
+
+			return new RationalNumbers(resultNumerator, resultDenominator);
+		}
+
+		public RationalNumbers dividedBy(RationalNumbers b) {
+			RationalNumbers bReciprocal = new RationalNumbers(b.denominator(), b.numerator());
+			return times(bReciprocal);
+		}
+
+		public boolean equals(Object x) {
+			if (this == x) {
+				return true;
+			}
+			if (x == null) {
+				return false;
+			}
+			if (this.getClass() != x.getClass()) {
+				return false;
+			}
+
+			RationalNumbers that = (RationalNumbers) x;
+			if (this.numerator() != that.numerator()) {
+				return false;
+			}
+			if (this.denominator() != that.denominator()) {
+				return false;
+			}
 			return true;
 		}
-		if (x == null) {
-			return false;
+
+		public String toString() {
+			return this.numerator() + "/" + this.denominator();
 		}
-		if (this.getClass() != x.getClass()) {
-			return false;
-		}
-		
-		Exercise16_RationalNumbers that = (Exercise16_RationalNumbers) x;
-		if (this.numerator() != that.numerator()) {
-			return false;
-		}
-		if (this.denominator() != that.denominator()) {
-			return false;
-		}
-		return true;
-	}
-	
-	public String toString() {
-		return this.numerator() + "/" + this.denominator();
 	}
 	
 	public static void main(String[] args) {
-		Exercise16_RationalNumbers rational1 = new Exercise16_RationalNumbers(1, 4);
-		Exercise16_RationalNumbers rational2 = new Exercise16_RationalNumbers(2, 3);
-		
-		Exercise16_RationalNumbers rationalPlus = rational1.plus(rational2);
+		new Exercise16_RationalNumbers().testRationalNumbers();
+	}
+
+	private void testRationalNumbers() {
+		RationalNumbers rational1 = new RationalNumbers(1, 4);
+		RationalNumbers rational2 = new RationalNumbers(2, 3);
+
+		RationalNumbers rationalPlus = rational1.plus(rational2);
 		StdOut.println("Plus test: 1/4 + 2/3 = " + rationalPlus.numerator + "/" + rationalPlus.denominator
 				+ " Expected: 11/12");
-		
-		Exercise16_RationalNumbers rationalMinus = rational1.minus(rational2);
+
+		RationalNumbers rationalMinus = rational1.minus(rational2);
 		StdOut.println("Minus test: 1/4 - 2/3 = " + rationalMinus.numerator + "/" + rationalMinus.denominator
 				+ " Expected: -5/12");
 
-		Exercise16_RationalNumbers rationalTimes = rational1.times(rational2);
+		RationalNumbers rationalTimes = rational1.times(rational2);
 		StdOut.println("Times test: 1/4 * 2/3 = " + rationalTimes.numerator + "/" + rationalTimes.denominator
 				+ " Expected: 1/6");
-		
-		Exercise16_RationalNumbers rationalDividedBy = rational1.dividedBy(rational2);
-		StdOut.println("Divided by test: 1/4 -/ 2/3 = " + rationalDividedBy.numerator + "/" + rationalDividedBy.denominator
+
+		RationalNumbers rationalDividedBy = rational1.dividedBy(rational2);
+		StdOut.println("Divided by test: 1/4 / 2/3 = " + rationalDividedBy.numerator + "/" + rationalDividedBy.denominator
 				+ " Expected: 3/8");
-		
-		Exercise16_RationalNumbers equalRational1 = rational1;
-		Exercise16_RationalNumbers equalRational2 = new Exercise16_RationalNumbers(rational1.numerator(), rational1.denominator());
-		Exercise16_RationalNumbers nonEqualRational = new Exercise16_RationalNumbers(7, 8);
+
+		RationalNumbers equalRational1 = rational1;
+		RationalNumbers equalRational2 = new RationalNumbers(rational1.numerator(), rational1.denominator());
+		RationalNumbers nonEqualRational = new RationalNumbers(7, 8);
 		StdOut.println("Equal test 1 = " + rational1.equals(equalRational1) + " Expected: true");
 		StdOut.println("Equal test 2 = " + rational1.equals(equalRational2) + " Expected: true");
 		StdOut.println("Equal test 3 = " + rational1.equals(nonEqualRational) + " Expected: false");
-		
+
 		StdOut.println("toString() test 1 = " + rational1 + " Expected: 1/4");
 		StdOut.println("toString() test 2 = " + rational2 + " Expected: 2/3");
 	}
-
 }
--- src/chapter1/section2/Exercise17_RationalNumbersRobust.java
+++ src/chapter1/section2/Exercise17_RationalNumbersRobust.java
@@ -6,172 +6,164 @@ import edu.princeton.cs.algs4.StdOut;
  * Created by Rene Argento
  */
 public class Exercise17_RationalNumbersRobust {
-	
-	private long numerator;
-	private long denominator;
-	
-	private final static String ASSERT_AVOIDING_OVERFLOW_MESSAGE = "Operation would cause overflow";
-	
-	public Exercise17_RationalNumbersRobust(int numerator, int denominator) {
-		if (denominator == 0) {
-			throw new RuntimeException("Denominator cannot be 0");
+
+	private class RationalNumbersRobust {
+		private long numerator;
+		private long denominator;
+
+		private final static String ASSERT_AVOIDING_OVERFLOW_MESSAGE = "Operation would cause overflow";
+
+		public RationalNumbersRobust(int numerator, int denominator) {
+			if (denominator == 0) {
+				throw new RuntimeException("Denominator cannot be 0");
+			}
+
+			this.numerator = numerator;
+			this.denominator = denominator;
+
+			int gcd = gcd(numerator, denominator);
+			this.numerator /= gcd;
+			this.denominator /= gcd;
+
+			if (this.denominator < 0) {
+				this.denominator = -1 * this.denominator;
+				this.numerator = -1 * this.numerator;
+			}
 		}
-		
-		this.numerator = numerator;
-		this.denominator = denominator;
-		
-		int gcd = gcd(numerator, denominator);
-		this.numerator /= gcd;
-		this.denominator /= gcd;
-		
-		if (this.denominator < 0) {
-			this.denominator = -1 * this.denominator;
-			this.numerator = -1 * this.numerator;
+
+		private int gcd(int numerator, int denominator) {
+			if (denominator == 0) {
+				return numerator;
+			} else {
+				return gcd(denominator, numerator % denominator);
+			}
 		}
-	}
-	
-	private static int gcd(int numerator, int denominator) {
-		if (denominator == 0) {
-			return numerator;
-		} else {
-			return gcd(denominator, numerator % denominator);
+
+		public int numerator() {
+			return (int) numerator;
 		}
-	}
-	
-	public int numerator() {
-		return (int) numerator;
-	}
-	
-	public int denominator() {
-		return (int) denominator;
-	}
-	
-	public Exercise17_RationalNumbersRobust plus(Exercise17_RationalNumbersRobust b) {
-		
-		assert this.numerator * b.denominator <= Integer.MAX_VALUE : ASSERT_AVOIDING_OVERFLOW_MESSAGE;
-		assert b.numerator * this.denominator <= Integer.MAX_VALUE : ASSERT_AVOIDING_OVERFLOW_MESSAGE;
-		assert this.numerator * b.denominator >= Integer.MIN_VALUE : ASSERT_AVOIDING_OVERFLOW_MESSAGE;
-		assert b.numerator * this.denominator >= Integer.MIN_VALUE : ASSERT_AVOIDING_OVERFLOW_MESSAGE;
 
-		
-		long newNumeratorA = this.numerator() * b.denominator();
-		long newNumeratorB = b.numerator() * this.denominator();
-		
-		assert newNumeratorA + newNumeratorB <= Integer.MAX_VALUE : ASSERT_AVOIDING_OVERFLOW_MESSAGE;
-		assert newNumeratorA + newNumeratorB >= Integer.MIN_VALUE : ASSERT_AVOIDING_OVERFLOW_MESSAGE;
+		public int denominator() {
+			return (int) denominator;
+		}
 
-		int resultNumerator = (int) (newNumeratorA + newNumeratorB);
-		
-		assert this.denominator * b.denominator <= Integer.MAX_VALUE : ASSERT_AVOIDING_OVERFLOW_MESSAGE;		
-		assert this.denominator * b.denominator >= Integer.MIN_VALUE : ASSERT_AVOIDING_OVERFLOW_MESSAGE;
-		
-		int resultDenominator = this.denominator() * b.denominator();
+		public RationalNumbersRobust plus(RationalNumbersRobust b) {
+			assert this.numerator * b.denominator <= Integer.MAX_VALUE : ASSERT_AVOIDING_OVERFLOW_MESSAGE;
+			assert b.numerator * this.denominator <= Integer.MAX_VALUE : ASSERT_AVOIDING_OVERFLOW_MESSAGE;
+			assert this.numerator * b.denominator >= Integer.MIN_VALUE : ASSERT_AVOIDING_OVERFLOW_MESSAGE;
+			assert b.numerator * this.denominator >= Integer.MIN_VALUE : ASSERT_AVOIDING_OVERFLOW_MESSAGE;
 
-		Exercise17_RationalNumbersRobust plusResult = new Exercise17_RationalNumbersRobust(resultNumerator, resultDenominator);
-		return plusResult;
-	}
-	
-	public Exercise17_RationalNumbersRobust minus(Exercise17_RationalNumbersRobust b) {
-		
-		assert this.numerator * b.denominator <= Integer.MAX_VALUE : ASSERT_AVOIDING_OVERFLOW_MESSAGE;
-		assert b.numerator * this.denominator <= Integer.MAX_VALUE : ASSERT_AVOIDING_OVERFLOW_MESSAGE;
-		assert this.numerator * b.denominator >= Integer.MIN_VALUE : ASSERT_AVOIDING_OVERFLOW_MESSAGE;
-		assert b.numerator * this.denominator >= Integer.MIN_VALUE : ASSERT_AVOIDING_OVERFLOW_MESSAGE;
-		
-		long newNumeratorA = this.numerator() * b.denominator();
-		long newNumeratorB = b.numerator() * this.denominator();
-		
-		assert newNumeratorA - newNumeratorB >= Integer.MIN_VALUE : ASSERT_AVOIDING_OVERFLOW_MESSAGE;
-		
-		int resultNumerator = (int) (newNumeratorA - newNumeratorB);
-		
-		assert this.denominator * b.denominator <= Integer.MAX_VALUE : ASSERT_AVOIDING_OVERFLOW_MESSAGE;
-		
-		int resultDenominator = this.denominator() * b.denominator();
+			long newNumeratorA = this.numerator() * b.denominator();
+			long newNumeratorB = b.numerator() * this.denominator();
 
-		Exercise17_RationalNumbersRobust minusResult = new Exercise17_RationalNumbersRobust(resultNumerator, resultDenominator);
-		return minusResult;
-	}
-	
-	public Exercise17_RationalNumbersRobust times(Exercise17_RationalNumbersRobust b) {
-		
-		assert this.numerator * b.numerator <= Integer.MAX_VALUE : ASSERT_AVOIDING_OVERFLOW_MESSAGE;
-		assert this.denominator * b.denominator <= Integer.MAX_VALUE : ASSERT_AVOIDING_OVERFLOW_MESSAGE;
-		assert this.numerator * b.numerator >= Integer.MIN_VALUE : ASSERT_AVOIDING_OVERFLOW_MESSAGE;
-		assert this.denominator * b.denominator >= Integer.MIN_VALUE : ASSERT_AVOIDING_OVERFLOW_MESSAGE;
-		
-		int resultNumerator = this.numerator() * b.numerator();
-		int resultDenominator = this.denominator() * b.denominator();
+			assert newNumeratorA + newNumeratorB <= Integer.MAX_VALUE : ASSERT_AVOIDING_OVERFLOW_MESSAGE;
+			assert newNumeratorA + newNumeratorB >= Integer.MIN_VALUE : ASSERT_AVOIDING_OVERFLOW_MESSAGE;
+			int resultNumerator = (int) (newNumeratorA + newNumeratorB);
 
-		Exercise17_RationalNumbersRobust minusResult = new Exercise17_RationalNumbersRobust(resultNumerator, resultDenominator);
-		return minusResult;
-	}
-	
-	public Exercise17_RationalNumbersRobust dividedBy(Exercise17_RationalNumbersRobust b) {
-		Exercise17_RationalNumbersRobust bReciprocal = new Exercise17_RationalNumbersRobust(b.denominator(), b.numerator());
-		
-		return times(bReciprocal);
-	}
-	
-	public boolean equals(Object x) {
-		if (this == x) {
+			assert this.denominator * b.denominator <= Integer.MAX_VALUE : ASSERT_AVOIDING_OVERFLOW_MESSAGE;
+			assert this.denominator * b.denominator >= Integer.MIN_VALUE : ASSERT_AVOIDING_OVERFLOW_MESSAGE;
+
+			int resultDenominator = this.denominator() * b.denominator();
+			return new RationalNumbersRobust(resultNumerator, resultDenominator);
+		}
+
+		public RationalNumbersRobust minus(RationalNumbersRobust b) {
+			assert this.numerator * b.denominator <= Integer.MAX_VALUE : ASSERT_AVOIDING_OVERFLOW_MESSAGE;
+			assert b.numerator * this.denominator <= Integer.MAX_VALUE : ASSERT_AVOIDING_OVERFLOW_MESSAGE;
+			assert this.numerator * b.denominator >= Integer.MIN_VALUE : ASSERT_AVOIDING_OVERFLOW_MESSAGE;
+			assert b.numerator * this.denominator >= Integer.MIN_VALUE : ASSERT_AVOIDING_OVERFLOW_MESSAGE;
+
+			long newNumeratorA = this.numerator() * b.denominator();
+			long newNumeratorB = b.numerator() * this.denominator();
+
+			assert newNumeratorA - newNumeratorB >= Integer.MIN_VALUE : ASSERT_AVOIDING_OVERFLOW_MESSAGE;
+			int resultNumerator = (int) (newNumeratorA - newNumeratorB);
+
+			assert this.denominator * b.denominator <= Integer.MAX_VALUE : ASSERT_AVOIDING_OVERFLOW_MESSAGE;
+			int resultDenominator = this.denominator() * b.denominator();
+
+			return new RationalNumbersRobust(resultNumerator, resultDenominator);
+		}
+
+		public RationalNumbersRobust times(RationalNumbersRobust b) {
+			assert this.numerator * b.numerator <= Integer.MAX_VALUE : ASSERT_AVOIDING_OVERFLOW_MESSAGE;
+			assert this.denominator * b.denominator <= Integer.MAX_VALUE : ASSERT_AVOIDING_OVERFLOW_MESSAGE;
+			assert this.numerator * b.numerator >= Integer.MIN_VALUE : ASSERT_AVOIDING_OVERFLOW_MESSAGE;
+			assert this.denominator * b.denominator >= Integer.MIN_VALUE : ASSERT_AVOIDING_OVERFLOW_MESSAGE;
+
+			int resultNumerator = this.numerator() * b.numerator();
+			int resultDenominator = this.denominator() * b.denominator();
+			return new RationalNumbersRobust(resultNumerator, resultDenominator);
+		}
+
+		public RationalNumbersRobust dividedBy(RationalNumbersRobust b) {
+			RationalNumbersRobust bReciprocal = new RationalNumbersRobust(b.denominator(), b.numerator());
+			return times(bReciprocal);
+		}
+
+		public boolean equals(Object x) {
+			if (this == x) {
+				return true;
+			}
+			if (x == null) {
+				return false;
+			}
+			if (this.getClass() != x.getClass()) {
+				return false;
+			}
+
+			RationalNumbersRobust that = (RationalNumbersRobust) x;
+			if (this.numerator() != that.numerator()) {
+				return false;
+			}
+			if (this.denominator() != that.denominator()) {
+				return false;
+			}
 			return true;
 		}
-		if (x == null) {
-			return false;
-		}
-		if (this.getClass() != x.getClass()) {
-			return false;
-		}
 
-		Exercise17_RationalNumbersRobust that = (Exercise17_RationalNumbersRobust) x;
-		if (this.numerator() != that.numerator()) {
-			return false;
+		public String toString() {
+			return this.numerator() + "/" + this.denominator();
 		}
-		if (this.denominator() != that.denominator()) {
-			return false;
-		}
-		return true;
 	}
-	
-	public String toString() {
-		return this.numerator() + "/" + this.denominator();
-	}
-	
+
 	public static void main(String[] args) {
-		Exercise17_RationalNumbersRobust rational1 = new Exercise17_RationalNumbersRobust(1, 4);
-		Exercise17_RationalNumbersRobust rational2 = new Exercise17_RationalNumbersRobust(2, 3);
+		new Exercise17_RationalNumbersRobust().testRationalNumbersRobust();
+	}
 
-		Exercise17_RationalNumbersRobust rationalPlus = rational1.plus(rational2);
+	private void testRationalNumbersRobust() {
+		RationalNumbersRobust rational1 = new RationalNumbersRobust(1, 4);
+		RationalNumbersRobust rational2 = new RationalNumbersRobust(2, 3);
+
+		RationalNumbersRobust rationalPlus = rational1.plus(rational2);
 		StdOut.println("Plus test: 1/4 + 2/3 = " + rationalPlus.numerator + "/" + rationalPlus.denominator
 				+ " Expected: 11/12");
 
-		Exercise17_RationalNumbersRobust rationalMinus = rational1.minus(rational2);
+		RationalNumbersRobust rationalMinus = rational1.minus(rational2);
 		StdOut.println("Minus test: 1/4 - 2/3 = " + rationalMinus.numerator + "/" + rationalMinus.denominator
 				+ " Expected: -5/12");
 
-		Exercise17_RationalNumbersRobust rationalTimes = rational1.times(rational2);
+		RationalNumbersRobust rationalTimes = rational1.times(rational2);
 		StdOut.println("Times test: 1/4 * 2/3 = " + rationalTimes.numerator + "/" + rationalTimes.denominator
 				+ " Expected: 1/6");
 
-		Exercise17_RationalNumbersRobust rationalDividedBy = rational1.dividedBy(rational2);
+		RationalNumbersRobust rationalDividedBy = rational1.dividedBy(rational2);
 		StdOut.println("Divided by test: 1/4 / 2/3 = " + rationalDividedBy.numerator + "/" + rationalDividedBy.denominator
 				+ " Expected: 3/8");
 
-		Exercise17_RationalNumbersRobust equalRational1 = rational1;
-		Exercise17_RationalNumbersRobust equalRational2 = new Exercise17_RationalNumbersRobust(rational1.numerator(), rational1.denominator());
-		Exercise17_RationalNumbersRobust nonEqualRational = new Exercise17_RationalNumbersRobust(7, 8);
+		RationalNumbersRobust equalRational1 = rational1;
+		RationalNumbersRobust equalRational2 = new RationalNumbersRobust(rational1.numerator(), rational1.denominator());
+		RationalNumbersRobust nonEqualRational = new RationalNumbersRobust(7, 8);
 		StdOut.println("Equal test 1 = " + rational1.equals(equalRational1) + " Expected: true");
 		StdOut.println("Equal test 2 = " + rational1.equals(equalRational2) + " Expected: true");
 		StdOut.println("Equal test 3 = " + rational1.equals(nonEqualRational) + " Expected: false");
-		
+
 		StdOut.println("toString() test 1 = " + rational1 + " Expected: 1/4");
 		StdOut.println("toString() test 2 = " + rational2 + " Expected: 2/3");
-		
-		//Would cause an overflow -to test, comment the GCD divisions on the constructor
-		Exercise17_RationalNumbersRobust rational3 = new Exercise17_RationalNumbersRobust(Integer.MAX_VALUE, Integer.MAX_VALUE);
-		//Exercise17 rationalPlusOverflow = rational1.plus(rational3);
-		//StdOut.println("toString() test 3 = " + rationalPlusOverflow);
-	}
 
+		// Would cause an overflow
+		RationalNumbersRobust r1 = new RationalNumbersRobust(-2147483648, 1);
+		RationalNumbersRobust r2 = new RationalNumbersRobust(-1, 1);
+		// r1.plus(r2);
+	}
 }
