68704cf36b2b193292ede6b43ebf46cbe025deaf
Rene Argento <rene.argento@gmail.com>
2021-06-15 18:09:32 +0200

    Updated exercise 1.3.21

--- src/chapter1/section3/Exercise21.java
+++ src/chapter1/section3/Exercise21.java
@@ -1,130 +1,32 @@
 package chapter1.section3;
 
-import java.util.Iterator;
-
 import edu.princeton.cs.algs4.StdOut;
 
 /**
  * Created by Rene Argento
  */
-public class Exercise21<Item> implements Iterable<Item> {
+// Thanks to Aaron-He (https://github.com/Aaron-He) for correcting the find() method signature.
+// https://github.com/reneargento/algorithms-sedgewick-wayne/issues/202
+public class Exercise21 {
 	
-	private class Node {
-		Item item;
-		Node next;
-	}
-	
-	private int size;
-	private Node first;
-	
-	public boolean isEmpty() {
-		return size == 0;
-	}
-	
-	public int size() {
-		return size;
-	}
-	
-	public void add(Item item) {
-		if (isEmpty()) {
-			first = new Node();
-			first.item = item;
-		} else {	
-			Node current;
-			for(current = first; current.next != null; current = current.next);
-			
-			Node newNode = new Node();
-			newNode.item = item;
-			current.next = newNode;
-		}
-		size++;
-	}
-	
-	public void deleteLastNode() {
-		if (!isEmpty()) {
-			if (size == 1) {
-				first = null;
-			} else {
-				Node current = first;
-				for (int i = 0; i < size - 2; i++) {
-					current = current.next;
-				}
-				current.next = null;
-			}
-			
-			size--;
-		}
-	}
-	
-	public void delete(int k) {
-		if (k > size || isEmpty()) {
-			return;
-		}
-		
-		if (k == 1) {
-			first = first.next;
-		} else {
-			Node current;
-			int count = 1;
-			
-			for(current = first; current != null; current = current.next) {
-				if (count == k - 1 && current.next != null) {
-					current.next = current.next.next;
-					break;
-				}
-				count++;
+	public boolean find(LinkedList<String> linkedList, String key) {
+		for (String item : linkedList) {
+			if (item.equals(key)) {
+				return true;
 			}
 		}
-		size--;
-	}
-	
-	public boolean find(String key) {
-		if (isEmpty()) {
-			return false;
-		}
-		
-		boolean contains = false;
-		Node current;
-		for (current = first; current != null; current = current.next) {
-			if (current.item.equals(key)) {
-				contains = true;
-				break;
-			}
-		}
-		return contains;
-	}
-	
-	@Override
-	public Iterator<Item> iterator() {
-		return new ListIterator();
-	}
-	
-	private class ListIterator implements Iterator<Item> {
-		Node current = first;
-		
-		@Override
-		public boolean hasNext() {
-			return current != null; 
-		}
-		
-		@Override
-		public Item next() {
-			Item item = current.item;
-			current = current.next;
-			
-			return item;
-		}
+		return false;
 	}
 
 	public static void main(String[] args) {
-		Exercise21<String> linkedList = new Exercise21<>();
-		linkedList.add("A");
-		linkedList.add("B");
-		linkedList.add("C");
-		linkedList.add("D");
+		Exercise21 exercise21 = new Exercise21();
+		LinkedList<String> linkedList = new LinkedList<>();
+		linkedList.insert("A");
+		linkedList.insert("B");
+		linkedList.insert("C");
+		linkedList.insert("D");
 		
-		StdOut.println("Find result: " + linkedList.find("B") + " Expected: true");
-		StdOut.println("Find result: " + linkedList.find("Z") + " Expected: false");
+		StdOut.println("Find B result: " + exercise21.find(linkedList, "B") + " Expected: true");
+		StdOut.println("Find Z result: " + exercise21.find(linkedList, "Z") + " Expected: false");
 	}
-
 }
