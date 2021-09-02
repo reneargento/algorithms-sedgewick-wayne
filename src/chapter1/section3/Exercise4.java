aa13aaec2ccbc6c1f90b616b4df6832578b67c6c
Rene Argento <rene.argento@gmail.com>
2021-09-02 22:51:06 +0200

    Updated exercise 1.3.4

--- src/chapter1/section3/Exercise4.java
+++ src/chapter1/section3/Exercise4.java
@@ -6,20 +6,24 @@ import edu.princeton.cs.algs4.StdOut;
 /**
  * Created by Rene Argento
  */
+// Thanks to SunJenry (https://github.com/SunJenry) for improving this exercise.
+// https://github.com/reneargento/algorithms-sedgewick-wayne/issues/207
 public class Exercise4 {
 
 	public static void main(String[] args) {
 		Exercise4 exercise4 = new Exercise4();
 
 		String textStreamInput = args[0]; //[()]
-		StdOut.println("Is balanced " + textStreamInput + ": " + exercise4.isBalanced(textStreamInput));
+		StdOut.println("Is balanced " + textStreamInput + ": " + exercise4.isBalanced(textStreamInput)
+				+ " Expected: true");
 
-		StdOut.println("Is balanced [()]{}{[()()]()}: " + exercise4.isBalanced("[()]{}{[()()]()}"));
-		StdOut.println("Is balanced [(]): " + exercise4.isBalanced("[(])"));
+		StdOut.println("Is balanced [()]{}{[()()]()}: " + exercise4.isBalanced("[()]{}{[()()]()}")
+				+ " Expected: true");
+		StdOut.println("Is balanced [(]): " + exercise4.isBalanced("[(])") + " Expected: false");
+		StdOut.println("Is balanced [(): " + exercise4.isBalanced("[()") + " Expected: false");
 	}
 	
 	private boolean isBalanced(String input) {
-
 		char[] parentheses = input.toCharArray();
 		Stack<Character> stack = new Stack<>();
 		
@@ -32,7 +36,6 @@ public class Exercise4 {
 				if (stack.isEmpty()) {
 					return false;
 				}
-				
 				char firstItem = stack.pop();
 				
 				if (parenthesis == ')' && firstItem != '('
@@ -42,8 +45,6 @@ public class Exercise4 {
 				}
 			}
 		}
-		
-		return true;
+		return stack.isEmpty();
 	}
-
 }
