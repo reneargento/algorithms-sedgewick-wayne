3c9dacf4b5b996e544c90e2ebbdbb5940cc9ce59
Rene Argento <rene.argento@gmail.com>
2022-01-31 01:00:39 +0100

    Updated exercise 3.4.21

--- src/chapter3/section4/Exercise21.java
+++ src/chapter3/section4/Exercise21.java
@@ -14,28 +14,31 @@ public class Exercise21 {
             super(size);
         }
 
-        //Average cost of search miss = ~1/2 * (1 + (1 / (1 - a)^2))
-        public double getAverageCostOfSearchMiss_byLoadFactor() {
+        // Average cost of search miss = ~1/2 * (1 + (1 / (1 - a)^2))
+        public double getAverageCostOfSearchMissByLoadFactor() {
             double loadFactor = getLoadFactor();
             return 0.5 * (1 + (1 / Math.pow(1 - loadFactor, 2)));
-
-    }
-
-    //Average cost of search miss = 1 + N/2M + (sum(sqrt(cluster[i].size)))/2M for all clusters in the table
-    public double getAverageCostOfSearchMiss_byClusterSizes() {
-      int clusterSize = 0;
-      int clusterSizeSqrtSum = 0;
-      for (int i = 0; i < size; i++) {
-        if (keys[i] != null) {
-          clusterSize++;
         }
-        else {
-          clusterSizeSqrtSum += clusterSize * clusterSize;
-          clusterSize = 0;
-        }
-      }
 
-      return 1 + ((double)(keysSize + clusterSizeSqrtSum))/ (size << 1);
+        // Average cost of search miss = 1 + N / 2M + (sum((cluster[i].size)^2)) / 2M for all clusters in the table
+        public double getAverageCostOfSearchMissByClusterSizes() {
+            int clusterSize = 0;
+            int clusterSizeSquareSum = 0;
+            
+            for (int i = 0; i < size; i++) {
+                if (keys[i] != null) {
+                    clusterSize++;
+                } else {
+                    clusterSizeSquareSum += clusterSize * clusterSize;
+                    clusterSize = 0;
+                }
+            }
+
+            if (clusterSize != 0) {
+                clusterSizeSquareSum += clusterSize * clusterSize;
+            }
+            return 1 + ((double) (keysSize + clusterSizeSquareSum)) / (size * 2);
+        }
     }
 
     public static void main(String[] args) {
@@ -48,9 +51,9 @@ public class Exercise21 {
             linearProbingHashTableAvgSearchMissCost.put(randomKey, randomKey);
         }
 
-    StdOut.printf("Average cost of search miss: byLoadFactor=%.2f, byClusterSizes=%.2f",
-        linearProbingHashTableAvgSearchMissCost.getAverageCostOfSearchMiss_byLoadFactor(),
-        linearProbingHashTableAvgSearchMissCost.getAverageCostOfSearchMiss_byClusterSizes());
+        StdOut.printf("Average cost of search miss by load factor: %.2f\n",
+                linearProbingHashTableAvgSearchMissCost.getAverageCostOfSearchMissByLoadFactor());
+        StdOut.printf("Average cost of search miss by cluster sizes: %.2f\n",
+                linearProbingHashTableAvgSearchMissCost.getAverageCostOfSearchMissByClusterSizes());
     }
-
 }
