66b23a36cefae5b11b83ac09efece3acbce106ef
Rene Argento <rene.argento@gmail.com>
2022-08-26 16:47:01 +0200

    Updated exercise 2.2.17

--- src/chapter2/section2/Exercise17_LinkedListSort.java
+++ src/chapter2/section2/Exercise17_LinkedListSort.java
@@ -11,63 +11,76 @@ import java.util.StringJoiner;
 // Thanks to dbf256 (https://github.com/dbf256) for mentioning that this exercise had an incorrect implementation of
 // natural sort.
 // https://github.com/reneargento/algorithms-sedgewick-wayne/issues/42
-@SuppressWarnings("unchecked")
-public class Exercise17_LinkedListSort<Item> implements Iterable<Item> {
+// Thanks to ckwastra (https://github.com/ckwastra) for noticing that the subarray selection was not being done optimally.
+// https://github.com/reneargento/algorithms-sedgewick-wayne/issues/261
+public class Exercise17_LinkedListSort  {
 
-    private class Node {
-        Item item;
-        Node next;
+    private static class LinkedList<Item> implements Iterable<Item> {
+        private class Node {
+            Item item;
+            Node next;
 
-        Node() {
+            Node(Item item) {
+                this.item = item;
+            }
         }
 
-        Node(Item item) {
-            this.item = item;
+        private int size;
+        private Node first;
+
+        public boolean isEmpty() {
+            return size == 0;
+        }
+
+        public int size() {
+            return size;
+        }
+
+        public void add(Item item) {
+            Node newNode = new Node(item);
+            newNode.next = first;
+
+            first = newNode;
+            size++;
+        }
+
+        public Iterator<Item> iterator() {
+            return new ListIterator();
+        }
+
+        private class ListIterator implements Iterator<Item> {
+            Node current = first;
+
+            public boolean hasNext() {
+                return current != null;
+            }
+
+            public Item next() {
+                Item item = current.item;
+                current = current.next;
+
+                return item;
+            }
         }
     }
 
-    private int size;
-    private Node first;
+    private static class MergeResult {
+        LinkedList<Integer>.Node newLow;
+        LinkedList<Integer>.Node newHigh;
+        LinkedList<Integer>.Node afterLastNode;
 
-    public boolean isEmpty() {
-        return size == 0;
-    }
-
-    public int size() {
-        return size;
-    }
-
-    public void add(Item item) {
-        Node newNode = new Node(item);
-        newNode.next = first;
-
-        first = newNode;
-        size++;
-    }
-
-    public Iterator<Item> iterator() {
-        return new ListIterator();
-    }
-
-    private class ListIterator implements Iterator<Item> {
-        Node current = first;
-
-        public boolean hasNext() {
-            return current != null;
-        }
-
-        public Item next() {
-            Item item = current.item;
-            current = current.next;
-
-            return item;
+        public MergeResult(LinkedList<Integer>.Node newLow, LinkedList<Integer>.Node newHigh,
+                           LinkedList<Integer>.Node afterLastNode) {
+            this.newLow = newLow;
+            this.newHigh = newHigh;
+            this.afterLastNode = afterLastNode;
         }
     }
 
     public static void main(String[] args) {
-        Exercise17_LinkedListSort<Comparable> linkedList = createList();
+        LinkedList<Integer> linkedList = createList();
 
-        Exercise17_LinkedListSort.Node newSourceNode = mergesort(linkedList.first);
+        LinkedList<Integer>.Node newSourceNode = mergesort(linkedList.first);
         linkedList.first = newSourceNode;
 
         StringJoiner sortedList = new StringJoiner(" ");
@@ -80,9 +93,8 @@ public class Exercise17_LinkedListSort<Item> implements Iterable<Item> {
         StdOut.println("Expected: -10 -9 0 1 5 20 55");
     }
 
-    private static Exercise17_LinkedListSort<Comparable> createList() {
-        Exercise17_LinkedListSort<Comparable> linkedList = new Exercise17_LinkedListSort<>();
-
+    private static LinkedList<Integer> createList() {
+        LinkedList<Integer> linkedList = new LinkedList<>();
         linkedList.add(-9);
         linkedList.add(20);
         linkedList.add(1);
@@ -90,52 +102,76 @@ public class Exercise17_LinkedListSort<Item> implements Iterable<Item> {
         linkedList.add(55);
         linkedList.add(-10);
         linkedList.add(0);
-
         return linkedList;
     }
 
-    private static Exercise17_LinkedListSort.Node mergesort(Exercise17_LinkedListSort<Comparable>.Node sourceNode) {
+    private static LinkedList<Integer>.Node mergesort(LinkedList<Integer>.Node sourceNode) {
         if (sourceNode == null || sourceNode.next == null) {
             return sourceNode;
         }
 
-        Exercise17_LinkedListSort<Comparable>.Node low = sourceNode;
-        Exercise17_LinkedListSort<Comparable>.Node middle = sourceNode;
-        Exercise17_LinkedListSort<Comparable>.Node high = sourceNode;
-        Exercise17_LinkedListSort<Comparable>.Node currentNode = sourceNode;
+        LinkedList<Integer>.Node beforeLow = null;
+        LinkedList<Integer>.Node low = sourceNode;
+        LinkedList<Integer>.Node middle;
+        LinkedList<Integer>.Node high;
 
-        boolean secondSubArray = false;
-
-        while (currentNode != null && currentNode.next != null) {
-
-            if (currentNode.item.compareTo(currentNode.next.item) > 0) {
-                if (!secondSubArray) {
-                    middle = currentNode;
-                    secondSubArray = true;
-                } else {
-                    high = currentNode;
-                    low = merge(low, middle, high);
-                    middle = high;
+        while (true) {
+            middle = findSortedSubArray(low);
+            if (middle.next == null) {
+                if (low.equals(sourceNode)) // Array is sorted
+                    break;
+                else {
+                    low = sourceNode;
+                    beforeLow = null;
+                    continue;
                 }
             }
-            currentNode = currentNode.next;
-        }
+            high = findSortedSubArray(middle.next);
+            MergeResult mergeResult = merge(low, middle, high);
+            if (low == sourceNode) {
+                sourceNode = mergeResult.newLow;
+            }
 
-        if (high.next != null && currentNode != null) {
-            low = merge(low, middle, currentNode);
-        }
+            if (beforeLow != null) {
+                beforeLow.next = mergeResult.newLow;
+            } else {
+                beforeLow = mergeResult.newHigh;
+            }
 
+            if (mergeResult.afterLastNode == null) {
+                low = sourceNode;
+                beforeLow = null;
+            } else {
+                low = mergeResult.afterLastNode;
+            }
+        }
         return low;
     }
 
-    private static Exercise17_LinkedListSort<Comparable>.Node merge(Exercise17_LinkedListSort<Comparable>.Node low,
-                                                                    Exercise17_LinkedListSort<Comparable>.Node middle,
-                                                                    Exercise17_LinkedListSort<Comparable>.Node high) {
-        Exercise17_LinkedListSort<Comparable>.Node leftNode = low;
-        Exercise17_LinkedListSort<Comparable>.Node rightNode = middle.next;
-        Exercise17_LinkedListSort<Comparable>.Node newLow;
-        Exercise17_LinkedListSort<Comparable>.Node afterLastNode = high.next;
-        Exercise17_LinkedListSort<Comparable>.Node aux;
+    private static LinkedList<Integer>.Node findSortedSubArray(LinkedList<Integer>.Node currentNode) {
+        while (currentNode.next != null) {
+            if (currentNode.item.compareTo(currentNode.next.item) > 0) {
+                return currentNode;
+            }
+            currentNode = currentNode.next;
+        }
+        return currentNode;
+    }
+
+    private static MergeResult merge(LinkedList<Integer>.Node low, LinkedList<Integer>.Node middle,
+                                     LinkedList<Integer>.Node high) {
+        LinkedList<Integer>.Node leftNode = low;
+        LinkedList<Integer>.Node rightNode = middle.next;
+        LinkedList<Integer>.Node newLow;
+        LinkedList<Integer>.Node newHigh;
+        LinkedList<Integer>.Node afterLastNode = high.next;
+        LinkedList<Integer>.Node aux;
+
+        if (middle.item.compareTo(high.item) > 0) {
+            newHigh = middle;
+        } else {
+            newHigh = high;
+        }
 
         if (leftNode.item.compareTo(rightNode.item) <= 0) {
             newLow = leftNode;
@@ -165,7 +201,6 @@ public class Exercise17_LinkedListSort<Item> implements Iterable<Item> {
             aux.next = leftNode;
             middle.next = afterLastNode;
         }
-
-        return newLow;
+        return new MergeResult(newLow, newHigh, afterLastNode);
     }
 }
