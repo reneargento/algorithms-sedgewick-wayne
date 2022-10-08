2761fd32fa0eb774b24ccd87e9c827e3ff07cca8
Rene Argento <rene.argento@gmail.com>
2022-10-08 20:03:26 +0200

    Updated exercise 2.5.29

--- src/chapter2/section5/Exercise29_SortFilesBySizeAndDate.java
+++ src/chapter2/section5/Exercise29_SortFilesBySizeAndDate.java
@@ -13,6 +13,8 @@ import java.util.List;
 /**
  * Created by Rene Argento on 16/04/17.
  */
+// Thanks to ckwastra (https://github.com/ckwastra) for suggesting a way to sort the files only once.
+// https://github.com/reneargento/algorithms-sedgewick-wayne/issues/269
 public class Exercise29_SortFilesBySizeAndDate {
 
     public static void main(String[] args) {
@@ -32,8 +34,14 @@ public class Exercise29_SortFilesBySizeAndDate {
         }
     }
 
-    private File[] readFlagsAndSortFiles(File directory, String[] flags) {
+    private static final Comparator<File> NAME_ORDER = Comparator.comparing(File::getName);
+    private static final Comparator<File> SIZE_ORDER = Comparator.comparingLong(File::length);
+    private static final Comparator<File> TIME_ORDER = Comparator.comparingLong(File::lastModified);
+    private static final Comparator<File> NAME_REVERSED_ORDER = NAME_ORDER.reversed();
+    private static final Comparator<File> SIZE_REVERSED_ORDER = SIZE_ORDER.reversed();
+    private static final Comparator<File> TIME_REVERSED_ORDER = TIME_ORDER.reversed();
 
+    private File[] readFlagsAndSortFiles(File directory, String[] flags) {
         File[] filesInCurrentDirectory = directory.listFiles();
         if (filesInCurrentDirectory == null) {
             return null;
@@ -55,144 +63,75 @@ public class Exercise29_SortFilesBySizeAndDate {
          * -t timestamp
          * -n name
          *
-         * -d decreasing
+         * -sd size decreasing
+         * -td timestamp decreasing
+         * -nd name decreasing
          *
-         * Usage:
-         * -s -t and -n can be followed by -d to specify a decreasing order
-         *  if nothing is specified, the default is increasing order
+         * Usage example:
+         * -s -td -n
+         * First sort by file size in increasing order, then by timestamp in decreasing order and
+         * then by file name in increasing order
          */
-        for (int i = flags.length - 1; i >= 0; i--) {
-            String flag = flags[i];
-
-            boolean isDecreasing = flag.equals("-d");
-
-            if (isDecreasing && i > 0) {
-                i--;
-                flag = flags[i];
-            } else if (isDecreasing) {
-                throw new IllegalArgumentException("Missing flag (-s -t or -n)");
-            }
-
-            switch (flag) {
-                case "-s":
-                    if (!isDecreasing) {
-                        Arrays.sort(files, fileSizeIncreasingComparator());
-                    } else {
-                        Arrays.sort(files, fileSizeDecreasingComparator());
+        switch (flags[0]) {
+            // No duplicate file names
+            case "-n":
+                // Sort by file name in increasing order
+                Arrays.sort(files, NAME_ORDER);
+                break;
+            case "-nd":
+                // Sort by file name in decreasing order
+                Arrays.sort(files, NAME_REVERSED_ORDER);
+                break;
+            default:
+                Comparator<File> comparator;
+                switch (flags[0]) {
+                    case "-t":
+                        // Sort by timestamp in increasing order
+                        comparator = TIME_ORDER;
+                        break;
+                    case "-td":
+                        // Sort by timestamp in decreasing order
+                        comparator = TIME_REVERSED_ORDER;
+                        break;
+                    case "-s":
+                        // Sort by file size in increasing order
+                        comparator = SIZE_ORDER;
+                        break;
+                    case "-sd":
+                        // Sort by file size in decreasing order
+                        comparator = SIZE_REVERSED_ORDER;
+                        break;
+                    default:
+                        throw new IllegalArgumentException("Invalid flag " + flags[0]);
+                }
+                // Accept up to 3 flags
+                int flagsToProcess = Math.min(3, flags.length);
+                outer: for (int i = 1; i < flagsToProcess; i++) {
+                    switch (flags[i]) {
+                        case "-n":
+                            comparator = comparator.thenComparing(NAME_ORDER);
+                            break outer;
+                        case "-nd":
+                            comparator = comparator.thenComparing(NAME_REVERSED_ORDER);
+                            break outer;
+                        case "-t":
+                            comparator = comparator.thenComparing(TIME_ORDER);
+                            break;
+                        case "-td":
+                            comparator = comparator.thenComparing(TIME_REVERSED_ORDER);
+                            break;
+                        case "-s":
+                            comparator = comparator.thenComparing(SIZE_ORDER);
+                            break;
+                        case "-sd":
+                            comparator = comparator.thenComparing(SIZE_REVERSED_ORDER);
+                            break;
+                        default:
+                            throw new IllegalArgumentException("Invalid flag " + flags[i]);
                     }
-                    break;
-                case "-t":
-                    if (!isDecreasing) {
-                        Arrays.sort(files, fileModifiedDateIncreasingComparator());
-                    } else {
-                        Arrays.sort(files, fileModifiedDateDecreasingComparator());
-                    }
-                    break;
-                case "-n":
-                    if (!isDecreasing) {
-                        Arrays.sort(files, fileNameIncreasingComparator());
-                    } else {
-                        Arrays.sort(files, fileNameDecreasingComparator());
-                    }
-                    break;
-                default: throw new IllegalArgumentException("Invalid flag. The only accepted flags are: -s -t -n -d");
-            }
+                }
+                Arrays.sort(files, comparator);
         }
-
         return files;
-    }
-
-    private Comparator<File> fileSizeIncreasingComparator() {
-        return new Comparator<File>() {
-            @Override
-            public int compare(File file1, File file2) {
-                long size1 = file1.length();
-                long size2 = file2.length();
-
-                if (size1 < size2) {
-                    return -1;
-                } else if (size1 > size2) {
-                    return 1;
-                }
-                return 0;
-            }
-        };
-    }
-
-    private Comparator<File> fileSizeDecreasingComparator() {
-        return new Comparator<File>() {
-            @Override
-            public int compare(File file1, File file2) {
-                long size1 = file1.length();
-                long size2 = file2.length();
-
-                if (size1 > size2) {
-                    return -1;
-                } else if (size1 < size2) {
-                    return 1;
-                }
-                return 0;
-            }
-        };
-    }
-
-    private Comparator<File> fileNameIncreasingComparator() {
-        return new Comparator<File>() {
-            @Override
-            public int compare(File file1, File file2) {
-                return file1.getName().compareTo(file2.getName());
-            }
-        };
-    }
-
-    private Comparator<File> fileNameDecreasingComparator() {
-        return new Comparator<File>() {
-            @Override
-            public int compare(File file1, File file2) {
-                int increasingCompare = file1.getName().compareTo(file2.getName());
-
-                if (increasingCompare < 0) {
-                    return 1;
-                } else if (increasingCompare > 0) {
-                    return -1;
-                }
-
-                return 0;
-            }
-        };
-    }
-
-    private Comparator<File> fileModifiedDateIncreasingComparator() {
-        return new Comparator<File>() {
-            @Override
-            public int compare(File file1, File file2) {
-                long lastModified1 = file1.lastModified();
-                long lastModified2 = file2.lastModified();
-
-                if (lastModified1 < lastModified2) {
-                    return -1;
-                } else if (lastModified1 > lastModified2) {
-                    return 1;
-                }
-                return 0;
-            }
-        };
-    }
-
-    private Comparator<File> fileModifiedDateDecreasingComparator() {
-        return new Comparator<File>() {
-            @Override
-            public int compare(File file1, File file2) {
-                long lastModified1 = file1.lastModified();
-                long lastModified2 = file2.lastModified();
-
-                if (lastModified1 > lastModified2) {
-                    return -1;
-                } else if (lastModified1 < lastModified2) {
-                    return 1;
-                }
-                return 0;
-            }
-        };
     }
 }
