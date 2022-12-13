4ba0dbd7f1c20ec0c852bb820508f0866326eb10
Rene Argento <rene.argento@gmail.com>
2022-12-13 17:55:08 -0300

    Updated exercise 3.2.8

--- src/chapter3/section2/Exercise8.java
+++ src/chapter3/section2/Exercise8.java
@@ -5,44 +5,57 @@ import edu.princeton.cs.algs4.StdOut;
 /**
  * Created by Rene Argento on 27/05/17.
  */
+// Thanks to ckwastra (https://github.com/ckwastra) for suggesting a O(1) solution.
+// https://github.com/reneargento/algorithms-sedgewick-wayne/issues/277
 public class Exercise8 {
 
     public static void main(String[] args) {
-        StdOut.println("Test 0: " + optCompares(0) + " Expected: 0");
-        StdOut.println("Test 1: " + optCompares(1) + " Expected: 2");
-        StdOut.println("Test 2: " + optCompares(2) + " Expected: 2");
-        StdOut.println("Test 3: " + optCompares(3) + " Expected: 2");
-        StdOut.println("Test 7: " + optCompares(7) + " Expected: 3");
-        StdOut.println("Test 8: " + optCompares(8) + " Expected: 3");
-        StdOut.println("Test 15: " + optCompares(15) + " Expected: 4");
-        StdOut.println("Test 16: " + optCompares(16) + " Expected: 4");
+        StdOut.printf("Test 0: %.2f Expected: 0.00\n", optCompares(0));
+        StdOut.printf("Test 1: %.2f Expected: 1.00\n", optCompares(1));
+        StdOut.printf("Test 2: %.2f Expected: 1.50\n", optCompares(2));
+        StdOut.printf("Test 3: %.2f Expected: 1.67\n", optCompares(3));
+        StdOut.printf("Test 7: %.2f Expected: 2.43\n", optCompares(7));
+        StdOut.printf("Test 8: %.2f Expected: 2.63\n", optCompares(8));
+        StdOut.printf("Test 15: %.2f Expected: 3.27\n", optCompares(15));
+        StdOut.printf("Test 16: %.2f Expected: 3.38\n", optCompares(16));
     }
 
-    // O(h)
-    private static int optCompares(int n) {
+    // O(1)
+    // Consider H = height of the tree.
+    // For nodes on the first H levels, the total path length is:
+    // SUM(from i = 1 to H - 1) i * 2^i = (H - 2) * 2^H + 2
+    // The total path in the last level is:
+    // (N - (2^H - 1)) * H = (N - 2^H + 1) * H
+    // Adding them, we have:
+    // (H - 2) * 2^H + 2 + (N - 2^H + 1) * H = (N + 1) * H - 2^(H + 1) + 2
+    private static double optCompares(int n) {
         if (n == 0) {
             return 0;
         }
+        int height = (int) (Math.log(n) / Math.log(2));
+        return ((n + 1) * height - Math.pow(2, height + 1) + 2) / n + 1;
+    }
 
+    // O(h)
+    private static double optCompares2(int n) {
+        if (n == 0) {
+            return 0;
+        }
         int totalCompares = 0;
-
-        int heightMinus1 = (int) (Math.log(n) / Math.log(2));
+        int height = (int) (Math.log(n) / Math.log(2));
         int numberOfNodesBeforeLastLevel = 0;
 
         // Compute the compares in all levels, except the last (because the last level may not be complete)
-        for (int i = 1; i <= heightMinus1; i++) {
+        for (int i = 1; i <= height; i++) {
             totalCompares += i * Math.pow(2, i - 1);
-
             numberOfNodesBeforeLastLevel += Math.pow(2, i - 1);
         }
 
         // Add compares required to reach the nodes in the last level
         int nodesInLastLevel = n - numberOfNodesBeforeLastLevel;
-        totalCompares += nodesInLastLevel * (heightMinus1 + 1);
+        totalCompares += nodesInLastLevel * (height + 1);
 
         // Total compares is the internal path length
-        int numberOfComparesRequiredByRandomSearchHit = (totalCompares / n) + 1;
-
-        return numberOfComparesRequiredByRandomSearchHit;
+        return (totalCompares / (double) n);
     }
 }
