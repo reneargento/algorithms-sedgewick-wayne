e927b3c060d608e2e4a613068877f916a98206d0
Rene Argento <rene.argento@gmail.com>
2022-12-13 21:07:36 -0300

    Updated exercise 4.1.32

--- src/chapter4/section1/Exercise32_ParallelEdgeDetection.java
+++ src/chapter4/section1/Exercise32_ParallelEdgeDetection.java
@@ -5,23 +5,26 @@ import edu.princeton.cs.algs4.StdOut;
 /**
  * Created by Rene Argento on 01/10/17.
  */
+// Thanks to SSoelvsten (https://github.com/SSoelvsten) for suggesting a fix to the runtime complexity
+// of countParallelEdges().
+// https://github.com/reneargento/algorithms-sedgewick-wayne/issues/278
 public class Exercise32_ParallelEdgeDetection {
 
     private int countParallelEdges(Graph graph) {
         int parallelEdges = 0;
+        int currentIteration = 1;
+        int[] neighbors = new int[graph.vertices()];
 
         for (int vertex = 0; vertex < graph.vertices(); vertex++) {
-            boolean[] neighbors = new boolean[graph.vertices()];
-
             for (int neighbor : graph.adjacent(vertex)) {
-                if (!neighbors[neighbor]) {
-                    neighbors[neighbor] = true;
+                if (neighbors[neighbor] != currentIteration) {
+                    neighbors[neighbor] = currentIteration;
                 } else {
                     parallelEdges++;
                 }
             }
+            currentIteration++;
         }
-
         return parallelEdges / 2;
     }
 
@@ -33,7 +36,7 @@ public class Exercise32_ParallelEdgeDetection {
         graph1.addEdge(1, 2);
         graph1.addEdge(2, 3);
         graph1.addEdge(3, 0);
-        StdOut.println(exercise32.countParallelEdges(graph1) + " Expected: 0");
+        StdOut.println("Parallel edges: " + exercise32.countParallelEdges(graph1) + " Expected: 0");
 
         Graph graph2 = new Graph(4);
         graph2.addEdge(0, 1);
@@ -41,7 +44,7 @@ public class Exercise32_ParallelEdgeDetection {
         graph2.addEdge(2, 3);
         graph2.addEdge(3, 0);
         graph2.addEdge(3, 2);
-        StdOut.println(exercise32.countParallelEdges(graph2) + " Expected: 1");
+        StdOut.println("Parallel edges: " + exercise32.countParallelEdges(graph2) + " Expected: 1");
 
         Graph graph3 = new Graph(4);
         graph3.addEdge(0, 1);
@@ -50,6 +53,6 @@ public class Exercise32_ParallelEdgeDetection {
         graph3.addEdge(3, 2);
         graph3.addEdge(3, 2);
         graph3.addEdge(3, 0);
-        StdOut.println(exercise32.countParallelEdges(graph3) + " Expected: 3");
+        StdOut.println("Parallel edges: " + exercise32.countParallelEdges(graph3) + " Expected: 3");
     }
 }
