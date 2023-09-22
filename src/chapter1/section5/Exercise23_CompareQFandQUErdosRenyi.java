4a7916a987e57b2a6db458a82bbe4beedf73da05
Rene Argento <rene.argento@gmail.com>
2023-09-22 17:33:40 +0200

    Updated exercise 1.5.23

--- src/chapter1/section5/Exercise23_CompareQFandQUErdosRenyi.java
+++ src/chapter1/section5/Exercise23_CompareQFandQUErdosRenyi.java
@@ -10,10 +10,11 @@ import java.util.List;
 /**
  * Created by Rene Argento on 20/12/16.
  */
+// Thanks to lhjl1065 (https://github.com/lhjl1065) for reporting a bug with one of the object types.
+// https://github.com/reneargento/algorithms-sedgewick-wayne/issues/298
 public class Exercise23_CompareQFandQUErdosRenyi {
 
-    private class Experiment {
-
+    private static class Experiment {
         int numberOfSites;
         double ratioBetweenUnionFindModels;
 
@@ -31,42 +32,40 @@ public class Exercise23_CompareQFandQUErdosRenyi {
     }
 
     private void doExperiments(int numberOfExperiments) {
-
         List<Exercise23_CompareQFandQUErdosRenyi.Experiment> experiments = new ArrayList<>();
-
         int numberOfSites = 512;
 
         for (int i = 0; i < numberOfExperiments; i++) {
-
+            List<Exercise18_RandomGridGenerator.Connection> connectionsGenerated =
+                    erdosRenyiGeneratingConnections(numberOfSites);
             // QuickUnion
             UF quickUnion = new QuickUnion(numberOfSites);
 
             Stopwatch timer = new Stopwatch();
-            List<Exercise18_RandomGridGenerator.Connection> connectionsGenerated =
-                    erdosRenyiGeneratingConnections(numberOfSites, quickUnion);
+            erdosRenyiUsingConnections(quickUnion, connectionsGenerated);
             double runningTimeQuickUnion = timer.elapsedTime();
 
             // QuickFind
-            UF quickFind = new QuickUnion(numberOfSites);
+            UF quickFind = new QuickFind(numberOfSites);
 
             timer = new Stopwatch();
             erdosRenyiUsingConnections(quickFind, connectionsGenerated);
             double runningTimeQuickFind = timer.elapsedTime();
 
-            Experiment experiment = new Experiment(numberOfSites, runningTimeQuickFind / runningTimeQuickUnion);
+            double ratio = runningTimeQuickFind / runningTimeQuickUnion;
+            Experiment experiment = new Experiment(numberOfSites, ratio);
             experiments.add(experiment);
 
             numberOfSites *= 2;
         }
-
         printResults(experiments);
     }
 
-    private List<Exercise18_RandomGridGenerator.Connection> erdosRenyiGeneratingConnections(int numberOfSites, UF unionFind) {
+    private List<Exercise18_RandomGridGenerator.Connection> erdosRenyiGeneratingConnections(int numberOfSites) {
+        UF quickUnion = new QuickUnion(numberOfSites);
         List<Exercise18_RandomGridGenerator.Connection> connectionsGenerated = new ArrayList<>();
 
-        while (unionFind.count() != 1) {
-
+        while (quickUnion.count() != 1) {
             int randomSite1 = StdRandom.uniform(numberOfSites);
             int randomSite2 = StdRandom.uniform(numberOfSites);
 
@@ -74,11 +73,10 @@ public class Exercise23_CompareQFandQUErdosRenyi {
                     randomSite1, randomSite2);
             connectionsGenerated.add(connection);
 
-            if (!unionFind.connected(randomSite1, randomSite2)) {
-                unionFind.union(randomSite1, randomSite2);
+            if (!quickUnion.connected(randomSite1, randomSite2)) {
+                quickUnion.union(randomSite1, randomSite2);
             }
         }
-
         return connectionsGenerated;
     }
 
@@ -101,7 +99,7 @@ public class Exercise23_CompareQFandQUErdosRenyi {
         int experimentId = 1;
 
         for (Experiment experiment : experiments) {
-            StdOut.printf("%7d %13d %23.1f\n", experimentId, experiment.numberOfSites, experiment.ratioBetweenUnionFindModels);
+            StdOut.printf("%10d %17d %23.1f\n", experimentId, experiment.numberOfSites, experiment.ratioBetweenUnionFindModels);
             experimentId++;
         }
     }
